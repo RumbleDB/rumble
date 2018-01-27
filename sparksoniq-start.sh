367a9607644ef20d17f9d83d4e4c6cad3712bb62
istefan <istefan@student.ethz.ch>
2018-01-27 20:33:36 +0100

    updated launch script

--- sparksoniq-start.sh
+++ sparksoniq-start.sh
@@ -7,4 +7,4 @@ mvn clean compile assembly:single
 fi
 
 
-spark-submit --class sparksoniq.ShellStart --conf spark.executor.extraClassPath=../lib/* --conf spark.driver.extraClassPath=../lib/* target/jsoniq-spark-app-0.9.2-jar-with-dependencies.jar --master local[2] --deploy-mode client
+spark-submit --class sparksoniq.ShellStart --conf spark.executor.extraClassPath=lib/* --conf spark.driver.extraClassPath=lib/* target/jsoniq-spark-app-0.9.2-jar-with-dependencies.jar --master local[2] --deploy-mode client
