b782406dd9462056c40d27219e76b3c1cc4635a0
canberker <canberkerwork@gmail.com>
2018-08-24 19:39:00 +0200

    override open for StringConcat

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/operational/StringConcatIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/operational/StringConcatIterator.java
@@ -29,29 +29,45 @@ import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.operational.base.BinaryOperationBaseIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 public class StringConcatIterator extends BinaryOperationBaseIterator {
+
+    StringItem result;
+
     public StringConcatIterator(RuntimeIterator left, RuntimeIterator right, IteratorMetadata iteratorMetadata) {
         super(left, right, OperationalExpressionBase.Operator.CONCAT, iteratorMetadata);
     }
 
     @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+
+        _leftIterator.open(_currentDynamicContext);
+        _rightIterator.open(_currentDynamicContext);
+        Item left = _leftIterator.next();
+        Item right = _rightIterator.next();
+        if (!(left instanceof StringItem) || !(right instanceof StringItem))
+            throw new UnexpectedTypeException("String concat expression has non strings args " +
+                    left.serialize() + ", " + right.serialize(), getMetadata());
+        StringItem leftString = (StringItem) left;
+        StringItem rightString = (StringItem) right;
+        _leftIterator.close();
+        _rightIterator.close();
+
+        this.result = new StringItem(leftString.getStringValue().concat(rightString.getStringValue()),
+                ItemMetadata.fromIteratorMetadata(getMetadata()));
+        this._hasNext = true;
+    }
+
+    @Override
     public AtomicItem next() {
         if (this.hasNext()) {
-            _leftIterator.open(_currentDynamicContext);
-            _rightIterator.open(_currentDynamicContext);
-            Item left = _leftIterator.next();
-            Item right = _rightIterator.next();
-            if (!(left instanceof StringItem) || !(right instanceof StringItem))
-                throw new UnexpectedTypeException("String concat expression has non strings args " +
-                        left.serialize() + ", " + right.serialize(), getMetadata());
-            StringItem leftString = (StringItem) left;
-            StringItem rightString = (StringItem) right;
-            _leftIterator.close();
-            _rightIterator.close();
             this._hasNext = false;
-            return new StringItem(leftString.getStringValue().concat(rightString.getStringValue()),
-                    ItemMetadata.fromIteratorMetadata(getMetadata()));
+            return this.result;
         }
         throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE, getMetadata());
 
