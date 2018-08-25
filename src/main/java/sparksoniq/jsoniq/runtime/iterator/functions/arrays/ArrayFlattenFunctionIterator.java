e61c52673b59f2b94da160d2d5494bbfae69bb1a
canberker <canberkerwork@gmail.com>
2018-08-25 12:20:05 +0200

    override open for ArrayFlatten

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/arrays/ArrayFlattenFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/arrays/ArrayFlattenFunctionIterator.java
@@ -4,6 +4,7 @@ import sparksoniq.exceptions.IteratorFlowException;
 import sparksoniq.jsoniq.item.Item;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import javax.naming.OperationNotSupportedException;
 import java.util.ArrayList;
@@ -17,17 +18,29 @@ public class ArrayFlattenFunctionIterator extends ArrayFunctionIterator {
     @Override
     public Item next() {
         if (this._hasNext) {
-            if (results == null) {
-                _currentIndex = 0;
-                results = new ArrayList<>();
-                RuntimeIterator sequenceIterator = this._children.get(0);
-                List<Item> items = getItemsFromIteratorWithCurrentContext(sequenceIterator);
-                flatten(items);
-            }
             return getResult();
         }
         throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " FLATTEN function",
                 getMetadata());
+    }
+
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+
+        _currentIndex = 0;
+        results = new ArrayList<>();
+        RuntimeIterator sequenceIterator = this._children.get(0);
+        List<Item> items = getItemsFromIteratorWithCurrentContext(sequenceIterator);
+        flatten(items);
+        if(results.size() == 0) {
+            this._hasNext = false;
+        } else {
+            this._hasNext = true;
+        }
     }
 
     public void flatten(List<Item> items) {
