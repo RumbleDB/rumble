89d61cb8ba8baff481ac4158aa6ef8a84f054be2
canberker <canberkerwork@gmail.com>
2018-08-25 12:21:48 +0200

    override open for ArrayMembers

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/arrays/ArrayMembersFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/arrays/ArrayMembersFunctionIterator.java
@@ -8,6 +8,7 @@ import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.functions.base.LocalFunctionCallIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import javax.naming.OperationNotSupportedException;
 import java.util.ArrayList;
@@ -23,27 +24,39 @@ public class ArrayMembersFunctionIterator extends ArrayFunctionIterator {
     @Override
     public Item next() {
         if (this._hasNext) {
-            if (results == null) {
-                _currentIndex = 0;
-                results = new ArrayList<>();
-                RuntimeIterator sequenceIterator = this._children.get(0);
-                List<Item> items= getItemsFromIteratorWithCurrentContext(sequenceIterator);
-                for (Item item:items) {
-                    if (item.isArray()) {
-                        try {
-                            int size = item.getSize();
-                            for (int i = 0; i < size; i++) {
-                                results.add(item.getItemAt(i));
-                            }
-                        } catch (OperationNotSupportedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
             return getResult();
         }
         throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + "MEMBERS function",
                 getMetadata());
+    }
+
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+
+        _currentIndex = 0;
+        results = new ArrayList<>();
+        RuntimeIterator sequenceIterator = this._children.get(0);
+        List<Item> items= getItemsFromIteratorWithCurrentContext(sequenceIterator);
+        for (Item item:items) {
+            if (item.isArray()) {
+                try {
+                    int size = item.getSize();
+                    for (int i = 0; i < size; i++) {
+                        results.add(item.getItemAt(i));
+                    }
+                } catch (OperationNotSupportedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if(results.size() == 0) {
+            this._hasNext = false;
+        } else {
+            this._hasNext = true;
+        }
     }
 }
