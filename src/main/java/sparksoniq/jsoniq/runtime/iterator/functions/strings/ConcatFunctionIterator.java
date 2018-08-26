0775b7ba04b81e3bcfa6524f14860a8697f2799b
canberker <canberkerwork@gmail.com>
2018-08-26 15:29:35 +0200

    override open for concat

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/strings/ConcatFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/strings/ConcatFunctionIterator.java
@@ -7,6 +7,7 @@ import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.functions.base.LocalFunctionCallIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.util.List;
 
@@ -17,15 +18,28 @@ public class ConcatFunctionIterator extends LocalFunctionCallIterator {
 
     @Override
     public Item next() {
-        if (this._hasNext) {
-            StringBuilder builder = new StringBuilder("");
-            for (RuntimeIterator iterator : this._children) {
-                StringItem stringItem = this.getSingleItemOfTypeFromIterator(iterator, StringItem.class);
-                builder.append(stringItem.getStringValue());
-            }
+        if (this.hasNext()) {
             this._hasNext = false;
-            return new StringItem(builder.toString(), ItemMetadata.fromIteratorMetadata(getMetadata()));
+            return result;
         } else
             throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " substring function", getMetadata());
     }
+
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+
+        StringBuilder builder = new StringBuilder("");
+        for (RuntimeIterator iterator : this._children) {
+            StringItem stringItem = this.getSingleItemOfTypeFromIterator(iterator, StringItem.class);
+            builder.append(stringItem.getStringValue());
+        }
+        this.result = new StringItem(builder.toString(), ItemMetadata.fromIteratorMetadata(getMetadata()));
+        this._hasNext = true;
+    }
+
+    private Item result;
 }
