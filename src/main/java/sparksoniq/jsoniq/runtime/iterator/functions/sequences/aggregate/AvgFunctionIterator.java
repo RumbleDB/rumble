87a3918fe7f909c1038109da8944eb9883db05b1
canberker <canberkerwork@gmail.com>
2018-08-26 14:46:36 +0200

    override open for avg

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/sequences/aggregate/AvgFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/sequences/aggregate/AvgFunctionIterator.java
@@ -6,6 +6,7 @@ import sparksoniq.jsoniq.item.Item;
 import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -17,25 +18,40 @@ public class AvgFunctionIterator extends AggregateFunctionIterator {
 
     @Override
     public Item next() {
-        if (this._hasNext) {
-            RuntimeIterator sequenceIterator = this._children.get(0);
-            List<Item> results = getItemsFromIteratorWithCurrentContext(sequenceIterator);
+        if (this.hasNext()) {
             this._hasNext = false;
-            results.forEach(r -> {
-                if (!Item.isNumeric(r))
-                    throw new IllegalArgumentException("Aggregate function argument is non numeric");
-            });
-            //TODO refactor empty items
-            if (results.size() == 0)
-                return null;
-            //TODO check numeric types conversions
-            BigDecimal sum = new BigDecimal(0);
-            for (Item r : results)
-                sum = sum.add(Item.getNumericValue(r, BigDecimal.class));
-            return new DecimalItem(sum.divide(new BigDecimal(results.size())),
-                    ItemMetadata.fromIteratorMetadata(getMetadata()));
+            return result;
         } else
             throw new IteratorFlowException(FLOW_EXCEPTION_MESSAGE + "AVG function",
                     getMetadata());
     }
+
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+
+        RuntimeIterator sequenceIterator = this._children.get(0);
+        List<Item> results = getItemsFromIteratorWithCurrentContext(sequenceIterator);
+        results.forEach(r -> {
+            if (!Item.isNumeric(r))
+                throw new IllegalArgumentException("Aggregate function argument is non numeric");
+        });
+        if (results.size() == 0) {
+            this._hasNext = false;
+        }
+        else {
+            this._hasNext = true;
+        }
+        //TODO check numeric types conversions
+        BigDecimal sum = new BigDecimal(0);
+        for (Item r : results)
+            sum = sum.add(Item.getNumericValue(r, BigDecimal.class));
+        this.result = new DecimalItem(sum.divide(new BigDecimal(results.size())),
+                ItemMetadata.fromIteratorMetadata(getMetadata()));
+    }
+
+    Item result;
 }
