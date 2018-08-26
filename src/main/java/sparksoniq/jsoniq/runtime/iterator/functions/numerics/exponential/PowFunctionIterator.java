d238384122bef9f187161ad3886e702bb5d87147
canberker <canberkerwork@gmail.com>
2018-08-26 14:37:01 +0200

    override open for pow

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/numerics/exponential/PowFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/numerics/exponential/PowFunctionIterator.java
@@ -9,6 +9,7 @@ import sparksoniq.jsoniq.runtime.iterator.EmptySequenceIterator;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.functions.base.LocalFunctionCallIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.util.List;
 
@@ -19,30 +20,40 @@ public class PowFunctionIterator extends LocalFunctionCallIterator {
 
     @Override
     public Item next() {
-        if (this._hasNext) {
-            RuntimeIterator iterator = this._children.get(0);
-            //TODO refactor empty items
-            if (iterator.getClass() == EmptySequenceIterator.class) {
-                return null;
-            }
-            else {
-                Item base = this.getSingleItemOfTypeFromIterator(iterator, Item.class);
-                Item exponent = this.getSingleItemOfTypeFromIterator(this._children.get(1), Item.class);
-                if (Item.isNumeric(base) && Item.isNumeric(exponent)) {
-                    Double result = Math.pow(Item.getNumericValue(base, Double.class)
-                            , Item.getNumericValue(exponent, Double.class));
-                    this._hasNext = false;
-                    return new DoubleItem(result,
-                            ItemMetadata.fromIteratorMetadata(getMetadata()));
-                }
-                else {
-                    throw new UnexpectedTypeException("Pow expression has non numeric args " +
-                            base.serialize() + ", " + exponent.serialize(), getMetadata());
-                }
-            }
+        if (this.hasNext()) {
+            this._hasNext = false;
+            return result;
         } else
             throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " pow function", getMetadata());
     }
 
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
 
+        RuntimeIterator iterator = this._children.get(0);
+        if (iterator.getClass() == EmptySequenceIterator.class) {
+            this._hasNext = false;
+        }
+        else {
+            Item base = this.getSingleItemOfTypeFromIterator(iterator, Item.class);
+            Item exponent = this.getSingleItemOfTypeFromIterator(this._children.get(1), Item.class);
+            if (Item.isNumeric(base) && Item.isNumeric(exponent)) {
+                Double result = Math.pow(Item.getNumericValue(base, Double.class)
+                        , Item.getNumericValue(exponent, Double.class));
+                this._hasNext = true;
+                this.result = new DoubleItem(result,
+                        ItemMetadata.fromIteratorMetadata(getMetadata()));
+            }
+            else {
+                throw new UnexpectedTypeException("Pow expression has non numeric args " +
+                        base.serialize() + ", " + exponent.serialize(), getMetadata());
+            }
+        }
+    }
+
+    Item result;
 }
