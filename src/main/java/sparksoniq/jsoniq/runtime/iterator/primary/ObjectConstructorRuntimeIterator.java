761a77a1b7e3f462beba9bddf1cd2be7f7c5897b
canberker <canberkerwork@gmail.com>
2018-08-26 16:25:59 +0200

    override open for objectconstructor

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/primary/ObjectConstructorRuntimeIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/primary/ObjectConstructorRuntimeIterator.java
@@ -28,12 +28,12 @@ import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.iterator.LocalRuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ObjectConstructorRuntimeIterator extends LocalRuntimeIterator {
-
     public ObjectConstructorRuntimeIterator(List<RuntimeIterator> keys, List<RuntimeIterator> values,
                                             IteratorMetadata iteratorMetadata) {
         super(keys, iteratorMetadata);
@@ -41,7 +41,6 @@ public class ObjectConstructorRuntimeIterator extends LocalRuntimeIterator {
         this._keys = keys;
         this._values = values;
     }
-
 
     public ObjectConstructorRuntimeIterator(List<ObjectConstructorRuntimeIterator> childExpressions,
                                             IteratorMetadata iteratorMetadata) {
@@ -52,55 +51,66 @@ public class ObjectConstructorRuntimeIterator extends LocalRuntimeIterator {
 
     @Override
     public ObjectItem next() {
-        if (this._hasNext) {
-            List<Item> values = new ArrayList<>();
-            List<String> keys = new ArrayList<>();
-            if (_isMergedObject) {
-                for (RuntimeIterator iterator : this._children) {
-                    iterator.open(_currentDynamicContext);
-                    ObjectItem item = (ObjectItem) iterator.next();
-                    iterator.close();
-                    keys.addAll(item.getKeys());
-                    values.addAll(item.getValues());
-                }
-                this._hasNext = false;
-                return new ObjectItem(keys, values, ItemMetadata.fromIteratorMetadata(getMetadata()));
-
-            } else {
-
-                for (RuntimeIterator valueIterator : this._values) {
-                    List<Item> currentResults = new ArrayList<>();
-                    valueIterator.open(this._currentDynamicContext);
-                    while (valueIterator.hasNext())
-                        currentResults.add(valueIterator.next());
-//                    if(valueIterator.hasNext())
-//                        throw new IteratorFlowException("Object value must return one item!");
-                    valueIterator.close();
-                    //SIMILAR TO ZORBA, if value is more than one item, wrap it in an array
-                    if (currentResults.size() > 1)
-                        values.add(new ArrayItem(currentResults, ItemMetadata.fromIteratorMetadata(getMetadata())));
-                    else
-                        values.add(currentResults.get(0));
-                }
-
-                for (RuntimeIterator keyIterator : this._keys) {
-                    keyIterator.open(this._currentDynamicContext);
-                    try {
-                        keys.add(((StringItem) keyIterator.next()).getStringValue());
-                    } catch (Exception ex) {
-                        throw new IteratorFlowException("Object must have string keys!", getMetadata());
-                    }
-                    if (keyIterator.hasNext())
-                        throw new IteratorFlowException("Object value must return one item!", getMetadata());
-                    keyIterator.close();
-                }
-                this._hasNext = false;
-                return new ObjectItem(keys, values, ItemMetadata.fromIteratorMetadata(getMetadata()));
-            }
+        if (this.hasNext()) {
+            this._hasNext = false;
+            return result;
         }
         throw new IteratorFlowException("Invalid next() call on object!", getMetadata());
     }
 
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+
+        List<Item> values = new ArrayList<>();
+        List<String> keys = new ArrayList<>();
+        if (_isMergedObject) {
+            for (RuntimeIterator iterator : this._children) {
+                iterator.open(_currentDynamicContext);
+                ObjectItem item = (ObjectItem) iterator.next();
+                iterator.close();
+                keys.addAll(item.getKeys());
+                values.addAll(item.getValues());
+            }
+            this.result = new ObjectItem(keys, values, ItemMetadata.fromIteratorMetadata(getMetadata()));
+
+        } else {
+
+            for (RuntimeIterator valueIterator : this._values) {
+                List<Item> currentResults = new ArrayList<>();
+                valueIterator.open(this._currentDynamicContext);
+                while (valueIterator.hasNext())
+                    currentResults.add(valueIterator.next());
+//                    if(valueIterator.hasNext())
+//                        throw new IteratorFlowException("Object value must return one item!");
+                valueIterator.close();
+                //SIMILAR TO ZORBA, if value is more than one item, wrap it in an array
+                if (currentResults.size() > 1)
+                    values.add(new ArrayItem(currentResults, ItemMetadata.fromIteratorMetadata(getMetadata())));
+                else
+                    values.add(currentResults.get(0));
+            }
+
+            for (RuntimeIterator keyIterator : this._keys) {
+                keyIterator.open(this._currentDynamicContext);
+                try {
+                    keys.add(((StringItem) keyIterator.next()).getStringValue());
+                } catch (Exception ex) {
+                    throw new IteratorFlowException("Object must have string keys!", getMetadata());
+                }
+                if (keyIterator.hasNext())
+                    throw new IteratorFlowException("Object value must return one item!", getMetadata());
+                keyIterator.close();
+            }
+            this.result = new ObjectItem(keys, values, ItemMetadata.fromIteratorMetadata(getMetadata()));
+        }
+        this._hasNext = true;
+    }
+
+    private ObjectItem result;
     private List<RuntimeIterator> _keys;
     private List<RuntimeIterator> _values;
     private boolean _isMergedObject = false;
