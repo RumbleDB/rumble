1d0056f31ae7088f8d87c69cb391f806a1b05677
canberker <canberkerwork@gmail.com>
2018-08-26 11:56:24 +0200

    override open for object-descendants

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/object/ObjectDescendantFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/object/ObjectDescendantFunctionIterator.java
@@ -6,6 +6,7 @@ import sparksoniq.jsoniq.item.ObjectItem;
 import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import javax.naming.OperationNotSupportedException;
 import java.util.ArrayList;
@@ -19,17 +20,30 @@ public class ObjectDescendantFunctionIterator extends ObjectFunctionIterator {
     @Override
     public Item next() {
         if (this._hasNext) {
-            if (results == null) {
-                _currentIndex = 0;
-                results = new ArrayList<>();
-                RuntimeIterator sequenceIterator = this._children.get(0);
-                List<Item> items = getItemsFromIteratorWithCurrentContext(sequenceIterator);
-                getDescendantObjects(items);
-            }
             return getResult();
         }
         throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " DESCENDANT-OBJECTS function",
                 getMetadata());
+    }
+
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+        _currentIndex = 0;
+        results = new ArrayList<>();
+
+        RuntimeIterator sequenceIterator = this._children.get(0);
+        List<Item> items = getItemsFromIteratorWithCurrentContext(sequenceIterator);
+        getDescendantObjects(items);
+
+        if (results.size() == 0) {
+            this._hasNext = false;
+        } else {
+            this._hasNext = true;
+        }
     }
 
     public void getDescendantObjects(List<Item> items) {
