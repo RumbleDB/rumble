97c2fa608c584ea59934dc3d39af43f04bcdfda8
canberker <canberkerwork@gmail.com>
2018-08-26 12:12:10 +0200

    override open for values

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/object/ObjectValuesFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/object/ObjectValuesFunctionIterator.java
@@ -5,6 +5,7 @@ import sparksoniq.jsoniq.item.Item;
 import sparksoniq.jsoniq.item.ObjectItem;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +18,29 @@ public class ObjectValuesFunctionIterator extends ObjectFunctionIterator {
     @Override
     public Item next() {
         if (this._hasNext) {
-            if (results == null) {
-                _currentIndex = 0;
-                results = new ArrayList<>();
-                RuntimeIterator objectIterator = this._children.get(0);
-                ObjectItem object = getSingleItemOfTypeFromIterator(objectIterator, ObjectItem.class);
-                for (Item item : object.getValues())
-                    results.add(item);
-            }
             return getResult();
         }
         throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " VALUES function",
                 getMetadata());
+    }
+
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+        _currentIndex = 0;
+        results = new ArrayList<>();
+        RuntimeIterator objectIterator = this._children.get(0);
+        ObjectItem object = getSingleItemOfTypeFromIterator(objectIterator, ObjectItem.class);
+        for (Item item : object.getValues())
+            results.add(item);
+
+        if (results.size() == 0) {
+            this._hasNext = false;
+        } else {
+            this._hasNext = true;
+        }
     }
 }
