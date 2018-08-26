4399efcc6e9009e78eb0f7621e59f917520114dd
canberker <canberkerwork@gmail.com>
2018-08-26 14:40:43 +0200

    override open for atan2

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/numerics/trigonometric/ATan2FunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/numerics/trigonometric/ATan2FunctionIterator.java
@@ -9,6 +9,7 @@ import sparksoniq.jsoniq.runtime.iterator.EmptySequenceIterator;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.functions.base.LocalFunctionCallIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.util.List;
 
@@ -19,24 +20,35 @@ public class ATan2FunctionIterator extends LocalFunctionCallIterator {
 
     @Override
     public Item next() {
-        if (this._hasNext) {
-            Item y = this.getSingleItemOfTypeFromIterator(this._children.get(0), Item.class);
-            Item x = this.getSingleItemOfTypeFromIterator(this._children.get(1), Item.class);
-            if (Item.isNumeric(y) && Item.isNumeric(x)) {
-                Double result = Math.atan2(Item.getNumericValue(y, Double.class)
-                        , Item.getNumericValue(x, Double.class));
-                this._hasNext = false;
-                return new DoubleItem(result,
-                        ItemMetadata.fromIteratorMetadata(getMetadata()));
-            }
-            else {
-                throw new UnexpectedTypeException("ATan2 expression has non numeric args " +
-                        y.serialize() + ", " + x.serialize(), getMetadata());
-
-            }
+        if (this.hasNext()) {
+            this._hasNext = false;
+            return result;
         } else
             throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " atan2 function", getMetadata());
     }
 
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
 
+        Item y = this.getSingleItemOfTypeFromIterator(this._children.get(0), Item.class);
+        Item x = this.getSingleItemOfTypeFromIterator(this._children.get(1), Item.class);
+        if (Item.isNumeric(y) && Item.isNumeric(x)) {
+            Double result = Math.atan2(Item.getNumericValue(y, Double.class)
+                    , Item.getNumericValue(x, Double.class));
+            this._hasNext = true;
+            this.result = new DoubleItem(result,
+                    ItemMetadata.fromIteratorMetadata(getMetadata()));
+        }
+        else {
+            throw new UnexpectedTypeException("ATan2 expression has non numeric args " +
+                    y.serialize() + ", " + x.serialize(), getMetadata());
+
+        }
+    }
+
+    Item result;
 }
