c79e5d05aee0c7b9dda2c50464395ef460276113
canberker <canberkerwork@gmail.com>
2018-08-26 15:32:13 +0200

    override open for substring

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/strings/SubstringFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/strings/SubstringFunctionIterator.java
@@ -8,6 +8,7 @@ import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.functions.base.LocalFunctionCallIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.util.List;
 
@@ -18,23 +19,34 @@ public class SubstringFunctionIterator extends LocalFunctionCallIterator {
 
     @Override
     public Item next() {
-        if (this._hasNext) {
-            String result;
-            StringItem stringItem = this.getSingleItemOfTypeFromIterator(this._children.get(0), StringItem.class);
-            IntegerItem indexItem = this.getSingleItemOfTypeFromIterator(this._children.get(1), IntegerItem.class);
-            int index = sanitizeIndexItem(indexItem);
-            if (this._children.size() > 2) {
-                IntegerItem endIndexItem = this.getSingleItemOfTypeFromIterator(this._children.get(2), IntegerItem.class);
-                int endIndex = sanitizeEndIndex(stringItem, endIndexItem, index);
-                result = stringItem.getStringValue().substring(index, endIndex);
-            } else {
-                result = stringItem.getStringValue().substring(index);
-            }
-
+        if (this.hasNext()) {
             this._hasNext = false;
-            return new StringItem(result, ItemMetadata.fromIteratorMetadata(getMetadata()));
+            return result;
         } else
             throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " substring function", getMetadata());
+    }
+
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+
+        String result;
+        StringItem stringItem = this.getSingleItemOfTypeFromIterator(this._children.get(0), StringItem.class);
+        IntegerItem indexItem = this.getSingleItemOfTypeFromIterator(this._children.get(1), IntegerItem.class);
+        int index = sanitizeIndexItem(indexItem);
+        if (this._children.size() > 2) {
+            IntegerItem endIndexItem = this.getSingleItemOfTypeFromIterator(this._children.get(2), IntegerItem.class);
+            int endIndex = sanitizeEndIndex(stringItem, endIndexItem, index);
+            result = stringItem.getStringValue().substring(index, endIndex);
+        } else {
+            result = stringItem.getStringValue().substring(index);
+        }
+
+        this.result = new StringItem(result, ItemMetadata.fromIteratorMetadata(getMetadata()));
+        this._hasNext = true;
     }
 
     private int sanitizeEndIndex(StringItem stringItem, IntegerItem endIndexItem, int startIndex) {
@@ -46,4 +58,6 @@ public class SubstringFunctionIterator extends LocalFunctionCallIterator {
         //char indexing starts from 1 in JSONiq
         return indexItem.getIntegerValue() - 1 > 0 ? indexItem.getIntegerValue() - 1 : 0;
     }
+
+    private Item result;
 }
