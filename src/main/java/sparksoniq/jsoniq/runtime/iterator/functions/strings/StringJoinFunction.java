ad4fda49603bf9eee3e39632c68f644e39cd2ed9
canberker <canberkerwork@gmail.com>
2018-08-26 15:30:36 +0200

    override open for stringjoin

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/strings/StringJoinFunction.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/strings/StringJoinFunction.java
@@ -8,6 +8,7 @@ import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.functions.base.LocalFunctionCallIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.util.List;
 
@@ -18,21 +19,35 @@ public class StringJoinFunction extends LocalFunctionCallIterator {
 
     @Override
     public Item next() {
-        if (this._hasNext) {
-            StringItem joinString = new StringItem("", new ItemMetadata(getMetadata().getExpressionMetadata()));
-            List<Item> strings = getItemsFromIteratorWithCurrentContext(this._children.get(0));
-            if (this._children.size() > 1)
-                joinString = getSingleItemOfTypeFromIterator(this._children.get(1), StringItem.class);
-            StringBuilder stringBuilder = new StringBuilder("");
-            for (Item item : strings) {
-                if (!(item instanceof StringItem))
-                    throw new UnexpectedTypeException("String item expected", this._children.get(0).getMetadata());
-                stringBuilder = !stringBuilder.toString().isEmpty() ? stringBuilder.append(joinString.getStringValue()) : stringBuilder;
-                stringBuilder = stringBuilder.append(((StringItem) item).getStringValue());
-            }
+        if (this.hasNext()) {
             this._hasNext = false;
-            return new StringItem(stringBuilder.toString(), ItemMetadata.fromIteratorMetadata(getMetadata()));
+            return result;
         } else
             throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " string-join function", getMetadata());
     }
+
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+
+        StringItem joinString = new StringItem("", new ItemMetadata(getMetadata().getExpressionMetadata()));
+        List<Item> strings = getItemsFromIteratorWithCurrentContext(this._children.get(0));
+        if (this._children.size() > 1)
+            joinString = getSingleItemOfTypeFromIterator(this._children.get(1), StringItem.class);
+        StringBuilder stringBuilder = new StringBuilder("");
+        for (Item item : strings) {
+            if (!(item instanceof StringItem))
+                throw new UnexpectedTypeException("String item expected", this._children.get(0).getMetadata());
+            stringBuilder = !stringBuilder.toString().isEmpty() ? stringBuilder.append(joinString.getStringValue()) : stringBuilder;
+            stringBuilder = stringBuilder.append(((StringItem) item).getStringValue());
+        }
+        this.result = new StringItem(stringBuilder.toString(), ItemMetadata.fromIteratorMetadata(getMetadata()));
+        this._hasNext = true;
+    }
+
+    private Item result;
+
 }
