988073d81c81234abfb68fe769c1c8bb4cafb04c
canberker <canberkerwork@gmail.com>
2018-08-26 12:47:37 +0200

    override open for ceiling fn.

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/numerics/CeilingFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/numerics/CeilingFunctionIterator.java
@@ -9,6 +9,7 @@ import sparksoniq.jsoniq.runtime.iterator.EmptySequenceIterator;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.functions.base.LocalFunctionCallIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.util.List;
 
@@ -20,27 +21,37 @@ public class CeilingFunctionIterator extends LocalFunctionCallIterator {
     @Override
     public Item next() {
         if (this._hasNext) {
-            RuntimeIterator iterator = this._children.get(0);
-            //TODO refactor empty items
-            if (iterator.getClass() == EmptySequenceIterator.class) {
-                return null;
-            }
-            else {
-                Item value = this.getSingleItemOfTypeFromIterator(iterator, Item.class);
-                if (Item.isNumeric(value)) {
-                    Double result = Math.ceil(Item.getNumericValue(value, Double.class));
-                    this._hasNext = false;
-                    return new DoubleItem(result,
-                            ItemMetadata.fromIteratorMetadata(getMetadata()));
-                }
-                else {
-                    throw new UnexpectedTypeException("Ceiling expression has non numeric args " +
-                            value.serialize(), getMetadata());
-                }
-            }
+            this._hasNext = false;
+            return result;
         } else
             throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " ceiling function", getMetadata());
     }
 
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
 
+        RuntimeIterator iterator = this._children.get(0);
+        if (iterator.getClass() == EmptySequenceIterator.class) {
+            this._hasNext = false;
+        }
+        else {
+            Item value = this.getSingleItemOfTypeFromIterator(iterator, Item.class);
+            if (Item.isNumeric(value)) {
+                Double result = Math.ceil(Item.getNumericValue(value, Double.class));
+                this._hasNext = true;
+                this.result = new DoubleItem(result,
+                        ItemMetadata.fromIteratorMetadata(getMetadata()));
+            }
+            else {
+                throw new UnexpectedTypeException("Ceiling expression has non numeric args " +
+                        value.serialize(), getMetadata());
+            }
+        }
+    }
+
+    Item result;
 }
