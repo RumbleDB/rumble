164d463c10495e6569b4893404f5c961a4b6e57a
canberker <canberkerwork@gmail.com>
2018-08-26 12:51:41 +0200

    override open for pi fn.

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/numerics/PiFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/numerics/PiFunctionIterator.java
@@ -9,6 +9,7 @@ import sparksoniq.jsoniq.runtime.iterator.EmptySequenceIterator;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.functions.base.LocalFunctionCallIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.util.List;
 
@@ -19,13 +20,23 @@ public class PiFunctionIterator extends LocalFunctionCallIterator {
 
     @Override
     public Item next() {
-        if (this._hasNext) {
+        if (this.hasNext()) {
             this._hasNext = false;
-            return new DoubleItem(Math.PI,
-                    ItemMetadata.fromIteratorMetadata(getMetadata()));
+            return result;
         } else
             throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " pi function", getMetadata());
     }
 
+    @Override
+    public void open(DynamicContext context) {
+        if (this._isOpen)
+            throw new IteratorFlowException("Runtime iterator cannot be opened twice", getMetadata());
+        this._isOpen = true;
+        this._currentDynamicContext = context;
+        this._hasNext = true;
+        this.result = new DoubleItem(Math.PI,
+                ItemMetadata.fromIteratorMetadata(getMetadata()));
+    }
 
+    Item result;
 }
