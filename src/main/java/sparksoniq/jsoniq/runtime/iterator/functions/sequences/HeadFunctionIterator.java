bbf50bd025f08f3f893388b5ed11613dfddc3828
canberker <canberkerwork@gmail.com>
2018-08-29 16:21:26 +0200

    implement head fn. with lazy eval.

---
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/sequences/HeadFunctionIterator.java
@@ -0,0 +1,47 @@
+package sparksoniq.jsoniq.runtime.iterator.functions.sequences;
+
+import com.sun.javafx.scene.control.ReadOnlyUnbackedObservableList;
+import sparksoniq.exceptions.IteratorFlowException;
+import sparksoniq.jsoniq.item.BooleanItem;
+import sparksoniq.jsoniq.item.Item;
+import sparksoniq.jsoniq.item.metadata.ItemMetadata;
+import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
+import sparksoniq.jsoniq.runtime.iterator.functions.base.LocalFunctionCallIterator;
+import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
+
+import java.util.List;
+
+public class HeadFunctionIterator extends LocalFunctionCallIterator {
+
+    private RuntimeIterator _iterator;
+    private Item _result;
+
+    public HeadFunctionIterator(List<RuntimeIterator> parameters, IteratorMetadata iteratorMetadata) {
+        super(parameters, iteratorMetadata);
+    }
+
+    @Override
+    public Item next() {
+        if (this.hasNext()) {
+            this._hasNext = false;
+            return _result;
+        }
+        throw new IteratorFlowException(FLOW_EXCEPTION_MESSAGE + "head function", getMetadata());
+    }
+
+    @Override
+    public void open(DynamicContext context) {
+        super.open(context);
+
+        _iterator = this._children.get(0);
+        _iterator.open(context);
+        if (_iterator.hasNext()) {
+            this._hasNext = true;
+            _result = _iterator.next();
+        } else {
+            this._hasNext = false;
+        }
+        _iterator.close();
+    }
+}
