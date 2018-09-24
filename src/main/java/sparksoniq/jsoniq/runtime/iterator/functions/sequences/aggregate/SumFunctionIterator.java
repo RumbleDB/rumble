582e14570d9e320aba30d4b17571c1876f5f1c1c
canberker <canberkerwork@gmail.com>
2018-09-25 00:08:59 +0200

    implement lazy evaluation and empty sequence support for sum, add tests

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/sequences/aggregate/SumFunctionIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/sequences/aggregate/SumFunctionIterator.java
@@ -1,33 +1,50 @@
 package sparksoniq.jsoniq.runtime.iterator.functions.sequences.aggregate;
 
 import sparksoniq.exceptions.IteratorFlowException;
+import sparksoniq.exceptions.UnexpectedTypeException;
 import sparksoniq.jsoniq.item.DecimalItem;
 import sparksoniq.jsoniq.item.Item;
 import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.DynamicContext;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public class SumFunctionIterator extends AggregateFunctionIterator {
+
+    private RuntimeIterator _iterator;
+
     public SumFunctionIterator(List<RuntimeIterator> arguments, IteratorMetadata iteratorMetadata) {
         super(arguments, AggregateFunctionOperator.SUM, iteratorMetadata);
     }
 
     @Override
+    public void open(DynamicContext context) {
+        super.open(context);
+
+        _iterator = this._children.get(0);
+        _iterator.open(_currentDynamicContext);
+        if (_iterator.hasNext()) {
+            this._hasNext = true;
+        } else {
+            this._hasNext = false;
+        }
+        _iterator.close();
+    }
+
+    @Override
     public Item next() {
         if (this._hasNext) {
-            RuntimeIterator sequenceIterator = this._children.get(0);
-            List<Item> results = getItemsFromIteratorWithCurrentContext(sequenceIterator);
+            List<Item> results = getItemsFromIteratorWithCurrentContext(_iterator);
             this._hasNext = false;
             results.forEach(r -> {
                 if (!Item.isNumeric(r))
-                    throw new IllegalArgumentException("Aggregate function argument is non numeric");
+                    throw new UnexpectedTypeException("Sum expression has non numeric args " +
+                            r.serialize(), getMetadata());
             });
-            //TODO refactor empty items
-            if (results.size() == 0)
-                return null;
+
             BigDecimal sumResult = new BigDecimal(0);
             for (Item r : results) {
                 BigDecimal current = Item.getNumericValue(r, BigDecimal.class);
