7c4286b16c78ec2a1461fa336e4b6294820fcbae
canberker <canberkerwork@gmail.com>
2018-11-30 17:36:28 +0100

    Implement better warning check

--- src/main/java/sparksoniq/spark/iterator/SparkRuntimeIterator.java
+++ src/main/java/sparksoniq/spark/iterator/SparkRuntimeIterator.java
@@ -64,11 +64,10 @@ public abstract class SparkRuntimeIterator extends RuntimeIterator {
 
         if(result == null){
             currentResultIndex = 0;
-            this._rdd = this.getRDD().persist(StorageLevel.MEMORY_ONLY());
+            this._rdd = this.getRDD();
             if(SparkContextManager.LIMIT_COLLECT()) {
-                long count = _rdd.count();
                 result = _rdd.take(SparkContextManager.COLLECT_ITEM_LIMIT);
-                if (count > SparkContextManager.COLLECT_ITEM_LIMIT) {
+                if (result.size() == SparkContextManager.COLLECT_ITEM_LIMIT) {
                     ShellStart.terminal.output("\nWarning: Results have been truncated to: " + SparkContextManager.COLLECT_ITEM_LIMIT
                             + " items. This value can be configured with the --result-size parameter at startup.\n");
                 }
