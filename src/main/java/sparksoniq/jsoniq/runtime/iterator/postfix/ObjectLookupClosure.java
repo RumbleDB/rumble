be65bcb57b643b03384024c93fbaf6b9ba33b43a
Ghislain Fourny <gfourny@inf.ethz.ch>
2018-12-13 11:27:39 +0100

    Fix lookup.

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/postfix/ObjectLookupClosure.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/postfix/ObjectLookupClosure.java
@@ -1,6 +1,7 @@
 package sparksoniq.jsoniq.runtime.iterator.postfix;
 
 import sparksoniq.jsoniq.item.Item;
+import sparksoniq.jsoniq.item.ObjectItem;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -17,6 +18,10 @@ public class ObjectLookupClosure implements FlatMapFunction<Item, Item> {
 
     public Iterator<Item> call(Item arg0) throws Exception {
         List<Item> results = new ArrayList<Item>();
+
+        if (!(arg0 instanceof ObjectItem))
+            return results.iterator();
+
         Item item = arg0.getItemByKey(_key);
         if(item != null)
         {
