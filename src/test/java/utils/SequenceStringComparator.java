a42acb467e27df8bf3fa5e2df63792a7a4d5654c
canberker <canberkerwork@gmail.com>
2018-12-14 19:19:14 +0100

    Improve sequence comparison

--- src/test/java/utils/SequenceStringComparator.java
+++ src/test/java/utils/SequenceStringComparator.java
@@ -8,6 +8,9 @@ public class SequenceStringComparator {
     public static boolean unorderedItemSequenceStringsAreEqual(String sequence1, String sequence2) {
         List<String> sequence1Items = Arrays.asList(sequence1.split(","));
         List<String> sequence2Items = Arrays.asList(sequence2.split(","));
+        if (sequence1Items.size() != sequence2Items.size()) {
+            return false;
+        }
         for (String sequence1Item : sequence1Items) {
             if (!sequence2Items.stream().anyMatch(s2 -> s2.replace(" ", "").replace("(", "").replace(")", "")
                     .equals(sequence1Item.replace(" ", "").replace("(", "").replace(")", ""))))
