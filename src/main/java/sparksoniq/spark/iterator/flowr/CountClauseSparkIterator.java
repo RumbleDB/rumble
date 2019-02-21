d74cb644d31b62476b8bf27cf5a496f60a32b2db
canberker <canberkerwork@gmail.com>
2019-02-21 09:57:22 +0100

    Remove rdd caching from CountClause.

--- src/main/java/sparksoniq/spark/iterator/flowr/CountClauseSparkIterator.java
+++ src/main/java/sparksoniq/spark/iterator/flowr/CountClauseSparkIterator.java
@@ -1,7 +1,6 @@
 package sparksoniq.spark.iterator.flowr;
 
 import org.apache.spark.api.java.JavaRDD;
-import sparksoniq.jsoniq.compiler.translator.expr.flowr.FLWOR_CLAUSES;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.primary.VariableReferenceIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
@@ -10,12 +9,10 @@ import sparksoniq.jsoniq.runtime.tupleiterator.SparkRuntimeTupleIterator;
 import sparksoniq.jsoniq.tuple.FlworTuple;
 import sparksoniq.semantics.DynamicContext;
 import sparksoniq.spark.closures.CountClauseClosure;
-import sparksoniq.spark.iterator.flowr.base.FlowrClauseSparkIterator;
-
-import java.util.Collections;
 
 public class CountClauseSparkIterator extends SparkRuntimeTupleIterator {
     VariableReferenceIterator _variableReference;
+
     public CountClauseSparkIterator(RuntimeTupleIterator child, RuntimeIterator variableReference, IteratorMetadata iteratorMetadata) {
         super(child, iteratorMetadata);
         _variableReference = (VariableReferenceIterator) variableReference;
@@ -23,12 +20,8 @@ public class CountClauseSparkIterator extends SparkRuntimeTupleIterator {
 
     @Override
     public JavaRDD<FlworTuple> getRDD(DynamicContext context) {
-        if (this._rdd == null) {
-            this._rdd = _child.getRDD(context);
-            String variableName = _variableReference.getVariableName();
-            // zipWithIndex starts from 0, increment indices by 1 for jsoniq convention
-            this._rdd = this._rdd.zipWithIndex().mapValues(index -> index + 1).map(new CountClauseClosure(variableName, getMetadata()));
-        }
-        return _rdd;
+        String variableName = _variableReference.getVariableName();
+        // zipWithIndex starts from 0, increment indices by 1 for jsoniq convention
+        return _child.getRDD(context).zipWithIndex().mapValues(index -> index + 1).map(new CountClauseClosure(variableName, getMetadata()));
     }
 }
