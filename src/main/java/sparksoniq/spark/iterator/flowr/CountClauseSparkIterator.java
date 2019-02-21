2ff3922d15e7e77f0509337a6e1b2efbb4d42502
canberker <canberkerwork@gmail.com>
2019-02-21 10:28:50 +0100

    Implement local countClause.

--- src/main/java/sparksoniq/spark/iterator/flowr/CountClauseSparkIterator.java
+++ src/main/java/sparksoniq/spark/iterator/flowr/CountClauseSparkIterator.java
@@ -1,6 +1,10 @@
 package sparksoniq.spark.iterator.flowr;
 
 import org.apache.spark.api.java.JavaRDD;
+import sparksoniq.exceptions.IteratorFlowException;
+import sparksoniq.exceptions.SparksoniqRuntimeException;
+import sparksoniq.jsoniq.item.IntegerItem;
+import sparksoniq.jsoniq.item.Item;
 import sparksoniq.jsoniq.runtime.iterator.RuntimeIterator;
 import sparksoniq.jsoniq.runtime.iterator.primary.VariableReferenceIterator;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
@@ -10,18 +14,73 @@ import sparksoniq.jsoniq.tuple.FlworTuple;
 import sparksoniq.semantics.DynamicContext;
 import sparksoniq.spark.closures.CountClauseClosure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CountClauseSparkIterator extends SparkRuntimeTupleIterator {
-    VariableReferenceIterator _variableReference;
+    private VariableReferenceIterator _variableReference;
+    private FlworTuple _nextLocalTupleResult;
+    private int _currentCountIndex;
 
     public CountClauseSparkIterator(RuntimeTupleIterator child, RuntimeIterator variableReference, IteratorMetadata iteratorMetadata) {
         super(child, iteratorMetadata);
         _variableReference = (VariableReferenceIterator) variableReference;
+        _currentCountIndex = 1 ;    // indices start at 1 in JSONiq
     }
+
+    @Override
+    public boolean isRDD() {
+        return _child.isRDD();
+    }
+
+    @Override
+    public void open(DynamicContext context) {
+        super.open(context);
+
+        // isRDD checks omitted, as open is used for non-RDD(local) operations
+
+        if (this._child != null) {
+            _child.open(_currentDynamicContext);
+
+            setNextLocalTupleResult();
+        } else {
+            throw new SparksoniqRuntimeException("Invalid count clause.");
+        }
+    }
+
+    @Override
+    public FlworTuple next() {
+        if(_hasNext == true){
+            FlworTuple result = _nextLocalTupleResult;      // save the result to be returned
+            setNextLocalTupleResult();              // calculate and store the next result
+            return result;
+        }
+        throw new IteratorFlowException("Invalid next() call in count flwor clause", getMetadata());
+    }
+
+    private void setNextLocalTupleResult() {
+        if (_child.hasNext()) {
+            FlworTuple inputTuple = _child.next();
+
+            List<Item> results = new ArrayList<>();
+            results.add(new IntegerItem(_currentCountIndex++));
+
+            FlworTuple newTuple = new FlworTuple(inputTuple, _variableReference.getVariableName(), results);
+            _nextLocalTupleResult = newTuple;
+            this._hasNext = true;
+        } else {
+            _child.close();
+            this._hasNext = false;
+        }
+    }
+
 
     @Override
     public JavaRDD<FlworTuple> getRDD(DynamicContext context) {
         String variableName = _variableReference.getVariableName();
         // zipWithIndex starts from 0, increment indices by 1 for jsoniq convention
-        return _child.getRDD(context).zipWithIndex().mapValues(index -> index + 1).map(new CountClauseClosure(variableName, getMetadata()));
+        return _child.getRDD(context).zipWithIndex()
+                .mapValues(index -> index + 1)
+                .map(new CountClauseClosure(variableName, getMetadata()));
     }
 }
