28d567638f7d1da5e6700ae233713039261ff2f7
Can Cikis <can@atfinity.ch>
2019-03-10 17:22:58 +0100

    Implement kryo pooling for multithreading support.

--- src/main/java/sparksoniq/spark/closures/ClosureUtils.java
+++ src/main/java/sparksoniq/spark/closures/ClosureUtils.java
@@ -9,30 +9,54 @@ import sparksoniq.jsoniq.item.KryoManager;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClosureUtils {
 
+    private static KryoManager KM = KryoManager.getInstance();
+
     public static byte[] serializeItemList(List<Item> toSerialize) {
-        Kryo kryo = KryoManager.getInstance().getOrCreateKryo();
+        Kryo kryo = KM.getOrCreateKryo();
 
         Output output = new Output(new ByteArrayOutputStream());
         kryo.writeClassAndObject(output, toSerialize);
         output.close();
 
         byte[] byteArray = output.getBuffer();
+
+        KM.releaseKryoInstance(kryo);
+
         return byteArray;
     }
 
-    public static Object deserializeRow(Row row) {
-        Input input = new Input(new ByteArrayInputStream((byte[])row.get(0)));
+    public static Object deserializeRowColumn(Row row, int columnIndex) {
+        Kryo kryo = KM.getOrCreateKryo();
 
-        Kryo kryo = KryoManager.getInstance().getOrCreateKryo();
+        Input input = new Input(new ByteArrayInputStream((byte[])row.get(columnIndex)));
 
         Object obj = kryo.readClassAndObject(input);
         input.close();
 
+        KM.releaseKryoInstance(kryo);
+
         return obj;
+    }
+
+
+    public static List<Object> deserializeEntireRow(Row row) {
+        Kryo kryo = KM.getOrCreateKryo();
+
+        ArrayList<Object> deserializedColumnObjects = new ArrayList<>();
+        for (int columnIndex = 0; columnIndex < row.length(); columnIndex++) {
+            Input input = new Input(new ByteArrayInputStream((byte[])row.get(columnIndex)));
+            Object deserializedColumnObject = kryo.readClassAndObject(input);
+            deserializedColumnObjects.add(deserializedColumnObject);
+            input.close();
+        }
+
+        KM.releaseKryoInstance(kryo);
+        return deserializedColumnObjects;
     }
 
 }
