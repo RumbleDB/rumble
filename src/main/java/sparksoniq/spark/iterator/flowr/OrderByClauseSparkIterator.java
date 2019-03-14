d880af33109b4556aa82ef94f58455ec97220648
Can Cikis <canberkerwork@gmail.com>
2019-03-15 00:12:18 +0100

    Fix tests for dataframe implementation.

--- src/main/java/sparksoniq/spark/iterator/flowr/OrderByClauseSparkIterator.java
+++ src/main/java/sparksoniq/spark/iterator/flowr/OrderByClauseSparkIterator.java
@@ -62,6 +62,13 @@ public class OrderByClauseSparkIterator extends SparkRuntimeTupleIterator {
     }
 
     @Override
+    public boolean isDataFrame() {
+        // TODO implement letclause and remove the following return statement
+        return false;
+        // return _child.isDataFrame();
+    }
+
+    @Override
     public void open(DynamicContext context) {
         super.open(context);
 
