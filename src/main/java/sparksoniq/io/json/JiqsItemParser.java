39506c05598455d7e1c981b929afd771ffee316a
Ghislain Fourny <gfourny@inf.ethz.ch>
2019-07-04 17:06:30 +0200

    Switch to JsonIter.

--- src/main/java/sparksoniq/io/json/JiqsItemParser.java
+++ src/main/java/sparksoniq/io/json/JiqsItemParser.java
@@ -22,11 +22,17 @@ package sparksoniq.io.json;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import com.jsoniter.JsonIterator;
+import com.jsoniter.ValueType;
+import com.jsoniter.any.Any;
+
 import sparksoniq.jsoniq.item.Item;
 import sparksoniq.jsoniq.item.ItemFactory;
 import sparksoniq.jsoniq.item.metadata.ItemMetadata;
 import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -35,41 +41,51 @@ import java.util.List;
 
 public class JiqsItemParser implements Serializable {
 
-    public static Item getItemFromObject(Object object, IteratorMetadata metadata) {
-        if (object != null) {
-            if (object instanceof String)
-                return ItemFactory.getInstance().createStringItem(object.toString());
-            if (object instanceof Integer)
-                return ItemFactory.getInstance().createIntegerItem((int) object);
-            if (object instanceof Double)
-                return ItemFactory.getInstance().createDoubleItem((double) object);
-            if (object instanceof BigDecimal)
-                return ItemFactory.getInstance().createDecimalItem(new BigDecimal(object.toString()));
-            if (object instanceof Boolean)
-                return ItemFactory.getInstance().createBooleanItem((boolean) object);
-            if (object instanceof JSONArray) {
-                JSONArray curentArray = (JSONArray) object;
-                int numberOfValues = curentArray.length();
-                List<Item> values = new ArrayList<>(numberOfValues);
-                int index = 0;
-                while (index < numberOfValues)
-                    values.add(getItemFromObject(curentArray.get(index++), metadata));
-                return ItemFactory.getInstance().createArrayItem(values);
+    public static Item getItemFromObject(JsonIterator object, IteratorMetadata metadata) {
+        try {
+            if (object != null) {
+                if (object.whatIsNext().equals(ValueType.STRING))
+                    return ItemFactory.getInstance().createStringItem(object.readString());
+                if (object.whatIsNext().equals(ValueType.NUMBER))
+                {
+                    String number = object.readNumberAsString();
+                    if(number.contains("E") || number.contains("E"))
+                    {
+                        return ItemFactory.getInstance().createDoubleItem(Double.parseDouble(number));
+                    }
+                    if(number.contains("."))
+                    {
+                        return ItemFactory.getInstance().createDecimalItem(new BigDecimal(number));
+                    }
+                    return ItemFactory.getInstance().createIntegerItem(Integer.parseInt(number));
+                }
+                if (object.whatIsNext().equals(ValueType.BOOLEAN))
+                    return ItemFactory.getInstance().createBooleanItem(object.readBoolean());
+                if (object.whatIsNext().equals(ValueType.ARRAY)) {
+                    List<Item> values = new ArrayList<Item>();
+                    while(object.readArray())
+                    {
+                        values.add(getItemFromObject(object, metadata));
+                    }
+                    return ItemFactory.getInstance().createArrayItem(values);
+                }
+                if (object.whatIsNext().equals(ValueType.OBJECT)) {
+                    List<String> keys = new ArrayList<String>();
+                    List<Item> values = new ArrayList<Item>();
+                    String s = null;
+                    while((s = object.readObject()) != null)
+                    {
+                        keys.add(s);
+                        values.add(getItemFromObject(object, metadata));
+                    }
+                    return ItemFactory.getInstance().createObjectItem(keys, values, ItemMetadata.fromIteratorMetadata(metadata));
+                }
+    
             }
-            if (object instanceof JSONObject) {
-                JSONObject currentObject = (JSONObject) object;
-                int numberOfValues = currentObject.length();
-                List<String> keys = new ArrayList<>(numberOfValues);
-                List<Item> values = new ArrayList<>(numberOfValues);
-                Iterator<String> keyIterator = currentObject.keys();
-                while (keyIterator.hasNext())
-                    keys.add(keyIterator.next());
-                keys.forEach(_key -> values.add(getItemFromObject(currentObject.get(_key), metadata)));
-                return ItemFactory.getInstance().createObjectItem(keys, values, ItemMetadata.fromIteratorMetadata(metadata));
-            }
-
+        } catch (IOException e)
+        {
+            return ItemFactory.getInstance().createNullItem();
         }
         return ItemFactory.getInstance().createNullItem();
     }
-
 }
