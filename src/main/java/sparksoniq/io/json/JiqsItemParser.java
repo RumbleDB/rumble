88d073c4554cef6db4d0636498deeb4baec70dfa
Ghislain Fourny <gfourny@inf.ethz.ch>
2019-07-04 17:18:26 +0200

    Fix issues.

--- src/main/java/sparksoniq/io/json/JiqsItemParser.java
+++ src/main/java/sparksoniq/io/json/JiqsItemParser.java
@@ -27,6 +27,7 @@ import com.jsoniter.JsonIterator;
 import com.jsoniter.ValueType;
 import com.jsoniter.any.Any;
 
+import sparksoniq.exceptions.SparksoniqRuntimeException;
 import sparksoniq.jsoniq.item.Item;
 import sparksoniq.jsoniq.item.ItemFactory;
 import sparksoniq.jsoniq.item.metadata.ItemMetadata;
@@ -43,49 +44,50 @@ public class JiqsItemParser implements Serializable {
 
     public static Item getItemFromObject(JsonIterator object, IteratorMetadata metadata) {
         try {
-            if (object != null) {
-                if (object.whatIsNext().equals(ValueType.STRING))
-                    return ItemFactory.getInstance().createStringItem(object.readString());
-                if (object.whatIsNext().equals(ValueType.NUMBER))
+            if (object.whatIsNext().equals(ValueType.STRING))
+                return ItemFactory.getInstance().createStringItem(object.readString());
+            if (object.whatIsNext().equals(ValueType.NUMBER))
+            {
+                String number = object.readNumberAsString();
+                if(number.contains("E") || number.contains("e"))
                 {
-                    String number = object.readNumberAsString();
-                    if(number.contains("E") || number.contains("E"))
-                    {
-                        return ItemFactory.getInstance().createDoubleItem(Double.parseDouble(number));
-                    }
-                    if(number.contains("."))
-                    {
-                        return ItemFactory.getInstance().createDecimalItem(new BigDecimal(number));
-                    }
-                    return ItemFactory.getInstance().createIntegerItem(Integer.parseInt(number));
+                    return ItemFactory.getInstance().createDoubleItem(Double.parseDouble(number));
                 }
-                if (object.whatIsNext().equals(ValueType.BOOLEAN))
-                    return ItemFactory.getInstance().createBooleanItem(object.readBoolean());
-                if (object.whatIsNext().equals(ValueType.ARRAY)) {
-                    List<Item> values = new ArrayList<Item>();
-                    while(object.readArray())
-                    {
-                        values.add(getItemFromObject(object, metadata));
-                    }
-                    return ItemFactory.getInstance().createArrayItem(values);
+                if(number.contains("."))
+                {
+                    return ItemFactory.getInstance().createDecimalItem(new BigDecimal(number));
                 }
-                if (object.whatIsNext().equals(ValueType.OBJECT)) {
-                    List<String> keys = new ArrayList<String>();
-                    List<Item> values = new ArrayList<Item>();
-                    String s = null;
-                    while((s = object.readObject()) != null)
-                    {
-                        keys.add(s);
-                        values.add(getItemFromObject(object, metadata));
-                    }
-                    return ItemFactory.getInstance().createObjectItem(keys, values, ItemMetadata.fromIteratorMetadata(metadata));
-                }
-    
+                return ItemFactory.getInstance().createIntegerItem(Integer.parseInt(number));
             }
+            if (object.whatIsNext().equals(ValueType.BOOLEAN))
+                return ItemFactory.getInstance().createBooleanItem(object.readBoolean());
+            if (object.whatIsNext().equals(ValueType.ARRAY)) {
+                List<Item> values = new ArrayList<Item>();
+                while(object.readArray())
+                {
+                    values.add(getItemFromObject(object, metadata));
+                }
+                return ItemFactory.getInstance().createArrayItem(values);
+            }
+            if (object.whatIsNext().equals(ValueType.OBJECT)) {
+                List<String> keys = new ArrayList<String>();
+                List<Item> values = new ArrayList<Item>();
+                String s = null;
+                while((s = object.readObject()) != null)
+                {
+                    keys.add(s);
+                    values.add(getItemFromObject(object, metadata));
+                }
+                return ItemFactory.getInstance().createObjectItem(keys, values, ItemMetadata.fromIteratorMetadata(metadata));
+            }
+            if (object.whatIsNext().equals(ValueType.NULL)) {
+                object.readNull();
+                return ItemFactory.getInstance().createNullItem();
+            }
+            throw new SparksoniqRuntimeException("Invalid value found while parsing. JSON is not well-formed!");
         } catch (IOException e)
         {
-            return ItemFactory.getInstance().createNullItem();
+            throw new SparksoniqRuntimeException("IO error while parsing. JSON is not well-formed!");
         }
-        return ItemFactory.getInstance().createNullItem();
     }
 }
