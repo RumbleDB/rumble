d9e117fa3aaa344e3bcd40dbdfa99d1b5667e9d3
Ghislain Fourny <gfourny@inf.ethz.ch>
2019-08-27 16:02:24 +0200

    Fix tabs.

--- src/main/java/org/rumbledb/api/RumbleConf.java
+++ src/main/java/org/rumbledb/api/RumbleConf.java
@@ -9,28 +9,28 @@ package org.rumbledb.api;
  *
  */
 public class RumbleConf {
-	private int _resultsSizeCap;
-	
-	public RumbleConf() {
-		_resultsSizeCap = 100;
-	}
-	
-	/**
-	 * Sets the number of Items that should be collected in case of a forced materialization. This applies in particular to a local use of the ItemIterator.
-	 * @param cap the maximum number of Items to collect.
-	 */
-	void setResultsSizeCap(int cap)
-	{
-		_resultsSizeCap = cap;
-	}
+    private int _resultsSizeCap;
 
-	/**
-	 * Gets the configured number of Items that should be collected in case of a forced materialization. This applies in particular to a local use of the ItemIterator.
-	 * @return the current number of Items to collect.
-	 */
-	int getResultsSizeCap()
-	{
-		return _resultsSizeCap;
-	}
+    public RumbleConf() {
+        _resultsSizeCap = 100;
+    }
+
+    /**
+     * Sets the number of Items that should be collected in case of a forced materialization. This applies in particular to a local use of the ItemIterator.
+     * @param cap the maximum number of Items to collect.
+     */
+    void setResultsSizeCap(int cap)
+    {
+        _resultsSizeCap = cap;
+    }
+
+    /**
+     * Gets the configured number of Items that should be collected in case of a forced materialization. This applies in particular to a local use of the ItemIterator.
+     * @return the current number of Items to collect.
+     */
+    int getResultsSizeCap()
+    {
+        return _resultsSizeCap;
+    }
 
 }
