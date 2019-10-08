643005da378edb8a45b9a82838bba364140d7fe4
AndreaRinaldi1 <andrearinaldi1@outlook.com>
2019-10-08 16:40:23 +0200

    Change TreatIterator implementation to avoid materialization. Use streaming fashion instead.

--- src/main/java/sparksoniq/jsoniq/runtime/iterator/operational/TreatIterator.java
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/operational/TreatIterator.java
@@ -16,9 +16,9 @@ import java.util.*;
 public class TreatIterator extends UnaryOperationIterator {
 
     private static final long serialVersionUID = 1L;
-    private List<Item> results;
     private final SequenceType _sequenceType;
-    private int _currentIndex = 0;
+    private Item _nextResult;
+    private int _childIndex;
 
     public TreatIterator(RuntimeIterator child, SequenceType sequenceType, IteratorMetadata iteratorMetadata) {
         super(child, OperationalExpressionBase.Operator.TREAT, iteratorMetadata);
@@ -28,63 +28,51 @@ public class TreatIterator extends UnaryOperationIterator {
     @Override
     public Item next() {
         if (this._hasNext) {
-            return getResult();
+            Item result = _nextResult;
+            setNextResult();
+            return result;
         } else
             throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE, getMetadata());
     }
 
-    public Item getResult() {
-        if (results == null || results.size() == 0) {
-            throw new IteratorFlowException("getResult called on an empty list of results", getMetadata());
-        }
-        if (_currentIndex == results.size() - 1)
-            _hasNext = false;
-        return results.get(_currentIndex++);
-    }
-
-    private void compute_treat_as() {
-        List<Item> items = new ArrayList<>();
-        _child.open(_currentDynamicContext);
-
-        while (_child.hasNext())
-            items.add(_child.next());
-        _child.close();
-
-        //... treat as ()
-        if (!items.isEmpty() && _sequenceType.isEmptySequence())
-            throw new TreatException(" " + ItemTypes.getItemTypeName(items.get(0).getClass().getSimpleName()) + " cannot be treated as type empty-sequence()", getMetadata());
-
+    private void setNextResult() {
         ItemType itemType = _sequenceType.getItemType();
         String sequenceTypeName = ItemTypes.getItemTypeName(itemType.getType().toString());
 
-        //Empty sequence, more items
-        if (items.isEmpty() && (_sequenceType.getArity() == SequenceType.Arity.One ||
-                _sequenceType.getArity() == SequenceType.Arity.OneOrMore)) {
-            throw new TreatException(" Empty sequence cannot be treated as type " + sequenceTypeName + _sequenceType.getArity().getSymbol(), getMetadata());
+        _nextResult = null;
+        if (_child.hasNext()) {
+            _nextResult = _child.next();
+        } else {
+            _child.close();
+            if (_childIndex == 0 && (_sequenceType.getArity() == SequenceType.Arity.One ||
+                    _sequenceType.getArity() == SequenceType.Arity.OneOrMore)) {
+                throw new TreatException(" Empty sequence cannot be treated as type " + sequenceTypeName + _sequenceType.getArity().getSymbol(), getMetadata());
+            }
         }
-        if (items.size() > 1 && (_sequenceType.getArity() == SequenceType.Arity.One ||
+
+        //... treat as ()
+        if (_nextResult != null && _sequenceType.isEmptySequence())
+            throw new TreatException(" " + ItemTypes.getItemTypeName(_nextResult.getClass().getSimpleName()) + " cannot be treated as type empty-sequence()", getMetadata());
+
+        //More items
+        if (_childIndex > 1 && (_sequenceType.getArity() == SequenceType.Arity.One ||
                 _sequenceType.getArity() == SequenceType.Arity.OneOrZero)) {
             throw new TreatException(" Sequences of more than one item cannot be treated as type " + sequenceTypeName + _sequenceType.getArity().getSymbol(), getMetadata());
         }
 
-        results = new ArrayList<>();
-
-        for (Item item : items) {
-            if (item.isTypeOf(itemType)) {
-                results.add(item);
-            } else {
-                results.clear();
-                throw new TreatException(" " + ItemTypes.getItemTypeName(item.getClass().getSimpleName()) + " cannot be treated as type " + sequenceTypeName + _sequenceType.getArity().getSymbol(), getMetadata());
-            }
+        if (_nextResult != null && !_nextResult.isTypeOf(itemType)) {
+            throw new TreatException(" " + ItemTypes.getItemTypeName(_nextResult.getClass().getSimpleName()) + " cannot be treated as type " + sequenceTypeName + _sequenceType.getArity().getSymbol(), getMetadata());
         }
+
+        this._hasNext = _nextResult != null;
+        _childIndex++;
     }
 
     @Override
     public void open(DynamicContext context) {
         super.open(context);
-        this._currentIndex = 0;
-
-        this.compute_treat_as();
-        this._hasNext = results.size() != 0;
+        this._childIndex = 0;
+        _child.open(_currentDynamicContext);
+        this.setNextResult();
     }
 }
