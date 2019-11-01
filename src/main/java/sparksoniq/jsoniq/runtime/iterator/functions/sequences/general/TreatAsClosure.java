79f071ae07c4d2f5720cd3e10dbc946649d3e2eb
AndreaRinaldi1 <andrearinaldi1@outlook.com>
2019-11-01 17:24:20 +0100

    Implementation of hybrid TreatAs iterator

---
+++ src/main/java/sparksoniq/jsoniq/runtime/iterator/functions/sequences/general/TreatAsClosure.java
@@ -0,0 +1,26 @@
+package sparksoniq.jsoniq.runtime.iterator.functions.sequences.general;
+
+import org.apache.spark.api.java.function.Function;
+import org.rumbledb.api.Item;
+import sparksoniq.exceptions.TreatException;
+import sparksoniq.jsoniq.runtime.metadata.IteratorMetadata;
+import sparksoniq.semantics.types.ItemTypes;
+import sparksoniq.semantics.types.SequenceType;
+
+public class TreatAsClosure implements Function<Item, Boolean> {
+    private SequenceType sequenceType;
+    private IteratorMetadata metadata;
+
+    public TreatAsClosure(SequenceType sequenceType, IteratorMetadata metadata) {
+        this.sequenceType = sequenceType;
+        this.metadata = metadata;
+    }
+
+    @Override
+    public Boolean call(Item input) throws Exception {
+        if (input != null && !input.isTypeOf(sequenceType.getItemType()))
+            throw new TreatException(" " + ItemTypes.getItemTypeName(input.getClass().getSimpleName()) + " cannot be treated as type "
+                    + ItemTypes.getItemTypeName(sequenceType.getItemType().getType().toString()) + sequenceType.getArity().getSymbol(), metadata);
+        return true;
+    }
+}
