c432c67214b8cfb546d5a8d693bd2302bb379ee5
Ghislain Fourny <gfourny@inf.ethz.ch>
2019-11-22 17:16:18 +0100

    Also support returning a count directly if the variable is a data frame.

--- src/main/java/sparksoniq/semantics/DynamicContext.java
+++ src/main/java/sparksoniq/semantics/DynamicContext.java
@@ -202,6 +202,9 @@ public class DynamicContext implements Serializable, KryoSerializable {
         if (_localVariableCounts.containsKey(varName)) {
             return _localVariableCounts.get(varName);
         }
+        if (_dfVariableValues.containsKey(varName)) {
+            return ItemFactory.getInstance().createIntegerItem((int) _dfVariableValues.get(varName).count());
+        }
         if (_rddVariableValues.containsKey(varName)) {
             return ItemFactory.getInstance().createIntegerItem((int) _rddVariableValues.get(varName).count());
         }
