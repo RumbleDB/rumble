6cd0689c607cddd4a507d610d65b3a0297242bbc
canberker <canberkerwork@gmail.com>
2019-11-27 19:58:27 +0100

    Fix empty DF handling of order by clause

--- src/main/java/sparksoniq/spark/iterator/flowr/OrderByClauseSparkIterator.java
+++ src/main/java/sparksoniq/spark/iterator/flowr/OrderByClauseSparkIterator.java
@@ -187,6 +187,9 @@ public class OrderByClauseSparkIterator extends RuntimeTupleIterator {
             throw new SparksoniqRuntimeException("Invalid orderby clause.");
         }
         Dataset<Row> df = _child.getDataFrame(context, getProjection(parentProjection));
+        if (df.count() == 0) {
+            return df;
+        }
         StructType inputSchema = df.schema();
 
         List<String> allColumns = DataFrameUtils.getColumnNames(inputSchema);
