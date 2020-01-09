ad56e10a27621390f198659c57e48d36ea7bf819
canberker <canberkerwork@gmail.com>
2020-01-09 19:09:10 +0100

    Apply spotless

--- src/main/java/sparksoniq/jsoniq/compiler/translator/expr/flowr/GroupByClauseVar.java
+++ src/main/java/sparksoniq/jsoniq/compiler/translator/expr/flowr/GroupByClauseVar.java
@@ -49,7 +49,8 @@ public class GroupByClauseVar extends FlworVarDecl {
 
     @Override
     protected void initHighestExecutionMode() {
-        // Execution mode of variable definitions is not used while defining execution mode of GroupByClauseRuntimeIterator
+        // Execution mode of variable definitions is not used while defining execution mode of
+        // GroupByClauseRuntimeIterator
         this._highestExecutionMode = null;
     }
 
