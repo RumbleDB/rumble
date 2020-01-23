5e09fed051720971b9073b6b7ef60ae198eeba31
canberker <canberkerwork@gmail.com>
2020-01-23 22:19:37 +0100

    Fix visitCountClause to initialize variableReference correctly

--- src/main/java/sparksoniq/semantics/visitor/StaticContextVisitor.java
+++ src/main/java/sparksoniq/semantics/visitor/StaticContextVisitor.java
@@ -208,6 +208,7 @@ public class StaticContextVisitor extends AbstractExpressionOrClauseVisitor<Stat
             expression.getMetadata(),
             ExecutionMode.LOCAL
         );
+        this.visit(expression.getCountVariable(), result);
         return result;
     }
 
