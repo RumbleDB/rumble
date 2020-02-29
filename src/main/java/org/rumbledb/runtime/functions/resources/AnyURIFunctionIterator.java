570a607c82e43bf76707b7fdd36d024aba60dddf
Falko Noe <falkonoe@gmail.com>
2020-02-29 17:32:03 +0100

    Added AnyURI function iterator

---
+++ src/main/java/org/rumbledb/runtime/functions/resources/AnyURIFunctionIterator.java
@@ -0,0 +1,59 @@
+package org.rumbledb.runtime.functions.resources;
+
+import org.rumbledb.api.Item;
+import org.rumbledb.exceptions.CastException;
+import org.rumbledb.exceptions.ExceptionMetadata;
+import org.rumbledb.exceptions.IteratorFlowException;
+import org.rumbledb.items.ItemFactory;
+import org.rumbledb.runtime.RuntimeIterator;
+import org.rumbledb.runtime.functions.base.LocalFunctionCallIterator;
+import sparksoniq.jsoniq.ExecutionMode;
+import sparksoniq.semantics.DynamicContext;
+
+import java.util.List;
+import java.net.URI;
+
+public class AnyURIFunctionIterator extends LocalFunctionCallIterator {
+
+    private static final long serialVersionUID = 1L;
+    private Item anyURIStringItem = null;
+
+    public AnyURIFunctionIterator(
+            List<RuntimeIterator> arguments,
+            ExecutionMode executionMode,
+            ExceptionMetadata iteratorMetadata
+    ) {
+        super(arguments, executionMode, iteratorMetadata);
+    }
+
+    @Override
+    public Item next() {
+        if (this.hasNext) {
+            this.hasNext = false;
+            try {
+                return ItemFactory.getInstance().createAnyURIItem(this.anyURIStringItem.getStringValue());
+            } catch (IllegalArgumentException e) {
+                String message = String.format(
+                    "\"%s\": value of type %s is not castable to type %s",
+                    this.anyURIStringItem.serialize(),
+                    "string",
+                    "anyURI"
+                );
+                throw new CastException(message, getMetadata());
+            }
+        } else
+            throw new IteratorFlowException(
+                    RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " anyURI function",
+                    getMetadata()
+            );
+    }
+
+
+    @Override
+    public void open(DynamicContext context) {
+        super.open(context);
+        this.anyURIStringItem = this.children.get(0)
+            .materializeFirstItemOrNull(this.currentDynamicContextForLocalExecution);
+        this.hasNext = this.anyURIStringItem != null;
+    }
+}
