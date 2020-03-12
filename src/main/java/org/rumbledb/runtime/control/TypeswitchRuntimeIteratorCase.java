7d54672684e35caa04c52e887fad65039bfb70f5
Ghislain Fourny <gfourny@inf.ethz.ch>
2020-03-12 15:02:43 +0100

    Add serialization for typeswitch.

--- src/main/java/org/rumbledb/runtime/control/TypeswitchRuntimeIteratorCase.java
+++ src/main/java/org/rumbledb/runtime/control/TypeswitchRuntimeIteratorCase.java
@@ -3,12 +3,19 @@ package org.rumbledb.runtime.control;
 import org.rumbledb.runtime.RuntimeIterator;
 import org.rumbledb.types.SequenceType;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.KryoSerializable;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-public class TypeswitchRuntimeIteratorCase {
-    private final String variableName;
-    private final List<SequenceType> sequenceTypeUnion;
-    private final RuntimeIterator returnIterator;
+public class TypeswitchRuntimeIteratorCase implements Serializable, KryoSerializable {
+    private String variableName;
+    private List<SequenceType> sequenceTypeUnion;
+    private RuntimeIterator returnIterator;
 
     public TypeswitchRuntimeIteratorCase(
             String variableName,
@@ -36,5 +43,20 @@ public class TypeswitchRuntimeIteratorCase {
 
     RuntimeIterator getReturnIterator() {
         return this.returnIterator;
+    }
+
+    @Override
+    public void write(Kryo kryo, Output output) {
+        kryo.writeObject(output, String.class);
+        kryo.writeObject(output, ArrayList.class);
+        kryo.writeObject(output, RuntimeIterator.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void read(Kryo kryo, Input input) {
+        this.variableName = kryo.readObject(input, String.class);
+        this.sequenceTypeUnion = kryo.readObject(input, ArrayList.class);
+        this.returnIterator = kryo.readObject(input, RuntimeIterator.class);
     }
 }
