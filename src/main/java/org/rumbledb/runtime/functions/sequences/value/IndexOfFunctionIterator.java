1c125fab6536f3708d9666d1d4c68237c9d5a594
Falko Noe <falkonoe@gmail.com>
2020-03-19 16:23:16 +0100

    Implemented getRDDAux

--- src/main/java/org/rumbledb/runtime/functions/sequences/value/IndexOfFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/sequences/value/IndexOfFunctionIterator.java
@@ -20,12 +20,12 @@
 
 package org.rumbledb.runtime.functions.sequences.value;
 
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.rumbledb.api.Item;
 import org.rumbledb.exceptions.ExceptionMetadata;
 import org.rumbledb.exceptions.IteratorFlowException;
 import org.rumbledb.exceptions.NonAtomicKeyException;
-import org.rumbledb.exceptions.UnexpectedTypeException;
 import org.rumbledb.items.ItemFactory;
 import org.rumbledb.runtime.HybridRuntimeIterator;
 import org.rumbledb.runtime.RuntimeIterator;
@@ -39,6 +39,7 @@ public class IndexOfFunctionIterator extends HybridRuntimeIterator {
 
     private static final long serialVersionUID = 1L;
     private RuntimeIterator sequenceIterator;
+    private RuntimeIterator searchIterator;
     private Item search;
     private Item nextResult;
     private int currentIndex;
@@ -50,18 +51,24 @@ public class IndexOfFunctionIterator extends HybridRuntimeIterator {
     ) {
         super(arguments, executionMode, iteratorMetadata);
         this.sequenceIterator = this.children.get(0);
-        this.search = this.children.get(1).materializeFirstItemOrNull(this.currentDynamicContextForLocalExecution);
+        this.searchIterator = this.children.get(1);
     }
 
     @Override
     protected JavaRDD<Item> getRDDAux(DynamicContext context) {
-        return null;
+        JavaRDD<Item> childRDD = this.sequenceIterator.getRDD(context);
+        this.search = this.searchIterator.materializeFirstItemOrNull(context);
+
+        JavaPairRDD<Item, Long> zippedRDD = childRDD.zipWithIndex();
+        JavaPairRDD<Item, Long> filteredRDD = zippedRDD.filter((item) -> item._1().equals(this.search));
+        return filteredRDD.map((item) -> ItemFactory.getInstance().createIntegerItem(item._2.intValue() + 1));
     }
 
     @Override
     protected void openLocal() {
         this.currentIndex = 0;
         this.sequenceIterator.open(this.currentDynamicContextForLocalExecution);
+        this.search = this.searchIterator.materializeFirstItemOrNull(this.currentDynamicContextForLocalExecution);
         setNextResult();
     }
 
