76ba1879c5ed79345b1dd7e744850b54588126a4
Ghislain Fourny <gfourny@inf.ethz.ch>
2020-03-30 14:09:26 +0200

    Add schemes.

--- src/main/java/org/rumbledb/runtime/functions/input/UrlValidator.java
+++ src/main/java/org/rumbledb/runtime/functions/input/UrlValidator.java
@@ -8,16 +8,25 @@ import org.rumbledb.exceptions.SparksoniqRuntimeException;
 import sparksoniq.spark.SparkSessionManager;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
 
 public class UrlValidator {
 
-    public static boolean isValid(String url) {
+    private static final String[] allowedSchemes = { "file", "hdfs", "s3", "s3a", "s3n", "wasb", "gs", "root" };
+
+    public static boolean exists(String url) {
+        URL locator = null;
+        try {
+            locator = new URL(url);
+        } catch (MalformedURLException e) {
+            throw new OurBadException("Malformed URL: " + url);
+        }
         if (
-            url.startsWith("file://")
-                || url.startsWith("/")
+            url.startsWith("/")
                 || url.startsWith("./")
-                || url.startsWith("hdfs://")
-                || url.startsWith("s3://")
+                || Arrays.asList(allowedSchemes).contains(locator.getProtocol())
         ) {
             JavaSparkContext sparkContext = SparkSessionManager.getInstance().getJavaSparkContext();
             try {
@@ -30,6 +39,6 @@ public class UrlValidator {
 
         }
 
-        throw new OurBadException("Unhandled url type found in the validator.");
+        throw new OurBadException("Cannot interprete URL: " + url);
     }
 }
