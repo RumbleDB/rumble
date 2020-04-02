94cce1fd294b51a535a362a8d0d84a6b0a415d09
Ghislain Fourny <gfourny@inf.ethz.ch>
2020-04-02 15:35:02 +0200

    Test.

--- src/main/java/org/rumbledb/runtime/functions/input/UrlValidator.java
+++ src/main/java/org/rumbledb/runtime/functions/input/UrlValidator.java
@@ -11,11 +11,8 @@ import org.rumbledb.exceptions.OurBadException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
 public class UrlValidator {
-
-    private static final String[] allowedSchemes = { "file", "hdfs", "s3", "s3a", "s3n", "wasb", "gs", "root" };
 
     public static URI validateURI(String url, ExceptionMetadata metadata) {
         if (url.isEmpty()) {
@@ -24,38 +21,31 @@ public class UrlValidator {
                     new ExceptionMetadata(0, 0)
             );
         }
-        URI locator = null;
         try {
-            locator = new URI(url);
+            return new URI(url);
         } catch (URISyntaxException e) {
             throw new CannotRetrieveResourceException("Malformed URL: " + url, metadata);
         }
-        if (locator.isAbsolute() && !Arrays.asList(allowedSchemes).contains(locator.getScheme())) {
-            throw new OurBadException("Cannot interpret URL: " + url);
-        }
-        return locator;
     }
 
     public static boolean exists(String url, ExceptionMetadata metadata) {
         URI locator = validateURI(url, metadata);
-        {
-            try {
-                FileContext fileContext = FileContext.getFileContext();
-                Path path = new Path(url);
-                return url.contains("*") || fileContext.util().exists(path);
+        try {
+            FileContext fileContext = FileContext.getFileContext();
+            Path path = new Path(url);
+            return url.contains("*") || fileContext.util().exists(path);
 
-            } catch (UnsupportedFileSystemException e) {
-                throw new CannotRetrieveResourceException(
-                        "No file system is configured for scheme " + url + "!",
-                        metadata
-                );
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new CannotRetrieveResourceException(
-                        "Error while accessing the " + locator.getScheme() + " filesystem.",
-                        metadata
-                );
-            }
+        } catch (UnsupportedFileSystemException e) {
+            throw new CannotRetrieveResourceException(
+                    "No file system is configured for scheme " + url + "!",
+                    metadata
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new CannotRetrieveResourceException(
+                    "Error while accessing the " + locator.getScheme() + " filesystem.",
+                    metadata
+            );
         }
     }
 
