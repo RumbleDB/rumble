85a919518a56eecf4b628ad7f62776dc5918f680
Ioana Stefan <iostefan@student.ethz.ch>
2020-04-27 11:06:00 +0300

    Remove redundant override.

--- src/main/java/org/rumbledb/expressions/CommaExpression.java
+++ src/main/java/org/rumbledb/expressions/CommaExpression.java
@@ -78,11 +78,6 @@ public class CommaExpression extends Expression {
     }
 
     @Override
-    public ExecutionMode getHighestExecutionMode(VisitorConfig visitorConfig) {
-        return super.getHighestExecutionMode(visitorConfig);
-    }
-
-    @Override
     public <T> T accept(AbstractNodeVisitor<T> visitor, T argument) {
         return visitor.visitCommaExpression(this, argument);
     }
