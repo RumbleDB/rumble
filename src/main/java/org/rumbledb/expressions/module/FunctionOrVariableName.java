0c7460bd443b89ce3d962942f8837c5852f8bf35
Ghislain Fourny <gfourny@inf.ethz.ch>
2020-05-27 17:46:53 +0200

    Spotless.

--- src/main/java/org/rumbledb/expressions/module/FunctionOrVariableName.java
+++ src/main/java/org/rumbledb/expressions/module/FunctionOrVariableName.java
@@ -4,35 +4,29 @@ public class FunctionOrVariableName {
     private String namespace;
     private String prefix;
     private String localName;
-    
-    public FunctionOrVariableName(String namespace, String prefix, String localName)
-    {
+
+    public FunctionOrVariableName(String namespace, String prefix, String localName) {
         this.namespace = namespace;
         this.prefix = prefix;
         this.localName = localName;
     }
-    
-    public String getNamespace()
-    {
+
+    public String getNamespace() {
         return this.namespace;
     }
-    
-    public String getPrefix()
-    {
+
+    public String getPrefix() {
         return this.prefix;
     }
-    
-    public String getLocalName()
-    {
+
+    public String getLocalName() {
         return this.localName;
     }
-    
+
     @Override
-    public String toString()
-    {
-        if(this.namespace != null)
-        {
-            return "{"+this.namespace+"}"+this.localName;
+    public String toString() {
+        if (this.namespace != null) {
+            return "{" + this.namespace + "}" + this.localName;
         }
         return this.localName;
     }
