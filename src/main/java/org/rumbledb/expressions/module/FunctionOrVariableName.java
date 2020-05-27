8b2aef32f230e4633239589df24012731d8fabed
Ghislain Fourny <gfourny@inf.ethz.ch>
2020-05-27 17:46:19 +0200

    Add file.

---
+++ src/main/java/org/rumbledb/expressions/module/FunctionOrVariableName.java
@@ -0,0 +1,40 @@
+package org.rumbledb.expressions.module;
+
+public class FunctionOrVariableName {
+    private String namespace;
+    private String prefix;
+    private String localName;
+    
+    public FunctionOrVariableName(String namespace, String prefix, String localName)
+    {
+        this.namespace = namespace;
+        this.prefix = prefix;
+        this.localName = localName;
+    }
+    
+    public String getNamespace()
+    {
+        return this.namespace;
+    }
+    
+    public String getPrefix()
+    {
+        return this.prefix;
+    }
+    
+    public String getLocalName()
+    {
+        return this.localName;
+    }
+    
+    @Override
+    public String toString()
+    {
+        if(this.namespace != null)
+        {
+            return "{"+this.namespace+"}"+this.localName;
+        }
+        return this.localName;
+    }
+
+}
