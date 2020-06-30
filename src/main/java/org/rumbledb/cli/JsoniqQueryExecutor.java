369764b372da6865ecb5bed2292d2968c7f3f673
Ghislain Fourny <gfourny@inf.ethz.ch>
2020-06-30 16:41:37 +0200

    Fix log issue.

--- src/main/java/org/rumbledb/cli/JsoniqQueryExecutor.java
+++ src/main/java/org/rumbledb/cli/JsoniqQueryExecutor.java
@@ -77,7 +77,10 @@ public class JsoniqQueryExecutor {
         URI logUri = null;
         if (logPath != null) {
             logUri = FileSystemUtil.resolveURIAgainstWorkingDirectory(logPath, ExceptionMetadata.EMPTY_METADATA);
-            FileSystemUtil.delete(logUri, ExceptionMetadata.EMPTY_METADATA);
+            if(FileSystemUtil.exists(logUri, ExceptionMetadata.EMPTY_METADATA))
+            {
+                FileSystemUtil.delete(logUri, ExceptionMetadata.EMPTY_METADATA);
+            }
         }
 
         List<Item> outputList = null;
