d12d3e35c92ca3d0d3087caa70dd40a07b6e3f0e
Ghislain Fourny <gfourny@inf.ethz.ch>
2020-07-23 17:35:58 +0200

    Refactor.

--- src/main/java/org/rumbledb/runtime/flwor/udfs/LetClauseUDF.java
+++ src/main/java/org/rumbledb/runtime/flwor/udfs/LetClauseUDF.java
@@ -20,19 +20,13 @@
 
 package org.rumbledb.runtime.flwor.udfs;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
 import org.apache.spark.sql.api.java.UDF2;
 import org.rumbledb.api.Item;
 import org.rumbledb.context.DynamicContext;
-import org.rumbledb.context.Name;
-import org.rumbledb.items.ItemFactory;
 import org.rumbledb.runtime.RuntimeIterator;
 import org.rumbledb.runtime.flwor.FlworDataFrameUtils;
 import scala.collection.mutable.WrappedArray;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -41,20 +35,8 @@ public class LetClauseUDF implements UDF2<WrappedArray<byte[]>, WrappedArray<Lon
 
     private static final long serialVersionUID = 1L;
     private RuntimeIterator expression;
-
-    private Map<String, List<String>> columnNamesByType;
-    private List<Name> serializedVariableNames;
-    private List<Name> countedVariableNames;
-
-    private List<List<Item>> deserializedParams;
-    private List<Item> longParams;
-    private DynamicContext parentContext;
-    private DynamicContext context;
+    private DataFrameContext dataFrameContext;
     private List<Item> nextResult;
-
-    private transient Kryo kryo;
-    private transient Output output;
-    private transient Input input;
 
     public LetClauseUDF(
             RuntimeIterator expression,
@@ -62,84 +44,26 @@ public class LetClauseUDF implements UDF2<WrappedArray<byte[]>, WrappedArray<Lon
             Map<String, List<String>> columnNamesByType
     ) {
         this.expression = expression;
-
-        this.deserializedParams = new ArrayList<>();
-        this.longParams = new ArrayList<>();
-        this.parentContext = context;
-        this.context = new DynamicContext(this.parentContext);
+        this.dataFrameContext = new DataFrameContext(context, columnNamesByType);
         this.nextResult = new ArrayList<>();
-
-        this.kryo = new Kryo();
-        this.kryo.setReferences(false);
-        FlworDataFrameUtils.registerKryoClassesKryo(this.kryo);
-        this.output = new Output(128, -1);
-        this.input = new Input();
-
-        this.columnNamesByType = columnNamesByType;
-        List<String> serializedColumNames = this.columnNamesByType.get("byte[]");
-        this.serializedVariableNames = new ArrayList<>(serializedColumNames.size());
-        for (String columnName : serializedColumNames) {
-            this.serializedVariableNames.add(Name.createVariableInNoNamespace(columnName));
-        }
-        List<String> countedColumNames = this.columnNamesByType.get("Long");
-        this.countedVariableNames = new ArrayList<>(countedColumNames.size());
-        for (String columnName : countedColumNames) {
-            this.countedVariableNames.add(Name.createVariableInNoNamespace(columnName));
-        }
     }
-
 
     @Override
     public byte[] call(WrappedArray<byte[]> wrappedParameters, WrappedArray<Long> wrappedParametersLong) {
-        this.deserializedParams.clear();
-        this.longParams.clear();
-        this.context.getVariableValues().removeAllVariables();
+        this.dataFrameContext.setFromWrappedParameters(wrappedParameters, wrappedParametersLong);
+
         this.nextResult.clear();
-
-        FlworDataFrameUtils.deserializeWrappedParameters(
-            wrappedParameters,
-            this.deserializedParams,
-            this.kryo,
-            this.input
-        );
-
-        // Long parameters correspond to pre-computed counts, when a materialization of the
-        // actual sequence was avoided upfront.
-        Object[] longParams = (Object[]) wrappedParametersLong.array();
-        for (Object longParam : longParams) {
-            Item count = ItemFactory.getInstance().createIntItem(((Long) longParam).intValue());
-            this.longParams.add(count);
-        }
-
-        FlworDataFrameUtils.prepareDynamicContext(
-            this.context,
-            this.serializedVariableNames,
-            this.countedVariableNames,
-            this.deserializedParams,
-            this.longParams
-        );
-
-        // apply expression in the dynamic context
-        this.expression.open(this.context);
+        this.expression.open(this.dataFrameContext.getContext());
         while (this.expression.hasNext()) {
             Item nextItem = this.expression.next();
             this.nextResult.add(nextItem);
         }
         this.expression.close();
 
-        return FlworDataFrameUtils.serializeItemList(this.nextResult, this.kryo, this.output);
+        return FlworDataFrameUtils.serializeItemList(
+            this.nextResult,
+            this.dataFrameContext.getKryo(),
+            this.dataFrameContext.getOutput()
+        );
     }
-
-    private void readObject(java.io.ObjectInputStream in)
-            throws IOException,
-                ClassNotFoundException {
-        in.defaultReadObject();
-
-        this.kryo = new Kryo();
-        this.kryo.setReferences(false);
-        FlworDataFrameUtils.registerKryoClassesKryo(this.kryo);
-        this.output = new Output(128, -1);
-        this.input = new Input();
-    }
-
 }
