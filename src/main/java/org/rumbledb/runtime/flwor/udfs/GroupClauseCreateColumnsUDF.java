cc299cc78069347becb837c51bed49765902b77e
Ghislain Fourny <gfourny@inf.ethz.ch>
2020-07-24 16:59:04 +0200

    Refactor grouping create columns.

--- src/main/java/org/rumbledb/runtime/flwor/udfs/GroupClauseCreateColumnsUDF.java
+++ src/main/java/org/rumbledb/runtime/flwor/udfs/GroupClauseCreateColumnsUDF.java
@@ -20,8 +20,6 @@
 
 package org.rumbledb.runtime.flwor.udfs;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.api.java.UDF2;
@@ -31,11 +29,8 @@ import org.rumbledb.context.DynamicContext;
 import org.rumbledb.context.Name;
 import org.rumbledb.exceptions.ExceptionMetadata;
 import org.rumbledb.exceptions.UnexpectedTypeException;
-import org.rumbledb.items.ItemFactory;
-import org.rumbledb.runtime.flwor.FlworDataFrameUtils;
 import scala.collection.mutable.WrappedArray;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -43,19 +38,11 @@ import java.util.Map;
 public class GroupClauseCreateColumnsUDF implements UDF2<WrappedArray<byte[]>, WrappedArray<Long>, Row> {
 
     private static final long serialVersionUID = 1L;
+    private DataFrameContext dataFrameContext;
     private List<Name> groupingVariableNames;
-    private List<Name> serializedVariableNames;
-    private List<Name> countedVariableNames;
 
-    private List<List<Item>> deserializedParams;
-    private List<Item> longParams;
-    private DynamicContext parentContext;
-    private DynamicContext context;
     private List<Object> results;
     private ExceptionMetadata metadata;
-
-    private transient Kryo kryo;
-    private transient Input input;
 
     public GroupClauseCreateColumnsUDF(
             List<Name> groupingVariableNames,
@@ -63,50 +50,16 @@ public class GroupClauseCreateColumnsUDF implements UDF2<WrappedArray<byte[]>, W
             Map<String, List<String>> columnNamesByType,
             ExceptionMetadata metadata
     ) {
+        this.dataFrameContext = new DataFrameContext(context, columnNamesByType);
         this.groupingVariableNames = groupingVariableNames;
-        this.deserializedParams = new ArrayList<>();
-        this.longParams = new ArrayList<>();
-        this.parentContext = context;
-        this.context = new DynamicContext(this.parentContext);
         this.results = new ArrayList<>();
-
-        this.kryo = new Kryo();
-        this.kryo.setReferences(false);
-        FlworDataFrameUtils.registerKryoClassesKryo(this.kryo);
-        this.input = new Input();
-        this.metadata = metadata;
-
-        List<String> serializedColumNames = columnNamesByType.get("byte[]");
-        this.serializedVariableNames = new ArrayList<>(serializedColumNames.size());
-        for (String columnName : serializedColumNames) {
-            this.serializedVariableNames.add(Name.createVariableInNoNamespace(columnName));
-        }
-        List<String> countedColumNames = columnNamesByType.get("Long");
-        this.countedVariableNames = new ArrayList<>(countedColumNames.size());
-        for (String columnName : countedColumNames) {
-            this.countedVariableNames.add(Name.createVariableInNoNamespace(columnName));
-        }
     }
 
     @Override
     public Row call(WrappedArray<byte[]> wrappedParameters, WrappedArray<Long> wrappedParametersLong) {
-        this.deserializedParams.clear();
+        this.dataFrameContext.setFromWrappedParameters(wrappedParameters, wrappedParametersLong);
+
         this.results.clear();
-
-        FlworDataFrameUtils.deserializeWrappedParameters(
-            wrappedParameters,
-            this.deserializedParams,
-            this.kryo,
-            this.input
-        );
-
-        // Long parameters correspond to pre-computed counts, when a materialization of the
-        // actual sequence was avoided upfront.
-        Object[] longParams = (Object[]) wrappedParametersLong.array();
-        for (Object longParam : longParams) {
-            Item count = ItemFactory.getInstance().createIntItem(((Long) longParam).intValue());
-            this.longParams.add(count);
-        }
 
         for (Name groupingVariableName : this.groupingVariableNames) {
             // nulls, true, false and empty sequences have special grouping captured in the first grouping column.
@@ -121,29 +74,9 @@ public class GroupClauseCreateColumnsUDF implements UDF2<WrappedArray<byte[]>, W
             int durationGroupIndex = 5;
             int dateTimeGroupIndex = 5;
 
-            // prepare dynamic context
-            this.context.getVariableValues().removeAllVariables();
-            int columnIndex = 0;
-            for (Name serializedVariableName : this.serializedVariableNames) {
-                this.context.getVariableValues()
-                    .addVariableValue(
-                        serializedVariableName,
-                        this.deserializedParams.get(columnIndex)
-                    );
-                ++columnIndex;
-            }
-            columnIndex = 0;
-            for (Name countedVariableName : this.countedVariableNames) {
-                this.context.getVariableValues()
-                    .addVariableCount(
-                        countedVariableName,
-                        this.longParams.get(columnIndex)
-                    );
-                ++columnIndex;
-            }
-
             boolean isEmptySequence = true;
-            List<Item> items = this.context.getVariableValues()
+            List<Item> items = this.dataFrameContext.getContext()
+                .getVariableValues()
                 .getLocalVariableValue(
                     groupingVariableName,
                     this.metadata
@@ -217,16 +150,5 @@ public class GroupClauseCreateColumnsUDF implements UDF2<WrappedArray<byte[]>, W
 
         }
         return RowFactory.create(this.results.toArray());
-    }
-
-    private void readObject(java.io.ObjectInputStream in)
-            throws IOException,
-                ClassNotFoundException {
-        in.defaultReadObject();
-
-        this.kryo = new Kryo();
-        this.kryo.setReferences(false);
-        FlworDataFrameUtils.registerKryoClassesKryo(this.kryo);
-        this.input = new Input();
     }
 }
