f9e61dc08cca7074943005b575a95054bf066265
Mario Arduini <mario.arduini.gen@gmail.com>
2020-10-12 23:24:49 +0200

    added boolean, null, variableReference, array and object constructors

--- src/main/java/org/rumbledb/compiler/InferTypeVisitor.java
+++ src/main/java/org/rumbledb/compiler/InferTypeVisitor.java
@@ -5,10 +5,7 @@ import org.rumbledb.exceptions.UnexpectedStaticTypeException;
 import org.rumbledb.expressions.AbstractNodeVisitor;
 import org.rumbledb.expressions.CommaExpression;
 import org.rumbledb.expressions.Expression;
-import org.rumbledb.expressions.primary.DecimalLiteralExpression;
-import org.rumbledb.expressions.primary.DoubleLiteralExpression;
-import org.rumbledb.expressions.primary.IntegerLiteralExpression;
-import org.rumbledb.expressions.primary.StringLiteralExpression;
+import org.rumbledb.expressions.primary.*;
 import org.rumbledb.types.ItemType;
 import org.rumbledb.types.SequenceType;
 
@@ -41,7 +38,7 @@ public class InferTypeVisitor extends AbstractNodeVisitor<Void> {
 
             // if a child expression has no inferred type throw an error
             if(childExpressionInferredType == null){
-                throw new UnexpectedStaticTypeException("A child expression of a CommaExpression was null");
+                throw new UnexpectedStaticTypeException("A child expression of a CommaExpression has no inferred type");
             }
 
             // if the child expression is an EMPTY_SEQUENCE it does not affect the comma expression type
@@ -91,6 +88,59 @@ public class InferTypeVisitor extends AbstractNodeVisitor<Void> {
     public Void visitDecimal(DecimalLiteralExpression expression, Void argument) {
         System.out.println("visiting Decimal literal");
         expression.setInferredSequenceType(new SequenceType(ItemType.decimalItem));
+        return argument;
+    }
+
+    @Override
+    public Void visitNull(NullLiteralExpression expression, Void argument) {
+        System.out.println("visiting Null literal");
+        expression.setInferredSequenceType(new SequenceType(ItemType.nullItem));
+        return argument;
+    }
+
+    @Override
+    public Void visitBoolean(BooleanLiteralExpression expression, Void argument) {
+        System.out.println("visiting Boolean literal");
+        expression.setInferredSequenceType(new SequenceType(ItemType.booleanItem));
+        return argument;
+    }
+
+    @Override
+    public Void visitVariableReference(VariableReferenceExpression expression, Void argument) {
+        SequenceType variableType = expression.getType();
+        if(variableType == null){
+            System.out.println("variable reference type was null so");
+            variableType = SequenceType.MOST_GENERAL_SEQUENCE_TYPE;
+        }
+        System.out.println("visiting variable reference with type: " + variableType);
+        expression.setInferredSequenceType(variableType);
+        return argument;
+    }
+
+    @Override
+    public Void visitArrayConstructor(ArrayConstructorExpression expression, Void argument) {
+        System.out.println("visiting Array constructor literal");
+        visitDescendants(expression, argument);
+        expression.setInferredSequenceType(new SequenceType(ItemType.arrayItem));
+        return argument;
+    }
+
+    @Override
+    public Void visitObjectConstructor(ObjectConstructorExpression expression, Void argument) {
+        System.out.println("visiting Object constructor literal");
+        visitDescendants(expression, argument);
+        // TODO: what about object merged constructor with childExpression
+        for(Expression keyExpression : expression.getKeys()){
+            SequenceType keySequenceType = keyExpression.getInferredSequenceType();
+            if(keySequenceType == null){
+                throw new UnexpectedStaticTypeException("One of the key in the object constructor has no inferred type");
+            }
+            ItemType keyType = keySequenceType.getItemType();
+            if(!keyType.equals(ItemType.stringItem) && !keyType.equals(ItemType.atomicItem) && !keyType.equals(ItemType.item)){
+                throw new UnexpectedStaticTypeException("The inferred static types for the keys of an Object must be String or one of its supertypes (i.e. atomicItem or item)");
+            }
+        }
+        expression.setInferredSequenceType(new SequenceType(ItemType.objectItem));
         return argument;
     }
 
