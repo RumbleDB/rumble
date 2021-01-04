415574ca1ec517eb3376bcfd12bf4e077f166044
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-01-04 10:55:47 +0100

    Spotless.

--- src/main/java/org/rumbledb/runtime/functions/io/JsonDocFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/io/JsonDocFunctionIterator.java
@@ -38,7 +38,6 @@ import org.rumbledb.runtime.functions.input.FileSystemUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.List;
 
