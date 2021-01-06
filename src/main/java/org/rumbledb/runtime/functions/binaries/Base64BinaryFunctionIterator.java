cfbd72ad03c8359ed5000cff7f2b62d0366a70fa
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-01-06 13:51:40 +0100

    Fix.

--- src/main/java/org/rumbledb/runtime/functions/binaries/Base64BinaryFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/binaries/Base64BinaryFunctionIterator.java
@@ -5,17 +5,19 @@ import org.rumbledb.context.DynamicContext;
 import org.rumbledb.exceptions.CastException;
 import org.rumbledb.exceptions.ExceptionMetadata;
 import org.rumbledb.exceptions.IteratorFlowException;
+import org.rumbledb.exceptions.UnexpectedTypeException;
 import org.rumbledb.expressions.ExecutionMode;
-import org.rumbledb.items.ItemFactory;
 import org.rumbledb.runtime.RuntimeIterator;
 import org.rumbledb.runtime.functions.base.LocalFunctionCallIterator;
+import org.rumbledb.runtime.typing.InstanceOfIterator;
+import org.rumbledb.types.ItemType;
 
 import java.util.List;
 
 public class Base64BinaryFunctionIterator extends LocalFunctionCallIterator {
 
     private static final long serialVersionUID = 1L;
-    private Item base64BinaryStringItem = null;
+    private Item item = null;
 
     public Base64BinaryFunctionIterator(
             List<RuntimeIterator> parameters,
@@ -29,31 +31,43 @@ public class Base64BinaryFunctionIterator extends LocalFunctionCallIterator {
     public Item next() {
         if (this.hasNext) {
             this.hasNext = false;
-            try {
-                return ItemFactory.getInstance().createBase64BinaryItem(this.base64BinaryStringItem.getStringValue());
-            } catch (IllegalArgumentException e) {
-                String message = String.format(
-                    "\"%s\": value of type %s is not castable to type %s",
-                    this.base64BinaryStringItem.serialize(),
-                    "string",
-                    "base64Binary"
-                );
-                throw new CastException(message, getMetadata());
+            if (
+                InstanceOfIterator.doesItemTypeMatchItem(ItemType.base64BinaryItem, this.item)
+                    || InstanceOfIterator.doesItemTypeMatchItem(ItemType.hexBinaryItem, this.item)
+                    || InstanceOfIterator.doesItemTypeMatchItem(ItemType.stringItem, this.item)
+            ) {
+                try {
+                    return this.item.castAs(ItemType.base64BinaryItem);
+                } catch (Exception e) {
+                    String message = String.format(
+                        "\"%s\": value of type %s is not castable to type %s: ",
+                        this.item.serialize(),
+                        this.item.getDynamicType(),
+                        "base64Binary"
+                    );
+                    throw new CastException(message, getMetadata());
+                }
             }
-        } else {
-            throw new IteratorFlowException(
-                    RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " base64Binary function",
-                    getMetadata()
+            String message = String.format(
+                "\"%s\": value of type %s is not castable to type %s: ",
+                this.item.serialize(),
+                this.item.getDynamicType(),
+                "base64Binary"
             );
+            throw new UnexpectedTypeException(message, getMetadata());
         }
+        throw new IteratorFlowException(
+                RuntimeIterator.FLOW_EXCEPTION_MESSAGE + " hexBinary function",
+                getMetadata()
+        );
     }
 
 
     @Override
     public void open(DynamicContext context) {
         super.open(context);
-        this.base64BinaryStringItem = this.children.get(0)
+        this.item = this.children.get(0)
             .materializeFirstItemOrNull(this.currentDynamicContextForLocalExecution);
-        this.hasNext = this.base64BinaryStringItem != null;
+        this.hasNext = this.item != null;
     }
 }
