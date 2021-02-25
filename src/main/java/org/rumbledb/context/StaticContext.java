807ea179ea51397a465903d432b430d13e30fb3a
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-02-25 16:00:18 +0100

    Enable override of predeclared prefixes.

--- src/main/java/org/rumbledb/context/StaticContext.java
+++ src/main/java/org/rumbledb/context/StaticContext.java
@@ -53,6 +53,20 @@ public class StaticContext implements Serializable, KryoSerializable {
     // TODO: should these be transient?
     private transient SequenceType contextItemStaticType;
     private transient Map<FunctionIdentifier, FunctionSignature> staticallyKnownFunctionSignatures;
+    
+    private static final Map<String, String> defaultBindings;
+    
+    static {
+        defaultBindings = new HashMap<>();
+        defaultBindings.put("local", Name.LOCAL_NS);
+        defaultBindings.put("fn", Name.FN_NS);
+        defaultBindings.put("math", Name.MATH_NS);
+        defaultBindings.put("map", Name.MAP_NS);
+        defaultBindings.put("array", Name.ARRAY_NS);
+        defaultBindings.put("xs", Name.XS_NS);
+        defaultBindings.put("jn", Name.JN_NS);
+        defaultBindings.put("js", Name.JS_NS);
+    }
 
     private RumbleRuntimeConfiguration configuration;
 
@@ -226,6 +240,14 @@ public class StaticContext implements Serializable, KryoSerializable {
             this.staticallyKnownNamespaces.put(prefix, namespace);
             return true;
         }
+        if(defaultBindings.containsKey(prefix))
+        {
+            if(this.staticallyKnownNamespaces.get(prefix).equals(defaultBindings.get(prefix)))
+            {
+               this.staticallyKnownNamespaces.put(prefix,  namespace);
+               return true;
+            }
+        }
         return false;
     }
 
@@ -341,6 +363,13 @@ public class StaticContext implements Serializable, KryoSerializable {
                 }
             }
             current = current.parent;
+        }
+    }
+
+    public void bindDefaultNamespaces() {
+        for(String prefix : defaultBindings.keySet())
+        {
+            bindNamespace(prefix, defaultBindings.get(prefix));
         }
     }
 }
