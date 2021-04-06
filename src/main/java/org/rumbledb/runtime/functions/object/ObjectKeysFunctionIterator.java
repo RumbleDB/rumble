6642ac0ebe3c07eea33e24422b0e635e32448d88
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-04-06 16:57:25 +0200

    Fix.

--- src/main/java/org/rumbledb/runtime/functions/object/ObjectKeysFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/object/ObjectKeysFunctionIterator.java
@@ -146,7 +146,9 @@ public class ObjectKeysFunctionIterator extends HybridRuntimeIterator {
 
     @Override
     protected void closeLocal() {
-        this.iterator.close();
+        if (!this.iterator.isDataFrame()) {
+            this.iterator.close();
+        }
     }
 
     @Override
