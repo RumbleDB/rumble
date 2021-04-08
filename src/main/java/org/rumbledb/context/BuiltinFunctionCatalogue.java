89591c5126055b5d953f688f19bb42234c682a94
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-04-08 16:38:35 +0200

    Take over from master.

--- src/main/java/org/rumbledb/context/BuiltinFunctionCatalogue.java
+++ src/main/java/org/rumbledb/context/BuiltinFunctionCatalogue.java
@@ -857,8 +857,8 @@ public class BuiltinFunctionCatalogue {
                 "math",
                 "round"
         ),
-        "double?",
-        "double?",
+        "numeric?",
+        "numeric?",
         RoundFunctionIterator.class,
         BuiltinFunction.BuiltinFunctionExecutionMode.LOCAL
     );
@@ -868,9 +868,9 @@ public class BuiltinFunctionCatalogue {
                 "math",
                 "round"
         ),
-        "double?",
+        "numeric?",
         "integer",
-        "double?",
+        "numeric?",
         RoundFunctionIterator.class,
         BuiltinFunction.BuiltinFunctionExecutionMode.LOCAL
     );
