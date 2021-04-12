87948c4278635ec2023deb10c1b806f4feb11860
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-04-12 13:27:46 +0200

    Restore.

--- src/test/java/iq/FrontendTests.java
+++ src/test/java/iq/FrontendTests.java
@@ -21,6 +21,7 @@
 package iq;
 
 
+import iq.base.AnnotationsTestsBase;
 import org.junit.Assert;
 import org.junit.Test;
 import org.rumbledb.compiler.VisitorHelpers;
@@ -31,9 +32,6 @@ import org.rumbledb.expressions.module.MainModule;
 import org.rumbledb.expressions.primary.VariableReferenceExpression;
 import org.rumbledb.runtime.functions.input.FileSystemUtil;
 import org.rumbledb.types.BuiltinTypesCatalogue;
-
-import iq.base.AnnotationsTestsBase;
-
 import java.io.File;
 import java.net.URI;
 import java.util.Arrays;
