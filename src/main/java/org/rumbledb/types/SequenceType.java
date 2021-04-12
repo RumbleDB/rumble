a1e5ff439b97a8f7887dd53ed753576623047f7e
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-04-12 13:26:58 +0200

    Restore file.

--- src/main/java/org/rumbledb/types/SequenceType.java
+++ src/main/java/org/rumbledb/types/SequenceType.java
@@ -311,6 +311,12 @@ public class SequenceType implements Serializable {
         sequenceTypes.put("float", new SequenceType(BuiltinTypesCatalogue.floatItem, SequenceType.Arity.One));
         sequenceTypes.put("float?", new SequenceType(BuiltinTypesCatalogue.floatItem, SequenceType.Arity.OneOrZero));
 
+        sequenceTypes.put("numeric", new SequenceType(BuiltinTypesCatalogue.numericItem, SequenceType.Arity.One));
+        sequenceTypes.put(
+            "numeric?",
+            new SequenceType(BuiltinTypesCatalogue.numericItem, SequenceType.Arity.OneOrZero)
+        );
+
         sequenceTypes.put("boolean", new SequenceType(BuiltinTypesCatalogue.booleanItem, SequenceType.Arity.One));
         sequenceTypes.put(
             "boolean?",
