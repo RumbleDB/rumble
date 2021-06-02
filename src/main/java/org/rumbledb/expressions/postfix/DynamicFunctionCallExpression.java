727256081107cdc323d8f0cd5b786db929211956
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-06-02 15:54:50 +0200

    Allow for DF execution of DFCs.

--- src/main/java/org/rumbledb/expressions/postfix/DynamicFunctionCallExpression.java
+++ src/main/java/org/rumbledb/expressions/postfix/DynamicFunctionCallExpression.java
@@ -20,9 +20,11 @@
 
 package org.rumbledb.expressions.postfix;
 
+import org.rumbledb.compiler.VisitorConfig;
 import org.rumbledb.exceptions.ExceptionMetadata;
 import org.rumbledb.exceptions.OurBadException;
 import org.rumbledb.expressions.AbstractNodeVisitor;
+import org.rumbledb.expressions.ExecutionMode;
 import org.rumbledb.expressions.Expression;
 import org.rumbledb.expressions.Node;
 import java.util.ArrayList;
@@ -114,5 +116,14 @@ public class DynamicFunctionCallExpression extends Expression {
             }
         }
         sb.append(")\n");
+    }
+
+    @Override
+    public void initHighestExecutionMode(VisitorConfig visitorConfig) {
+        if (this.arguments.size() == 0) {
+            this.highestExecutionMode = ExecutionMode.LOCAL;
+            return;
+        }
+        this.highestExecutionMode = this.arguments.get(0).getHighestExecutionMode(visitorConfig);
     }
 }
