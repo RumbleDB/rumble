a17a2072642621dd77f1402bfb940f51084d81cb
Pierre Motard <p.motard@outlook.com>
2021-06-07 20:01:40 +0200

    LowerCase pass spotless test

--- src/main/java/org/rumbledb/runtime/functions/strings/LowerCaseFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/strings/LowerCaseFunctionIterator.java
@@ -23,14 +23,12 @@ package org.rumbledb.runtime.functions.strings;
 import org.rumbledb.api.Item;
 import org.rumbledb.context.DynamicContext;
 import org.rumbledb.exceptions.ExceptionMetadata;
-import org.rumbledb.exceptions.IteratorFlowException;
 import org.rumbledb.expressions.ExecutionMode;
 import org.rumbledb.items.ItemFactory;
 import org.rumbledb.runtime.AtMostOneItemLocalRuntimeIterator;
 import org.rumbledb.runtime.RuntimeIterator;
 
 import java.util.List;
-import java.util.Locale;
 
 public class LowerCaseFunctionIterator extends AtMostOneItemLocalRuntimeIterator {
 
