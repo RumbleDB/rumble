dae4a5b25937e3af8cd5280969b9e882821b331b
Pierre Motard <p.motard@outlook.com>
2021-07-24 00:10:42 +0200

    min to volcano style.

--- src/main/java/org/rumbledb/runtime/functions/sequences/aggregate/MinFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/sequences/aggregate/MinFunctionIterator.java
@@ -24,20 +24,19 @@ import org.apache.spark.api.java.JavaRDD;
 import org.rumbledb.api.Item;
 import org.rumbledb.context.DynamicContext;
 import org.rumbledb.context.Name;
-import org.rumbledb.exceptions.DefaultCollationException;
-import org.rumbledb.exceptions.ExceptionMetadata;
-import org.rumbledb.exceptions.InvalidArgumentTypeException;
-import org.rumbledb.exceptions.RumbleException;
+import org.rumbledb.exceptions.*;
 import org.rumbledb.expressions.ExecutionMode;
 import org.rumbledb.items.ItemComparator;
 import org.rumbledb.items.ItemFactory;
 import org.rumbledb.items.structured.JSoundDataFrame;
 import org.rumbledb.runtime.AtMostOneItemLocalRuntimeIterator;
 import org.rumbledb.runtime.RuntimeIterator;
+import org.rumbledb.runtime.misc.ComparisonIterator;
 import org.rumbledb.runtime.primary.VariableReferenceIterator;
 import sparksoniq.spark.SparkSessionManager;
+import org.rumbledb.expressions.comparison.ComparisonExpression.ComparisonOperator;
+import org.rumbledb.runtime.misc.ComparisonIterator;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -48,6 +47,10 @@ public class MinFunctionIterator extends AtMostOneItemLocalRuntimeIterator {
     private static final long serialVersionUID = 1L;
     private RuntimeIterator iterator;
     private Item result;
+    private Item nextResult;
+    private Item minResult;
+    private ItemComparator comparator;
+
 
     public MinFunctionIterator(
             List<RuntimeIterator> arguments,
@@ -56,17 +59,18 @@ public class MinFunctionIterator extends AtMostOneItemLocalRuntimeIterator {
     ) {
         super(arguments, executionMode, iteratorMetadata);
         this.iterator = this.children.get(0);
-    }
-
-    @Override
-    public Item materializeFirstItemOrNull(DynamicContext context) {
-        ItemComparator comparator = new ItemComparator(
+        this.comparator = new ItemComparator(
                 true,
                 new InvalidArgumentTypeException(
                         "Min expression input error. Input has to be non-null atomics of matching types",
                         getMetadata()
                 )
         );
+    }
+
+    @Override
+    public Item materializeFirstItemOrNull(DynamicContext context) {
+
         if (this.children.size() == 2) {
             String collation = this.children.get(1).materializeFirstItemOrNull(context).getStringValue();
             if (!collation.equals("http://www.w3.org/2005/xpath-functions/collation/codepoint")) {
@@ -82,7 +86,12 @@ public class MinFunctionIterator extends AtMostOneItemLocalRuntimeIterator {
             }
 
             try {
-                return itemTypePromotion(Collections.min(results, comparator));
+                System.out.println("before open context");
+                open(context);
+                System.out.println("before  next");
+                return next();
+                //return itemTypePromotion(Collections.min(results, comparator));
+
             } catch (RumbleException e) {
                 RumbleException ex = new InvalidArgumentTypeException(
                         "Min expression input error. Input has to be non-null atomics of matching types.",
@@ -114,10 +123,70 @@ public class MinFunctionIterator extends AtMostOneItemLocalRuntimeIterator {
         if (rdd.isEmpty()) {
             return null;
         }
-        this.result = rdd.min(comparator);
+        this.result = rdd.min(comparator); // this
         return itemTypePromotion(this.result);
 
+    }
 
+    @Override
+    public void open(DynamicContext dynamicContext) {
+        this.iterator.open(dynamicContext);
+        System.out.println("next it  ");
+        this.minResult = this.iterator.next();
+        System.out.println("setNextRes");
+        setNextResult();
+    }
+
+    @Override
+    public Item next() {
+        //Item result = this.nextResult;
+        //setNextResult();
+        System.out.println("NEXT");
+        System.out.println(this.minResult);
+        System.out.println(this.nextResult);
+        return this.minResult;
+        //throw new IteratorFlowException(FLOW_EXCEPTION_MESSAGE + "min function", getMetadata());
+    }
+
+    private void setNextResult() {
+        this.nextResult = null;
+        while (this.iterator.hasNext()) {
+            System.out.println("init candidate");
+            Item candidateItem = this.iterator.next();
+            System.out.println("candidate is ");
+            System.out.println(candidateItem.getIntValue());
+            long c = ComparisonIterator.compareItems(candidateItem, this.minResult, ComparisonOperator.GC_LT, getMetadata());
+            System.out.println("c");
+            System.out.println(c);
+            if (c < 0) {
+                this.minResult = candidateItem;
+                this.nextResult = this.minResult;
+            }
+        }
+        System.out.println("out loop");
+        if (this.nextResult == null) {
+            this.hasNext = false;
+            this.iterator.close();
+        } else {
+            this.hasNext = true;
+        }
+        next();
+    }
+
+    @Override
+    public void reset(DynamicContext dynamicContext) {
+        this.iterator.reset(dynamicContext);
+        setNextResult();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.hasNext;
+    }
+
+    @Override
+    public void close() {
+        this.iterator.close();
     }
 
     public Map<Name, DynamicContext.VariableDependency> getVariableDependencies() {
