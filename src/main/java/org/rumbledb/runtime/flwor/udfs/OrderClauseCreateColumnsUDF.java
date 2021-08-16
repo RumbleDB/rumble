507f6469ee7968e4919109e7f719ea8e15c67f06
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-08-16 15:51:18 +0200

    Update OrderClauseCreateColumnsUDF.java

--- src/main/java/org/rumbledb/runtime/flwor/udfs/OrderClauseCreateColumnsUDF.java
+++ src/main/java/org/rumbledb/runtime/flwor/udfs/OrderClauseCreateColumnsUDF.java
@@ -74,7 +74,7 @@ public class OrderClauseCreateColumnsUDF implements UDF1<Row, Row> {
 
     @Override
     public Row call(Row row) {
-        this.dataFrameContext.setFromRow(row, null);
+        this.dataFrameContext.setFromRow(row);
 
         this.results.clear();
 
