86edd072fd5f75f9cbff11dd03fdac44ec6b0dc5
Ghislain Fourny <gfourny@inf.ethz.ch>
2021-08-19 12:08:23 +0200

    Fix code.

--- src/main/java/org/rumbledb/context/VariableValues.java
+++ src/main/java/org/rumbledb/context/VariableValues.java
@@ -196,10 +196,10 @@ public class VariableValues implements Serializable, KryoSerializable {
         }
 
         if (this.dataFrameVariableValues.containsKey(varName)) {
+            JSoundDataFrame df = this.dataFrameVariableValues.get(varName);
             System.err.println("VariableValues");
             df.getDataFrame().show();
             System.err.println(df.getItemType());
-            JSoundDataFrame df = this.dataFrameVariableValues.get(varName);
             JavaRDD<Row> rowRDD = df.javaRDD();
             return rowRDD.map(new RowToItemMapper(metadata, df.getItemType()));
         }
