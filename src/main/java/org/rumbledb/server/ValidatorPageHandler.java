8643eaef0fa45a161d4157e869d481f4488aabdf
Pierre <p.motard@outlook.com>
2021-10-13 13:44:36 +0200

    Update rumble server for jsound validator

---
+++ src/main/java/org/rumbledb/server/ValidatorPageHandler.java
@@ -0,0 +1,25 @@
+package org.rumbledb.server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.apache.commons.io.IOUtils;
+import org.rumbledb.cli.Main;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+@SuppressWarnings("restriction")
+public class ValidatorPageHandler implements HttpHandler {
+
+    public ValidatorPageHandler() {
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = IOUtils.toString(Main.class.getResourceAsStream("/assets/validserver.html"), "UTF-8");;
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream stream = exchange.getResponseBody();
+        stream.write(response.getBytes());
+        stream.close();
+    }
+}
