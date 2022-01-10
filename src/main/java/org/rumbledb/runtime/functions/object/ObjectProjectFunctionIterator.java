5ca5160eb61eeb4408eb3a83e0cfef04d7fb1c43
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2022-01-10 17:38:44 +0100

    Fix contexts.

--- src/main/java/org/rumbledb/runtime/functions/object/ObjectProjectFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/object/ObjectProjectFunctionIterator.java
@@ -144,7 +144,7 @@ public class ObjectProjectFunctionIterator extends HybridRuntimeIterator {
     @Override
     public JavaRDD<Item> getRDDAux(DynamicContext context) {
         JavaRDD<Item> childRDD = this.iterator.getRDD(context);
-        this.projectionKeys = this.children.get(1).materialize(this.currentDynamicContextForLocalExecution);
+        this.projectionKeys = this.children.get(1).materialize(context);
         FlatMapFunction<Item, Item> transformation = new ObjectProjectClosure(
                 this.projectionKeys,
                 getMetadata()
