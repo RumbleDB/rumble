1d4e58a2182e12a5d50513493078f897e86f11ec
Ghislain Fourny <gfourny@inf.ethz.ch>
2022-03-31 17:01:53 +0200

    Update OrderByClauseSparkIterator.java

--- src/main/java/org/rumbledb/runtime/flwor/clauses/OrderByClauseSparkIterator.java
+++ src/main/java/org/rumbledb/runtime/flwor/clauses/OrderByClauseSparkIterator.java
@@ -590,7 +590,6 @@ public class OrderByClauseSparkIterator extends RuntimeTupleIterator {
     /**
      * Says whether this expression evaluation triggers a Spark job.
      *
-     * @param visitorConfig the configuration of the visitor.
      * @return true if the execution triggers a Spark, false otherwise, null if undetermined yet.
      */
     @Override
