88b49771913d64c02b6f78e4814ade933dc7a15d
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2022-04-11 15:56:25 +0200

    Add error code.

---
+++ src/main/java/org/rumbledb/exceptions/DatesWithTimezonesNotSupported.java
@@ -0,0 +1,16 @@
+package org.rumbledb.exceptions;
+
+import org.rumbledb.errorcodes.ErrorCode;
+
+public class DatesWithTimezonesNotSupported extends RumbleException {
+
+
+    private static final long serialVersionUID = 1L;
+
+    private static final String message =
+        "By default, dates timezones cannot be converted to DataFrames. Try --dates-with-timezone yes to avoid DataFrames for dates.";
+
+    public DatesWithTimezonesNotSupported(ExceptionMetadata metadata) {
+        super(message, ErrorCode.CastErrorCode, metadata);
+    }
+}
