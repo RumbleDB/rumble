e25e20cbf9b8e174f460656170d62fde4da2b0fa
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2022-04-12 09:04:38 +0200

    Detect job in a job at more places.

--- src/main/java/org/rumbledb/runtime/flwor/udfs/ForClauseUDF.java
+++ src/main/java/org/rumbledb/runtime/flwor/udfs/ForClauseUDF.java
@@ -24,6 +24,7 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.api.java.UDF1;
 import org.rumbledb.api.Item;
 import org.rumbledb.context.DynamicContext;
+import org.rumbledb.exceptions.JobWithinAJobException;
 import org.rumbledb.runtime.RuntimeIterator;
 import org.rumbledb.runtime.flwor.FlworDataFrameColumn;
 import org.rumbledb.runtime.flwor.FlworDataFrameUtils;
@@ -48,6 +49,13 @@ public class ForClauseUDF implements UDF1<Row, List<byte[]>> {
     ) {
         this.dataFrameContext = new DataFrameContext(context, columnNames);
         this.expression = expression;
+        if (this.expression.isSparkJobNeeded()) {
+            throw new JobWithinAJobException(
+                    "The expression in this clause requires parallel execution, but is itself executed in parallel. Please consider moving it up or unnest it if it is independent on previous FLWOR variables.",
+                    this.expression.getMetadata()
+            );
+        }
+
         this.nextResult = new ArrayList<>();
         this.results = new ArrayList<>();
     }
