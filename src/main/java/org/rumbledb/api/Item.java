a1f99c1368a11cc7b29b01fa2fab3f9bd71c469d
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2024-02-27 17:12:53 +0100

    Fix javadoc.

--- src/main/java/org/rumbledb/api/Item.java
+++ src/main/java/org/rumbledb/api/Item.java
@@ -698,7 +698,7 @@ public interface Item extends Serializable, KryoSerializable {
     /**
      * Returns the mutability level of the item.
      *
-     * @return int representing nestedness of the item inside transform expressions.
+     * @return an int representing nestedness of the item inside transform expressions.
      */
     default void setMutabilityLevel(int mutabilityLevel) {
     }
