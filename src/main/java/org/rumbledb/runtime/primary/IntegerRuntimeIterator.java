b81cf1cf59fb22c821f8d7202e83e929b6d94122
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2024-03-13 13:49:45 +0100

    Fix test.

--- src/main/java/org/rumbledb/runtime/primary/IntegerRuntimeIterator.java
+++ src/main/java/org/rumbledb/runtime/primary/IntegerRuntimeIterator.java
@@ -25,6 +25,11 @@ import org.rumbledb.context.DynamicContext;
 import org.rumbledb.context.RuntimeStaticContext;
 import org.rumbledb.items.ItemFactory;
 import org.rumbledb.runtime.flwor.NativeClauseContext;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
 import org.rumbledb.runtime.AtMostOneItemLocalRuntimeIterator;
 
 public class IntegerRuntimeIterator extends AtMostOneItemLocalRuntimeIterator {
@@ -52,5 +57,17 @@ public class IntegerRuntimeIterator extends AtMostOneItemLocalRuntimeIterator {
                 nativeClauseContext,
                 "CAST (" + this.item.getIntValue() + "BD AS DECIMAL(38, 0))"
         );
+    }
+
+    @Override
+    public void write(Kryo kryo, Output output) {
+        super.write(kryo, output);
+        kryo.writeClassAndObject(output, this.item);
+    }
+
+    @Override
+    public void read(Kryo kryo, Input input) {
+        super.read(kryo, input);
+        this.item = (Item) kryo.readClassAndObject(input);
     }
 }
