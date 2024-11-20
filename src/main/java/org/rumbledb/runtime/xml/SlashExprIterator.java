ac67df6b558e65ab06160995596135fdd28b7871
Marco Schöb <marcoschoeb@gmail.com>
2024-11-20 10:18:21 +0100

    fix wrong indexlookup test and update slashexpr implementation accordingly

--- src/main/java/org/rumbledb/runtime/xml/SlashExprIterator.java
+++ src/main/java/org/rumbledb/runtime/xml/SlashExprIterator.java
@@ -1,7 +1,6 @@
 package org.rumbledb.runtime.xml;
 
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.rumbledb.api.Item;
 import org.rumbledb.context.DynamicContext;
@@ -11,7 +10,6 @@ import org.rumbledb.exceptions.IteratorFlowException;
 import org.rumbledb.runtime.HybridRuntimeIterator;
 import org.rumbledb.runtime.RuntimeIterator;
 import scala.Tuple2;
-import sparksoniq.spark.SparkSessionManager;
 
 import java.util.*;
 
@@ -23,7 +21,6 @@ public class SlashExprIterator extends HybridRuntimeIterator {
     private List<Item> results = null;
     private int nextResultCounter = 0;
     private Item nextResult;
-    private boolean rightIsStep;
 
 
     public SlashExprIterator(
@@ -34,7 +31,6 @@ public class SlashExprIterator extends HybridRuntimeIterator {
         super(Arrays.asList(sequence, stepIterator), staticContext);
         this.leftIterator = sequence;
         this.rightIterator = stepIterator;
-        this.rightIsStep = rightIterator instanceof StepExprIterator;
     }
 
     @Override
@@ -42,28 +38,17 @@ public class SlashExprIterator extends HybridRuntimeIterator {
         JavaRDD<Item> childRDD = this.leftIterator.getRDD(dynamicContext);
 
         // apply right iterator, could be step or predicate/sequencelookup
-        if (this.rightIsStep) {
-            FlatMapFunction<Item, Item> transformation = new SlashExprClosure(this.rightIterator, dynamicContext);
-            JavaRDD<Item> result = childRDD.flatMap(transformation);
+        FlatMapFunction<Item, Item> transformation = new SlashExprClosure(this.rightIterator, dynamicContext);
+        JavaRDD<Item> result = childRDD.flatMap(transformation);
 
-            // get unique items based on unique document position
-            JavaRDD<Item> res = result.mapToPair(item -> new Tuple2<>(item.getXmlDocumentPosition(), item))
-                .groupByKey()
-                .values()
-                .map(it -> it.iterator().next());
+        // get unique items based on unique document position
+        JavaRDD<Item> res = result.mapToPair(item -> new Tuple2<>(item.getXmlDocumentPosition(), item))
+            .groupByKey()
+            .values()
+            .map(it -> it.iterator().next());
 
-            // sort because spark doesnt guarantee any ordering
-            return res.sortBy(Item::getXmlDocumentPosition, true, 1);
-
-        } else {
-            // TODO, currently we materialize when we encounter a predicate/seqlookup
-            // need to figure out how to do this properly
-            List<Item> items = childRDD.collect();
-            dynamicContext.getVariableValues().addVariableValue(Name.CONTEXT_ITEM, items);
-            items = this.rightIterator.materialize(dynamicContext);
-            JavaSparkContext sparkContext = SparkSessionManager.getInstance().getJavaSparkContext();
-            return sparkContext.parallelize(items);
-        }
+        // sort because spark doesnt guarantee any ordering
+        return res.sortBy(Item::getXmlDocumentPosition, true, 1);
     }
 
     @Override
@@ -109,9 +94,17 @@ public class SlashExprIterator extends HybridRuntimeIterator {
     private void setNextResult() {
         if (this.results == null) {
             List<Item> left = this.leftIterator.materialize(this.currentDynamicContextForLocalExecution);
-            this.currentDynamicContextForLocalExecution.getVariableValues()
-                .addVariableValue(Name.CONTEXT_ITEM, left);
-            this.results = this.rightIterator.materialize(this.currentDynamicContextForLocalExecution);
+            this.results = new ArrayList<>();
+            for (Item currentItem : left) {
+                this.currentDynamicContextForLocalExecution.getVariableValues()
+                    .addVariableValue(Name.CONTEXT_ITEM, Collections.singletonList(currentItem));
+                this.results.addAll(this.rightIterator.materialize(this.currentDynamicContextForLocalExecution));
+                this.currentDynamicContextForLocalExecution.getVariableValues().removeVariable(Name.CONTEXT_POSITION);
+            }
+            // take unique
+            this.results = new ArrayList<>(new LinkedHashSet<>(this.results));
+            // Sort values in document order.
+            this.results.sort(Item::compareXmlNode);
         }
         if (this.nextResultCounter < this.results.size()) {
             this.nextResult = this.results.get(nextResultCounter++);
