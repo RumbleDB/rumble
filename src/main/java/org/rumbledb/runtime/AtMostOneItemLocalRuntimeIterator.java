0c0c6600d335450cad5fb4cd3e7515ccb87a23de
Marco Schöb <marcoschoeb@gmail.com>
2025-02-13 15:26:12 +0100

    change --language to --default-language

--- src/main/java/org/rumbledb/runtime/AtMostOneItemLocalRuntimeIterator.java
+++ src/main/java/org/rumbledb/runtime/AtMostOneItemLocalRuntimeIterator.java
@@ -185,7 +185,7 @@ public abstract class AtMostOneItemLocalRuntimeIterator extends RuntimeIterator 
                 System.err.println(
                     "Note: effective boolean value of "
                         + (item.isObject() ? "Object " : "Array ")
-                        + "accessed which throws error in JSONiq 3.1 in alignment with Xquery 3.1 spec.\n If you want to revert to the old functionality use the --language jsoniq10 command line option"
+                        + "accessed which throws error in JSONiq 3.1 in alignment with Xquery 3.1 spec.\n If you want to revert to the old functionality use the --default-language jsoniq10 command line option"
                 );
             }
         }
