74c45bbdbecaf79ff5f9bed4dced5b3352015c92
Ghislain Fourny <gfourny@inf.ethz.ch>
2025-08-04 13:09:26 +0200

    Update ReturnClauseSparkIterator.java

--- src/main/java/org/rumbledb/runtime/flwor/clauses/ReturnClauseSparkIterator.java
+++ src/main/java/org/rumbledb/runtime/flwor/clauses/ReturnClauseSparkIterator.java
@@ -119,6 +119,7 @@ public class ReturnClauseSparkIterator extends HybridRuntimeIterator {
                     result = result.union(intermediateResult);
                 }
             }
+            this.child.close();
             if (result == null) {
                 return SparkSessionManager.getInstance().getJavaSparkContext().emptyRDD();
             }
