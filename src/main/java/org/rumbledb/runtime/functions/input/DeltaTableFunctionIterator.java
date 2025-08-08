4a384cfa98e1831b132d4df1f94ff40e14fb5b71
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2025-08-08 14:27:24 +0200

    Do not persist three of the columns.

--- src/main/java/org/rumbledb/runtime/functions/input/DeltaTableFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/input/DeltaTableFunctionIterator.java
@@ -9,6 +9,8 @@ import org.rumbledb.runtime.DataFrameRuntimeIterator;
 import org.rumbledb.runtime.RuntimeIterator;
 import sparksoniq.spark.SparkSessionManager;
 
+import static org.apache.spark.sql.functions.lit;
+
 import java.util.List;
 
 
@@ -30,6 +32,9 @@ public class DeltaTableFunctionIterator extends DataFrameRuntimeIterator {
 
         String selectQuery = "SELECT * FROM " + collectionName + " ORDER BY " + SparkSessionManager.rowOrderColumnName;
         Dataset<Row> dataFrame = SparkSessionManager.getInstance().getOrCreateSession().sql(selectQuery);
+        dataFrame = dataFrame.withColumn(SparkSessionManager.mutabilityLevelColumnName, lit(0));
+        dataFrame = dataFrame.withColumn(SparkSessionManager.pathInColumnName, lit(""));
+        dataFrame = dataFrame.withColumn(SparkSessionManager.tableLocationColumnName, lit(collectionName));
 
         // dataFrame.show();
 
