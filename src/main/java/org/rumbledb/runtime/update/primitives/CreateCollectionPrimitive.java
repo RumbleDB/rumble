f21b21b9f4e1e1b25df110656d3253dc091921cf
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2025-08-08 14:27:24 +0200

    Do not persist three of the columns.

--- src/main/java/org/rumbledb/runtime/update/primitives/CreateCollectionPrimitive.java
+++ src/main/java/org/rumbledb/runtime/update/primitives/CreateCollectionPrimitive.java
@@ -72,22 +72,11 @@ public class CreateCollectionPrimitive implements UpdatePrimitive {
             monotonically_increasing_id().cast("double")
         );
 
-        this.contents = this.contents.withColumn(SparkSessionManager.mutabilityLevelColumnName, lit(0));
-        this.contents = this.contents.withColumn(SparkSessionManager.pathInColumnName, lit(""));
-
         if (this.isTable) {
-            this.contents = this.contents.withColumn(
-                SparkSessionManager.tableLocationColumnName,
-                lit(this.collectionName)
-            );
             this.contents.write()
                 .format("delta")
                 .saveAsTable(this.collectionName);
         } else {
-            this.contents = this.contents.withColumn(
-                SparkSessionManager.tableLocationColumnName,
-                lit("delta.`" + this.collectionName + "`")
-            );
             this.contents.write()
                 .format("delta")
                 .option("path", "delta/" + this.collectionName)
