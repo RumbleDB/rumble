f4b168e0556fffb5b29b30b2d00de49d0cb1df32
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2025-08-12 16:25:59 +0200

    Fix parsing

--- src/main/java/org/rumbledb/expressions/update/TruncateCollectionExpression.java
+++ src/main/java/org/rumbledb/expressions/update/TruncateCollectionExpression.java
@@ -6,25 +6,34 @@ import org.rumbledb.expressions.AbstractNodeVisitor;
 import org.rumbledb.expressions.Expression;
 import org.rumbledb.expressions.Node;
 
+import static org.apache.spark.sql.functions.regexp_substr;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class TruncateCollectionExpression extends Expression {
     private Expression collectionName;
+    private boolean isTable;
 
     public TruncateCollectionExpression(
             Expression collectionName,
+            boolean isTable,
             ExceptionMetadata metadata
     ) {
         super(metadata);
         if (collectionName == null) {
             throw new OurBadException("collection must be identified for truncation.");
         }
+        this.isTable = isTable;
         this.collectionName = collectionName;
     }
 
     public Expression getCollectionName() {
         return this.collectionName;
+    }
+
+    public boolean isTable() {
+        return this.isTable;
     }
 
     @Override
