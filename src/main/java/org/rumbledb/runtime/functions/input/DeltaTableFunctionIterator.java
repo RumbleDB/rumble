0716aa79c95e26acbb18c8021d718ee3d53529fa
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2025-08-12 17:02:53 +0200

    Enable opening tables with no __rowId field.

--- src/main/java/org/rumbledb/runtime/functions/input/DeltaTableFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/input/DeltaTableFunctionIterator.java
@@ -2,6 +2,7 @@ package org.rumbledb.runtime.functions.input;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.types.StructField;
 import org.rumbledb.context.DynamicContext;
 import org.rumbledb.context.RuntimeStaticContext;
 import org.rumbledb.items.structured.JSoundDataFrame;
@@ -30,14 +31,35 @@ public class DeltaTableFunctionIterator extends DataFrameRuntimeIterator {
         RuntimeIterator collectionNameIterator = this.children.get(0);
         String collectionName = collectionNameIterator.materializeFirstItemOrNull(context).getStringValue();
 
-        String selectQuery = "SELECT * FROM " + collectionName + " ORDER BY " + SparkSessionManager.rowOrderColumnName;
-        Dataset<Row> dataFrame = SparkSessionManager.getInstance().getOrCreateSession().sql(selectQuery);
-        dataFrame = dataFrame.withColumn(SparkSessionManager.mutabilityLevelColumnName, lit(0));
-        dataFrame = dataFrame.withColumn(SparkSessionManager.pathInColumnName, lit(""));
-        dataFrame = dataFrame.withColumn(SparkSessionManager.tableLocationColumnName, lit(collectionName));
-
-        // dataFrame.show();
-
-        return new JSoundDataFrame(dataFrame);
+        Dataset<Row> dataFrame = SparkSessionManager.getInstance().getOrCreateSession().table(collectionName);
+        StructField[] fields = dataFrame.schema().fields();
+        boolean hasIntRowId = false;
+        for (org.apache.spark.sql.types.StructField field : fields) {
+            if (field.name().equals("__rowId") && field.dataType().typeName().equals("int")) {
+                hasIntRowId = true;
+                break;
+            }
+        }
+        boolean hasDoubleRowOrder = false;
+        for (org.apache.spark.sql.types.StructField field : fields) {
+            if (field.name().equals("__rowOrder") && field.dataType().typeName().equals("double")) {
+                hasDoubleRowOrder = true;
+                break;
+            }
+        }
+        if (!hasIntRowId) {
+            return new JSoundDataFrame(dataFrame);
+        } else if (hasDoubleRowOrder) {
+            dataFrame = dataFrame.orderBy(SparkSessionManager.rowOrderColumnName);
+            dataFrame = dataFrame.withColumn(SparkSessionManager.mutabilityLevelColumnName, lit(0));
+            dataFrame = dataFrame.withColumn(SparkSessionManager.pathInColumnName, lit(""));
+            dataFrame = dataFrame.withColumn(SparkSessionManager.tableLocationColumnName, lit(collectionName));
+            return new JSoundDataFrame(dataFrame);
+        } else {
+            dataFrame = dataFrame.withColumn(SparkSessionManager.mutabilityLevelColumnName, lit(0));
+            dataFrame = dataFrame.withColumn(SparkSessionManager.pathInColumnName, lit(""));
+            dataFrame = dataFrame.withColumn(SparkSessionManager.tableLocationColumnName, lit(collectionName));
+            return new JSoundDataFrame(dataFrame);
+        }
     }
 }
