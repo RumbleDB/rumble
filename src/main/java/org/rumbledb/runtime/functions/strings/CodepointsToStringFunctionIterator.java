5d463f8812656ea928aa3cd6bf0e7014f4f04a7e
EPMatt <30753195+EPMatt@users.noreply.github.com>
2026-03-16 12:00:39 +0000

    refactor(codepoints): streamline control character validation logic in CodepointsToStringFunctionIterator

--- src/main/java/org/rumbledb/runtime/functions/strings/CodepointsToStringFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/strings/CodepointsToStringFunctionIterator.java
@@ -75,30 +75,20 @@ public class CodepointsToStringFunctionIterator extends AtMostOneItemLocalRuntim
         return ItemFactory.getInstance().createStringItem(stringBuilder.toString());
     }
 
-    private static boolean isPermittedControlCharacter(int codepoint, String xmlVersion) {
-        boolean isC0 = (codepoint >= 0 && codepoint <= 31);
-        boolean isC1 = (codepoint >= 127 && codepoint <= 159);
-
-        if (!(isC0 || isC1)) {
-            return false;
-        }
-
-        if (xmlVersion.equals("1.0")) {
-            return codepoint == 9 || codepoint == 10 || codepoint == 13;
-        } else {
-            return codepoint != 0;
-        }
-    }
-
     private static boolean isValidCodePoint(int codepoint, String xmlVersion) {
-        if (codepoint < 0 || codepoint > 1114111)
+        if (codepoint < 0 || codepoint > 1114111) {
             return false;
+        }
 
         boolean isC0 = (codepoint >= 0 && codepoint <= 31);
         boolean isC1 = (codepoint >= 127 && codepoint <= 159);
 
         if (isC0 || isC1) {
-            return isPermittedControlCharacter(codepoint, xmlVersion);
+            if (xmlVersion.equals("1.0")) {
+                return codepoint == 9 || codepoint == 10 || codepoint == 13;
+            } else {
+                return codepoint != 0;
+            }
         }
 
         return (codepoint <= 55295)
