fcb05c511a74afe642ab562adb5a83974b58d6fe
EPMatt <30753195+EPMatt@users.noreply.github.com>
2026-03-18 09:54:09 +0000

    feat(functions): add support for array:reverse function

---
+++ src/main/java/org/rumbledb/runtime/functions/arrays/ArrayReverseFunctionIterator.java
@@ -0,0 +1,147 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.rumbledb.runtime.functions.arrays;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.rumbledb.api.Item;
+import org.rumbledb.context.DynamicContext;
+import org.rumbledb.context.RuntimeStaticContext;
+import org.rumbledb.exceptions.IteratorFlowException;
+import org.rumbledb.exceptions.MoreThanOneItemException;
+import org.rumbledb.exceptions.NoItemException;
+import org.rumbledb.exceptions.OurBadException;
+import org.rumbledb.exceptions.UnexpectedTypeException;
+import org.rumbledb.items.ItemFactory;
+import org.rumbledb.items.structured.JSoundDataFrame;
+import org.rumbledb.runtime.HybridRuntimeIterator;
+import org.rumbledb.runtime.RuntimeIterator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayReverseFunctionIterator extends HybridRuntimeIterator {
+
+    private static final long serialVersionUID = 1L;
+
+    private final RuntimeIterator arrayIterator;
+    private Item resultItem;
+    private boolean hasProducedResult;
+
+    public ArrayReverseFunctionIterator(
+            List<RuntimeIterator> arguments,
+            RuntimeStaticContext staticContext
+    ) {
+        super(arguments, staticContext);
+        if (arguments.size() != 1) {
+            throw new OurBadException("array:reverse must have exactly one argument.");
+        }
+        this.arrayIterator = arguments.get(0);
+        this.resultItem = null;
+        this.hasProducedResult = false;
+    }
+
+    @Override
+    protected void openLocal() {
+        this.arrayIterator.open(this.currentDynamicContextForLocalExecution);
+        initializeResult(this.currentDynamicContextForLocalExecution);
+        this.hasNext = this.resultItem != null;
+        this.hasProducedResult = false;
+    }
+
+    private void initializeResult(DynamicContext context) {
+        Item arrayItem;
+        try {
+            arrayItem = this.arrayIterator.materializeExactlyOneItem(context);
+        } catch (NoItemException e) {
+            this.resultItem = null;
+            return;
+        } catch (MoreThanOneItemException e) {
+            throw new UnexpectedTypeException(
+                    "array:reverse expects exactly one array argument.",
+                    getMetadata()
+            );
+        }
+
+        if (!arrayItem.isArray()) {
+            throw new UnexpectedTypeException(
+                    "Type error; argument to array:reverse must be an array.",
+                    getMetadata()
+            );
+        }
+
+        List<List<Item>> memberSequences = arrayItem.getMemberSequences();
+        List<List<Item>> reversedMemberSequences = new ArrayList<>(memberSequences);
+        Collections.reverse(reversedMemberSequences);
+
+        this.resultItem = ItemFactory.getInstance()
+            .createArrayFromMemberSequences(reversedMemberSequences, false);
+    }
+
+    @Override
+    protected boolean hasNextLocal() {
+        return this.hasNext;
+    }
+
+    @Override
+    protected Item nextLocal() {
+        if (!this.hasNext || this.hasProducedResult) {
+            throw new IteratorFlowException(RuntimeIterator.FLOW_EXCEPTION_MESSAGE, getMetadata());
+        }
+        this.hasProducedResult = true;
+        this.hasNext = false;
+        return this.resultItem;
+    }
+
+    @Override
+    protected void resetLocal() {
+        this.arrayIterator.reset(this.currentDynamicContextForLocalExecution);
+        initializeResult(this.currentDynamicContextForLocalExecution);
+        this.hasNext = this.resultItem != null;
+        this.hasProducedResult = false;
+    }
+
+    @Override
+    protected void closeLocal() {
+        if (this.arrayIterator.isOpen()) {
+            this.arrayIterator.close();
+        }
+        this.resultItem = null;
+        this.hasProducedResult = false;
+    }
+
+    @Override
+    public JavaRDD<Item> getRDDAux(DynamicContext dynamicContext) {
+        throw new OurBadException(
+                "array:reverse is currently supported only in local execution mode."
+        );
+    }
+
+    @Override
+    protected boolean implementsDataFrames() {
+        return false;
+    }
+
+    @Override
+    public JSoundDataFrame getDataFrame(DynamicContext dynamicContext) {
+        throw new OurBadException(
+                "array:reverse is currently supported only in local execution mode."
+        );
+    }
+}
+
