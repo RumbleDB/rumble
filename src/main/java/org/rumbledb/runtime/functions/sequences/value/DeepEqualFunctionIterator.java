d995cccfce8917f3606157ee4d2310391e4f91cc
EPMatt <30753195+EPMatt@users.noreply.github.com>
2026-03-21 13:13:32 +0000

    feat(functions): add support for array:sort function

--- src/main/java/org/rumbledb/runtime/functions/sequences/value/DeepEqualFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/sequences/value/DeepEqualFunctionIterator.java
@@ -131,6 +131,18 @@ public class DeepEqualFunctionIterator extends AtMostOneItemLocalRuntimeIterator
             return true;
         }
 
+        if (item1.isArray() && item2.isArray()) {
+            if (item1.getSize() != item2.getSize()) {
+                return false;
+            }
+            for (int i = 0; i < item1.getSize(); i++) {
+                if (!checkDeepEqual(item1.getSequenceAt(i), item2.getSequenceAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         // If both items are nodes, use node-specific deep-equal logic
         if (item1.isNode() && item2.isNode()) {
             return checkNodesDeepEqual(item1, item2);
