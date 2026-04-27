81874603b23172aa8e2a09d5ac336dee2926d6be
Henrik Pätzold <62308300+HenrikPaetzold@users.noreply.github.com>
2026-04-27 12:25:03 +0200

    detect tail recursion in nested conditional expressions

--- src/main/java/org/rumbledb/compiler/TailCallOptimizationVisitor.java
+++ src/main/java/org/rumbledb/compiler/TailCallOptimizationVisitor.java
@@ -1,6 +1,5 @@
 package org.rumbledb.compiler;
 
-
 import org.rumbledb.expressions.Expression;
 import org.rumbledb.expressions.Node;
 import org.rumbledb.expressions.control.ConditionalExpression;
@@ -9,59 +8,49 @@ import org.rumbledb.expressions.primary.FunctionCallExpression;
 import org.rumbledb.expressions.primary.InlineFunctionExpression;
 
 public class TailCallOptimizationVisitor extends CloneVisitor {
+
     @Override
     public Node visitFunctionDeclaration(FunctionDeclaration expression, Node argument) {
         FunctionDeclaration fd = new FunctionDeclaration(
                 (InlineFunctionExpression) visit(expression.getExpression(), argument),
                 expression.getMetadata()
         );
-        InlineFunctionExpression inlineFunctionExpression = (InlineFunctionExpression) fd.getExpression();
+
+        InlineFunctionExpression inlineFunctionExpression =
+            (InlineFunctionExpression) fd.getExpression();
+
         Expression body = inlineFunctionExpression.getBody().getExpression();
-        if (body instanceof ConditionalExpression) {
-            if (((ConditionalExpression) body).getBranch() instanceof FunctionCallExpression) {
-                if (
-                    ((FunctionCallExpression) ((ConditionalExpression) body).getBranch()).getFunctionIdentifier()
-                        .equals(fd.getFunctionIdentifier())
-                ) {
-                    if (
-                        ((FunctionCallExpression) ((ConditionalExpression) body).getBranch()).getArguments()
-                            .size() == fd.getChildren().size()
-                    ) {
-                        if (
-                            !((FunctionCallExpression) ((ConditionalExpression) body).getBranch())
-                                .isPartialApplication()
-                        ) {
-                            ((FunctionCallExpression) ((ConditionalExpression) body).getBranch())
-                                .setTailCallOptimization(
-                                    true
-                                );
-                        }
-                    }
-                }
-            }
-            if (((ConditionalExpression) body).getElseBranch() instanceof FunctionCallExpression) {
-                if (
-                    ((FunctionCallExpression) ((ConditionalExpression) body).getElseBranch()).getFunctionIdentifier()
-                        .equals(fd.getFunctionIdentifier())
-                ) {
-                    if (
-                        ((FunctionCallExpression) ((ConditionalExpression) body).getElseBranch()).getArguments()
-                            .size() == fd.getChildren().size()
-                    ) {
-                        if (
-                            !((FunctionCallExpression) ((ConditionalExpression) body).getElseBranch())
-                                .isPartialApplication()
-                        ) {
-                            System.err.println("Set tail call optimization for function " + fd.getFunctionIdentifier());
-                            ((FunctionCallExpression) ((ConditionalExpression) body).getElseBranch())
-                                .setTailCallOptimization(
-                                    true
-                                );
-                        }
-                    }
-                }
-            }
-        }
+
+        markTailCalls(body, fd);
+
         return fd;
+    }
+
+    private void markTailCalls(Expression expression, FunctionDeclaration fd) {
+        if (expression == null) {
+            return;
+        }
+
+        if (expression instanceof FunctionCallExpression) {
+            FunctionCallExpression functionCall = (FunctionCallExpression) expression;
+            if (isTailRecursiveCall(functionCall, fd)) {
+                // System.err.println("Set tail call optimization for function " + fd.getFunctionIdentifier());
+                functionCall.setTailCallOptimization(true);
+            }
+
+            return;
+        }
+
+        if (expression instanceof ConditionalExpression) {
+            ConditionalExpression conditional = (ConditionalExpression) expression;
+            markTailCalls(conditional.getBranch(), fd);
+            markTailCalls(conditional.getElseBranch(), fd);
+        }
+    }
+
+    private boolean isTailRecursiveCall(FunctionCallExpression functionCall, FunctionDeclaration fd) {
+        return functionCall.getFunctionIdentifier().equals(fd.getFunctionIdentifier())
+            && functionCall.getArguments().size() == fd.getFunctionIdentifier().getArity()
+            && !functionCall.isPartialApplication();
     }
 }
