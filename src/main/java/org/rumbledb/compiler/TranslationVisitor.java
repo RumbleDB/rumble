ebebaea4245320f2ac9c272d558c5113829335d3
Henrik Pätzold <62308300+HenrikPaetzold@users.noreply.github.com>
2026-05-06 18:22:00 +0200

    debugging

--- src/main/java/org/rumbledb/compiler/TranslationVisitor.java
+++ src/main/java/org/rumbledb/compiler/TranslationVisitor.java
@@ -1608,6 +1608,10 @@ public class TranslationVisitor extends JsoniqBaseVisitor<Node> {
                         createMetadataFromContext(ctx)
                 );
             } else {
+                System.err.println("Unknown postfix child:");
+                System.err.println("Class: " + child.getClass().getName());
+                System.err.println("Text:  " + child.getText());
+                System.err.println("Tree:  " + child.toStringTree());
                 throw new OurBadException("Unrecognized postfix expression found.");
             }
         }
