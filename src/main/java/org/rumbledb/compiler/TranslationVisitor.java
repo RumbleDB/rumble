c222c1de9f3e4531eff1a9b8f02a191a99511df9
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2026-05-12 14:03:57 +0200

    Fix tests.

--- src/main/java/org/rumbledb/compiler/TranslationVisitor.java
+++ src/main/java/org/rumbledb/compiler/TranslationVisitor.java
@@ -2696,19 +2696,24 @@ public class TranslationVisitor extends JsoniqParserBaseVisitor<Node> {
         if (name.getNamespace().equals(Name.JSONIQ_DEFAULT_FUNCTION_NS)) {
             typeName = Name.createVariableInDefaultTypeNamespace(name.getLocalName());
         }
-        ItemType targetType = BuiltinTypesCatalogue.getItemTypeByName(name);
-        // In XQuery, no constructor function exists for xs:NOTATION or xs:anyAtomicType.
-        // Keep these as unresolved function calls to raise XPST0017 as required.
         if (
             BuiltinTypesCatalogue.typeExists(typeName)
                 && children.size() == 1
                 && !name.equals(Name.createVariableInDefaultFunctionNamespace("boolean"))
         ) {
-            return new CastExpression(
-                    children.get(0),
-                    new SequenceType(targetType, SequenceType.Arity.OneOrZero),
-                    metadata
-            );
+            ItemType targetType = BuiltinTypesCatalogue.getItemTypeByName(name);
+            // In XQuery, no constructor function exists for xs:NOTATION or xs:anyAtomicType.
+            // Keep these as unresolved function calls to raise XPST0017 as required.
+            if (
+                !targetType.equals(BuiltinTypesCatalogue.NOTATIONItem)
+                    && !targetType.equals(BuiltinTypesCatalogue.atomicItem)
+            ) {
+                return new CastExpression(
+                        children.get(0),
+                        new SequenceType(targetType, SequenceType.Arity.OneOrZero),
+                        metadata
+                );
+            }
         }
         return new FunctionCallExpression(
                 name,
