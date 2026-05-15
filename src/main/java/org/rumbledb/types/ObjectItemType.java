918638ad8b327f30fd61f6c6594a06a17d4d4448
Ghislain Fourny <ghislain.fourny@inf.ethz.ch>
2026-05-15 17:14:32 +0200

    Fix merging object schemas.

--- src/main/java/org/rumbledb/types/ObjectItemType.java
+++ src/main/java/org/rumbledb/types/ObjectItemType.java
@@ -1,5 +1,16 @@
 package org.rumbledb.types;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.apache.commons.collections.ListUtils;
 import org.rumbledb.api.Item;
 import org.rumbledb.config.RumbleRuntimeConfiguration;
@@ -9,8 +20,6 @@ import org.rumbledb.context.StaticContext;
 import org.rumbledb.exceptions.ExceptionMetadata;
 import org.rumbledb.exceptions.InvalidSchemaException;
 import org.rumbledb.exceptions.OurBadException;
-
-import java.util.*;
 
 public class ObjectItemType implements ItemType {
 
@@ -323,16 +332,23 @@ public class ObjectItemType implements ItemType {
      */
     private Map<String, FieldDescriptor> mergeObjectContent(ObjectItemType other) {
         Map<String, FieldDescriptor> merged = new LinkedHashMap<>();
-        for (Map.Entry<String, FieldDescriptor> entry : this.getObjectContentFacet().entrySet()) {
-            merged.put(entry.getKey(), FieldDescriptor.copy(entry.getValue()));
-        }
-        for (Map.Entry<String, FieldDescriptor> entry : other.getObjectContentFacet().entrySet()) {
-            FieldDescriptor existing = merged.get(entry.getKey());
-            if (existing == null) {
-                merged.put(entry.getKey(), FieldDescriptor.copy(entry.getValue()));
-                continue;
+        Set<String> fields = new TreeSet<>();
+        fields.addAll(this.getObjectContentFacet().keySet());
+        fields.addAll(other.getObjectContentFacet().keySet());
+        for (String field : fields) {
+            FieldDescriptor fd1 = this.getObjectContentFacet().get(field);
+            FieldDescriptor fd2 = other.getObjectContentFacet().get(field);
+            if (fd1 != null && fd2 != null) {
+                merged.put(field, mergeDescriptors(fd1, fd2));
+            } else if (fd1 != null) {
+                FieldDescriptor fieldDescriptor = FieldDescriptor.copy(fd1);
+                fieldDescriptor.setRequired(false);
+                merged.put(field, fieldDescriptor);
+            } else {
+                FieldDescriptor fieldDescriptor = FieldDescriptor.copy(fd2);
+                fieldDescriptor.setRequired(false);
+                merged.put(field, fieldDescriptor);
             }
-            merged.put(entry.getKey(), mergeDescriptors(existing, entry.getValue()));
         }
         return merged;
     }
