446b6c50b251588a4c5c7e9e0fe3d39adf039582
Henrik Paetzold <hpaetzold@student.ethz.ch>
2026-05-20 13:39:26 +0200

    Add skeletons for additional built-in functions

---
+++ src/main/java/org/rumbledb/runtime/functions/xml/ParseXMLFragmentFunctionIterator.java
@@ -0,0 +1,26 @@
+package org.rumbledb.runtime.functions.xml;
+
+import org.rumbledb.api.Item;
+import org.rumbledb.context.DynamicContext;
+import org.rumbledb.context.RuntimeStaticContext;
+import org.rumbledb.exceptions.UnimplementedFunctionException;
+import org.rumbledb.runtime.AtMostOneItemLocalRuntimeIterator;
+import org.rumbledb.runtime.RuntimeIterator;
+
+import java.util.List;
+
+public class ParseXMLFragmentFunctionIterator extends AtMostOneItemLocalRuntimeIterator {
+    private static final long serialVersionUID = 1L;
+
+    public ParseXMLFragmentFunctionIterator(
+            List<RuntimeIterator> arguments,
+            RuntimeStaticContext staticContext
+    ) {
+        super(arguments, staticContext);
+    }
+
+    @Override
+    public Item materializeFirstItemOrNull(DynamicContext context) {
+        throw new UnimplementedFunctionException("fn:parse-xml-fragment", getMetadata());
+    }
+}
