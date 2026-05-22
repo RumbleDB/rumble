28e3202407629cd483406b05acdcbfc50df178f4
Henrik Pätzold <62308300+HenrikPaetzold@users.noreply.github.com>
2026-05-22 11:56:59 +0200

    implement fn:function-arity

--- src/main/java/org/rumbledb/runtime/functions/typing/FunctionArityFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/typing/FunctionArityFunctionIterator.java
@@ -3,10 +3,12 @@ package org.rumbledb.runtime.functions.typing;
 import org.rumbledb.api.Item;
 import org.rumbledb.context.DynamicContext;
 import org.rumbledb.context.RuntimeStaticContext;
-import org.rumbledb.exceptions.UnimplementedFunctionException;
+import org.rumbledb.items.FunctionItem;
+import org.rumbledb.items.ItemFactory;
 import org.rumbledb.runtime.AtMostOneItemLocalRuntimeIterator;
 import org.rumbledb.runtime.RuntimeIterator;
 
+import java.math.BigInteger;
 import java.util.List;
 
 public class FunctionArityFunctionIterator extends AtMostOneItemLocalRuntimeIterator {
@@ -21,6 +23,8 @@ public class FunctionArityFunctionIterator extends AtMostOneItemLocalRuntimeIter
 
     @Override
     public Item materializeFirstItemOrNull(DynamicContext context) {
-        throw new UnimplementedFunctionException("fn:function-arity", getMetadata());
+        FunctionItem functionItem = (FunctionItem) this.children.get(0).materializeFirstItemOrNull(context);
+
+        return ItemFactory.getInstance().createIntegerItem(BigInteger.valueOf(functionItem.getParameterNames().size()));
     }
 }
