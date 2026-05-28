ba1dcdc973ec36721752311eb20521e2da55556a
Henrik Pätzold <62308300+HenrikPaetzold@users.noreply.github.com>
2026-05-28 14:18:13 +0200

    refine fn:local-name signature

--- src/main/java/org/rumbledb/context/BuiltinFunctionCatalogue.java
+++ src/main/java/org/rumbledb/context/BuiltinFunctionCatalogue.java
@@ -4234,7 +4234,7 @@ public class BuiltinFunctionCatalogue {
 
     static final BuiltinFunction local_name_with_arg = createBuiltinFunction(
         new Name(Name.FN_NS, "fn", "local-name"),
-        "item?",
+        "node()?",
         "string",
         LocalNameFunctionIterator.class,
         BuiltinFunction.BuiltinFunctionExecutionMode.LOCAL
