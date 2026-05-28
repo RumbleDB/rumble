d36c3f81d32363a1e80e61073b228726ad0c2617
Henrik Pätzold <62308300+HenrikPaetzold@users.noreply.github.com>
2026-05-28 14:36:30 +0200

    implement fn:local-name

--- src/main/java/org/rumbledb/runtime/functions/xml/LocalNameFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/xml/LocalNameFunctionIterator.java
@@ -2,8 +2,10 @@ package org.rumbledb.runtime.functions.xml;
 
 import org.rumbledb.api.Item;
 import org.rumbledb.context.DynamicContext;
+import org.rumbledb.context.Name;
 import org.rumbledb.context.RuntimeStaticContext;
-import org.rumbledb.exceptions.UnimplementedFunctionException;
+import org.rumbledb.exceptions.UnexpectedTypeException;
+import org.rumbledb.items.ItemFactory;
 import org.rumbledb.runtime.AtMostOneItemLocalRuntimeIterator;
 import org.rumbledb.runtime.RuntimeIterator;
 
@@ -21,6 +23,26 @@ public class LocalNameFunctionIterator extends AtMostOneItemLocalRuntimeIterator
 
     @Override
     public Item materializeFirstItemOrNull(DynamicContext context) {
-        throw new UnimplementedFunctionException("fn:local-name", getMetadata());
+        Item node = getContextNode(context);
+        if (node == null) {
+            return ItemFactory.getInstance().createStringItem("");
+        }
+        if (!node.isNode()) {
+            throw new UnexpectedTypeException("The argument must be a reference to an XML node", getMetadata());
+        }
+        Name nodeName = node.nodeName();
+        if (nodeName == null) {
+            return ItemFactory.getInstance().createStringItem("");
+        }
+        return ItemFactory.getInstance().createStringItem(nodeName.getLocalName());
+    }
+
+    private Item getContextNode(DynamicContext context) {
+        if (!this.children.isEmpty()) {
+            return this.children.get(0).materializeFirstItemOrNull(context);
+        }
+        return context.getVariableValues()
+            .getLocalVariableValue(Name.CONTEXT_ITEM, getMetadata())
+            .get(0);
     }
 }
