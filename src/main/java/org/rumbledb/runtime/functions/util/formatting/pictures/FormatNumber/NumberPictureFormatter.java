52542f5c5b0988088f528dc04457b70476810f79
Jimmy Cai <jimmy@cai.im>
2026-06-25 14:56:36 +0200

    Remove unused type parameter in AdjustedNumber class inside NumberPictureFormatter

--- src/main/java/org/rumbledb/runtime/functions/util/formatting/pictures/FormatNumber/NumberPictureFormatter.java
+++ src/main/java/org/rumbledb/runtime/functions/util/formatting/pictures/FormatNumber/NumberPictureFormatter.java
@@ -255,10 +255,10 @@ public class NumberPictureFormatter {
             }
 
             if (Double.isInfinite(value)) {
-                return new AdjustedNumber(resolvedNumber.type, null, true);
+                return new AdjustedNumber(null, true);
             }
 
-            return new AdjustedNumber(resolvedNumber.type, BigDecimal.valueOf(value), false);
+            return new AdjustedNumber(BigDecimal.valueOf(value), false);
         }
 
         if (ResolvedNumber.FLOAT.equals(resolvedNumber.type)) {
@@ -270,10 +270,10 @@ public class NumberPictureFormatter {
             }
 
             if (Float.isInfinite(value)) {
-                return new AdjustedNumber(resolvedNumber.type, null, true);
+                return new AdjustedNumber(null, true);
             }
 
-            return new AdjustedNumber(resolvedNumber.type, BigDecimal.valueOf(floatToBigDecimal(value)), false);
+            return new AdjustedNumber(BigDecimal.valueOf(floatToBigDecimal(value)), false);
         }
 
         BigDecimal value = resolvedNumber.decimalValue;
@@ -283,7 +283,7 @@ public class NumberPictureFormatter {
             value = value.multiply(BigDecimal.valueOf(1000));
         }
 
-        return new AdjustedNumber(resolvedNumber.type, value, false);
+        return new AdjustedNumber(value, false);
     }
 
     private static MantissaExponentPair computeMantissaExponent(
@@ -334,12 +334,10 @@ public class NumberPictureFormatter {
     }
 
     private static final class AdjustedNumber {
-        final String type;
         final BigDecimal value;
         final boolean infinite;
 
-        AdjustedNumber(String type, BigDecimal value, boolean infinite) {
-            this.type = type;
+        AdjustedNumber(BigDecimal value, boolean infinite) {
             this.value = value;
             this.infinite = infinite;
         }
