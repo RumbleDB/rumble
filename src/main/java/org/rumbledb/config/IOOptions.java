367c0dd7f82f21fb9516d3dadb592b52cf155482
Jimmy Cai <jimmy@cai.im>
2026-06-26 19:37:26 +0200

    Add IOOptions class for configuration management

---
+++ src/main/java/org/rumbledb/config/IOOptions.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.rumbledb.config;
+
+import java.util.List;
+
+public class IOOptions {
+    /**
+     * Allowed URI prefixes for read/write (with I/O functions to read data)
+     */
+    private List<String> allowedPrefixes;
+
+    /**
+     * Input format for reading from standard input.
+     */
+    private String inputFormat;
+
+    /**
+     * Number of output partitions to write to the output path.
+     */
+    private int numberOfOutputPartitions;
+
+    /**
+     * Path from which the JSONiq or XQuery query is to be read.
+     */
+    private String queryPath;
+
+    /**
+     * Path to which the output path should be written.
+     */
+    private String outputPath;
+
+    /**
+     * Log path
+     */
+    private String logPath;
+
+    /**
+     * Query that was passed from the command line
+     * (This should be removed later when we have separate classes for CLI and API)
+     */
+    private String query;
+
+    /**
+     * Current shell filter for post-processing output (e.g. JSON beautifier)
+     */
+    private String shell;
+}
