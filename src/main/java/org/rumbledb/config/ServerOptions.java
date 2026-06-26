9087dcf90bb877f0c65cb6b067a10dbc6a303693
Jimmy Cai <jimmy@cai.im>
2026-06-26 19:54:01 +0200

    Add ServerOptions class for configuration of server settings

---
+++ src/main/java/org/rumbledb/config/ServerOptions.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.rumbledb.config;
+
+public class ServerOptions {
+    /**
+     * Host name when used in server mode.
+     */
+    private String host;
+
+    /**
+     * Port number when used in server mode.
+     */
+    private int port;
+
+    /**
+     * Application should start in shell mode.
+     */
+    private boolean isShell;
+
+    /**
+     * Application should start in server mode.
+     */
+    private boolean isServer;
+}
