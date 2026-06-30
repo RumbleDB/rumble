7ddf020db57f7b60f92d4bf657125012527da9c8
Jimmy Cai <jimmy@cai.im>
2026-06-30 21:43:55 +0200

    Add split option for allowed URI prefixes in AccessArguments
    Old syntax is now supported:
    
    `rumbledb run --allowed-uri-prefixes "file:///tmp;hdfs:///data"`

--- src/main/java/org/rumbledb/cli/arguments/AccessArguments.java
+++ src/main/java/org/rumbledb/cli/arguments/AccessArguments.java
@@ -8,6 +8,7 @@ import picocli.CommandLine.Option;
 public final class AccessArguments {
     @Option(
         names = { "--allowed-uri-prefixes" },
+        split = ";",
         description = "Allowed URI prefixes for read/write (with I/O functions to read data)"
     )
     private List<String> allowedPrefixes = new ArrayList<>();
