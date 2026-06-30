8e2dfb8e9895522c7fb022034fbc74d12a8d172e
Jimmy Cai <jimmy@cai.im>
2026-06-30 23:27:06 +0200

    Add new configuration model classes for Rumble

---
+++ src/main/java/org/rumbledb/config/model/BindingsConfig.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.rumbledb.config.model;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.rumbledb.api.Item;
+import org.rumbledb.context.Name;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Value;
+import lombok.experimental.Accessors;
+
+@Value
+@Builder(toBuilder = true)
+@Accessors(fluent = true)
+public class BindingsConfig {
+    @Default
+    private Map<Name, List<Item>> externalVariableValues = new HashMap<>();
+
+    @Default
+    private Map<Name, String> unparsedExternalVariableValues = new HashMap<>();
+
+    @Default
+    private Map<Name, String> externalVariableValuesReadFromFiles = new HashMap<>();
+
+    @Default
+    private Map<Name, Dataset<Row>> externalVariableValuesReadFromDataFrames = new HashMap<>();
+
+    @Default
+    private Set<Name> externalVariablesReadFromStandardInput = new HashSet<>();
+
+    @Default
+    private Map<Name, String> externalVariablesInputFormats = new HashMap<>();
+
+    public List<Item> getExternalVariableValue(Name name) {
+        return this.externalVariableValues.get(name);
+    }
+
+    public String getUnparsedExternalVariableValue(Name name) {
+        return this.unparsedExternalVariableValues.get(name);
+    }
+
+    public String getExternalVariableValueReadFromFile(Name name) {
+        return this.externalVariableValuesReadFromFiles.get(name);
+    }
+
+    public Dataset<Row> getExternalVariableValueReadFromDataFrame(Name name) {
+        return this.externalVariableValuesReadFromDataFrames.get(name);
+    }
+
+    public boolean readFromStandardInput(Name variableName) {
+        return this.externalVariablesReadFromStandardInput.contains(variableName);
+    }
+
+    public String getInputFormat(Name variableName) {
+        return this.externalVariablesInputFormats.get(variableName);
+    }
+
+    public List<Name> getExternalVariablesReadFromDataFrames() {
+        return new ArrayList<>(this.externalVariableValuesReadFromDataFrames.keySet());
+    }
+}
---
+++ src/main/java/org/rumbledb/config/model/SemanticsConfig.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.rumbledb.config.model;
+
+import java.util.Objects;
+
+import org.rumbledb.exceptions.CliException;
+
+import lombok.Builder;
+import lombok.Value;
+import lombok.experimental.Accessors;
+
+@Value
+@Accessors(fluent = true)
+public class SemanticsConfig {
+    public static final boolean DEFAULT_DATES_WITH_TIMEZONE = false;
+    public static final boolean DEFAULT_LAX_JSON_NULL_VALIDATION = true;
+    public static final String DEFAULT_QUERY_LANGUAGE = "jsoniq10";
+    public static final String DEFAULT_XML_VERSION = "1.1";
+
+    private boolean datesWithTimeZone;
+    private boolean laxJSONNullValidation;
+    private String queryLanguage;
+    private String xmlVersion;
+    private String staticBaseUri;
+
+    @Builder(toBuilder = true)
+    private SemanticsConfig(
+            Boolean datesWithTimeZone,
+            Boolean laxJSONNullValidation,
+            String queryLanguage,
+            String xmlVersion,
+            String staticBaseUri
+    ) {
+        this.datesWithTimeZone = Objects.requireNonNullElse(datesWithTimeZone, DEFAULT_DATES_WITH_TIMEZONE);
+        this.laxJSONNullValidation = Objects.requireNonNullElse(
+            laxJSONNullValidation,
+            DEFAULT_LAX_JSON_NULL_VALIDATION
+        );
+        this.queryLanguage = Objects.requireNonNullElse(queryLanguage, DEFAULT_QUERY_LANGUAGE);
+        this.xmlVersion = Objects.requireNonNullElse(xmlVersion, DEFAULT_XML_VERSION);
+        this.staticBaseUri = staticBaseUri;
+    }
+
+    private static String normalizeXmlVersion(String xmlVersion) {
+        if (xmlVersion == null) {
+            return DEFAULT_XML_VERSION;
+        }
+
+        String normalized = xmlVersion.trim();
+        if ("1.0".equals(normalized) || "1.1".equals(normalized)) {
+            return normalized;
+        }
+
+        throw new CliException(
+                "Argument --xml-version must be \"1.0\" or \"1.1\" (was: " + xmlVersion + ")."
+        );
+    }
+
+    public static class SemanticsConfigBuilder {
+        public SemanticsConfigBuilder xmlVersion(String xmlVersion) {
+            this.xmlVersion = normalizeXmlVersion(xmlVersion);
+            return this;
+        }
+    }
+}
