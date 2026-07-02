f16da3b5ab9e441623931eca6a9dec9e6173823a
Jimmy Cai <jimmy@cai.im>
2026-07-02 20:25:25 +0200

    Implement CLIBindings class and update BindingsArguments to return instance of CLIBindings

--- src/main/java/org/rumbledb/cli/arguments/BindingsArguments.java
+++ src/main/java/org/rumbledb/cli/arguments/BindingsArguments.java
@@ -1,26 +1,27 @@
 package org.rumbledb.cli.arguments;
 
+import org.rumbledb.cli.bindings.CLIBindings;
+import org.rumbledb.cli.bindings.FileValue;
+import org.rumbledb.cli.bindings.LexicalValue;
+import org.rumbledb.cli.bindings.StandardInputValue;
+import org.rumbledb.cli.bindings.VariableSource;
+import org.rumbledb.context.Name;
+import org.rumbledb.exceptions.CliException;
+
+import java.net.URI;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Option;
 
 public final class BindingsArguments {
-    private static final String DEFAULT_CONTEXT_ITEM_INPUT_FORMAT = "json";
-
     @ArgGroup(exclusive = true, multiplicity = "0..1")
-    private ContextItemSource contextItemSource = new ContextItemSource();
+    private ContextItemSource contextItemSource;
 
     private static final class ContextItemSource {
-        @ArgGroup(exclusive = true, multiplicity = "1")
-        private ContextItemValue contextItemValue = new ContextItemValue();
-
-        @ArgGroup(exclusive = true, multiplicity = "1")
-        private ContextItemInput contextItemInput = new ContextItemInput();
-    }
-
-    private static final class ContextItemValue {
         @Option(
             names = { "-I", "--context-item" },
             paramLabel = "value",
@@ -31,15 +32,13 @@ public final class BindingsArguments {
             }
         )
         private String value;
-    }
 
-    private static final class ContextItemInput {
         @Option(
             names = { "-i", "--context-item-input" },
             paramLabel = "path",
-            description = "Reads the context item value from the standard input."
+            description = "Reads the context item from a file, or from standard input when the path is '-'."
         )
-        private String value;
+        private String input;
     }
 
     @Option(
@@ -58,12 +57,66 @@ public final class BindingsArguments {
                 + "\"declare variable $foo external;\""
         }
     )
-    private Map<String, String> variables = new HashMap<>();
+    private Map<String, String> literalVariables = new HashMap<>();
 
     @Option(
         names = "--variable-from-file",
         paramLabel = "name=path",
         description = "Initializes a global variable with a value read from the supplied file."
     )
-    private Map<String, String> variablesFromFiles = new HashMap<>();
+    private Map<String, String> fileVariables = new HashMap<>();
+
+    /**
+     * Converts parsed command-line arguments without reading or interpreting their values.
+     */
+    public CLIBindings toCliBindings() {
+        // Check that we don't have duplicated variables defined from different sources
+
+        Set<String> duplicatedVariables = new HashSet<>(literalVariables.keySet());
+        duplicatedVariables.retainAll(fileVariables.keySet());
+
+        if (!duplicatedVariables.isEmpty()) {
+            throw new CliException(
+                    "Cannot define the same variable from both --variable and --variable-from-file: "
+                        + String.join(", ", duplicatedVariables)
+            );
+        }
+
+        Map<Name, VariableSource> variables = new HashMap<>();
+
+        for (Map.Entry<String, String> entry : literalVariables.entrySet()) {
+            variables.put(Name.createVariableInNoNamespace(entry.getKey()), new LexicalValue(entry.getValue()));
+        }
+
+        for (Map.Entry<String, String> entry : fileVariables.entrySet()) {
+            variables.put(
+                Name.createVariableInNoNamespace(entry.getKey()),
+                new FileValue(
+                        parseLocation(entry.getValue())
+                )
+            );
+        }
+
+        if (contextItemSource != null) {
+            if (contextItemSource.value != null) {
+                variables.put(Name.CONTEXT_ITEM, new LexicalValue(contextItemSource.value));
+            } else if (contextItemSource.input != null) {
+                if (contextItemSource.input.equals("-")) {
+                    variables.put(Name.CONTEXT_ITEM, new StandardInputValue(this.contextItemInputFormat));
+                } else {
+                    variables.put(Name.CONTEXT_ITEM, new FileValue(parseLocation(contextItemSource.input)));
+                }
+            }
+        }
+
+        return new CLIBindings(variables);
+    }
+
+    private static URI parseLocation(String location) {
+        try {
+            return URI.create(location);
+        } catch (IllegalArgumentException e) {
+            throw new CliException("Invalid URI provided: " + location);
+        }
+    }
 }
