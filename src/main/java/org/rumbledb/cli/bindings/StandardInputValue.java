5727fbb140d16ff8fc9c331f216d10f68bbf8269
Jimmy Cai <jimmy@cai.im>
2026-07-02 20:23:54 +0200

    Refactor StandardInputValue to include InputFormat string

--- src/main/java/org/rumbledb/cli/bindings/StandardInputValue.java
+++ src/main/java/org/rumbledb/cli/bindings/StandardInputValue.java
@@ -1,16 +1,32 @@
 package org.rumbledb.cli.bindings;
 
+import java.util.Objects;
+
 public class StandardInputValue implements VariableSource {
     public enum InputFormat {
-        JSON,
-        TEXT
+        JSON("json"),
+        TEXT("text");
+
+        private final String format;
+
+        InputFormat(String format) {
+            this.format = format;
+        }
+
+        public static InputFormat fromString(String format) {
+            for (InputFormat inputFormat : InputFormat.values()) {
+                if (inputFormat.format.equalsIgnoreCase(format)) {
+                    return inputFormat;
+                }
+            }
+            throw new IllegalArgumentException("Invalid input format: " + format);
+        }
     }
 
     public final InputFormat format;
 
-    public StandardInputValue() {
-        // Default to JSON if no format is specified
-        this(InputFormat.JSON);
+    public StandardInputValue(String format) {
+        this.format = Objects.requireNonNullElse(InputFormat.fromString(format), InputFormat.JSON);
     }
 
     public StandardInputValue(InputFormat format) {
