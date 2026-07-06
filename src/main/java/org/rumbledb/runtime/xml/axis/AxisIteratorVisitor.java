d139745af4fdd4da2cd80365e72a8df41f3fc303
Jimmy Cai <jimmy@cai.im>
2026-07-06 23:40:14 +0200

    Comment out configuration mutation in AxisIteratorVisitor to prevent misuse during execution

--- src/main/java/org/rumbledb/runtime/xml/axis/AxisIteratorVisitor.java
+++ src/main/java/org/rumbledb/runtime/xml/axis/AxisIteratorVisitor.java
@@ -29,12 +29,13 @@ public class AxisIteratorVisitor {
             case ATTRIBUTE:
                 return new AttributeAxisIterator(staticContext);
             case FOLLOWING:
-                staticContext.getConfiguration().setOptimizeParentPointers(false);
+                // staticContext.getConfiguration().setOptimizeParentPointers(false);
                 return new FollowingAxisIterator(staticContext);
             case DESCENDANT:
                 return new DescendantAxisIterator(staticContext);
             case FOLLOWING_SIBLING:
-                staticContext.getConfiguration().setOptimizeParentPointers(false);
+                // TODO: I believe this is a misuse as we are mutating the configuration in the middle of the execution.
+                // staticContext.getConfiguration().setOptimizeParentPointers(false);
                 return new FollowingSiblingAxisIterator(staticContext);
             case DESCENDANT_OR_SELF:
                 return new DescendantOrSelfAxisIterator(staticContext);
@@ -47,7 +48,8 @@ public class AxisIteratorVisitor {
     }
 
     public AxisIterator visit(ReverseStepExpr reverseStep, RuntimeStaticContext staticContext) {
-        staticContext.getConfiguration().setOptimizeParentPointers(false);
+        // TODO: I believe this is a misuse as we are mutating the configuration in the middle of the execution.
+        // staticContext.getConfiguration().setOptimizeParentPointers(false);
         switch (reverseStep.getReverseAxis()) {
             case PARENT:
                 return new ParentAxisIterator(staticContext);
