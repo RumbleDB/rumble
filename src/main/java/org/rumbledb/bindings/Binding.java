73e98047f832ba3da8efa0f7966317ebf3712b34
Jimmy Cai <jimmy@cai.im>
2026-07-07 00:14:34 +0200

    Apply spotless

--- src/main/java/org/rumbledb/bindings/Binding.java
+++ src/main/java/org/rumbledb/bindings/Binding.java
@@ -3,6 +3,7 @@ package org.rumbledb.bindings;
 import java.io.Serializable;
 
 public sealed interface Binding
-        extends Serializable
+        extends
+            Serializable
         permits DataFrameBinding, FileBinding, ItemSequenceBinding, LexicalBinding, StandardInputBinding {
 }
