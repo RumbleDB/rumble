4f6e7d44e10951899d134df017aa7c69edb4d672
Henrik Pätzold <62308300+HenrikPaetzold@users.noreply.github.com>
2026-07-08 13:15:56 +0200

    implement more functions

--- src/main/java/org/rumbledb/runtime/functions/xml/HasChildrenFunctionIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/xml/HasChildrenFunctionIterator.java
@@ -2,8 +2,10 @@ package org.rumbledb.runtime.functions.xml;
 
 import org.rumbledb.api.Item;
 import org.rumbledb.context.DynamicContext;
+import org.rumbledb.context.Name;
 import org.rumbledb.context.RuntimeStaticContext;
-import org.rumbledb.exceptions.UnimplementedFunctionException;
+import org.rumbledb.exceptions.UnexpectedTypeException;
+import org.rumbledb.items.ItemFactory;
 import org.rumbledb.runtime.AtMostOneItemLocalRuntimeIterator;
 import org.rumbledb.runtime.RuntimeIterator;
 
@@ -21,6 +23,22 @@ public class HasChildrenFunctionIterator extends AtMostOneItemLocalRuntimeIterat
 
     @Override
     public Item materializeFirstItemOrNull(DynamicContext context) {
-        throw new UnimplementedFunctionException("fn:has-children", getMetadata());
+        Item node = getContextNode(context);
+        if (node == null) {
+            return ItemFactory.getInstance().createBooleanItem(false);
+        }
+        if (!node.isNode()) {
+            throw new UnexpectedTypeException("The argument to fn:has-children must be a node", getMetadata());
+        }
+        return ItemFactory.getInstance().createBooleanItem(!node.children().isEmpty());
+    }
+
+    private Item getContextNode(DynamicContext context) {
+        if (!this.children.isEmpty()) {
+            return this.children.get(0).materializeFirstItemOrNull(context);
+        }
+        return context.getVariableValues()
+            .getLocalVariableValue(Name.CONTEXT_ITEM, getMetadata())
+            .get(0);
     }
 }
