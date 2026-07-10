600c9486bfa35d3269fbb1f109bcc79260edeec9
Jimmy Cai <jimmy@cai.im>
2026-07-10 16:36:16 +0200

    Revert getActualSequenceType

--- src/main/java/org/rumbledb/compiler/StaticContextVisitor.java
+++ src/main/java/org/rumbledb/compiler/StaticContextVisitor.java
@@ -241,7 +241,7 @@ public class StaticContextVisitor extends AbstractNodeVisitor<StaticContext> {
         StaticContext result = new StaticContext(argument);// add a block level to function declaration body
         result.addVariable(
             clause.getVariableName(),
-            clause.getSequenceType(),
+            clause.getActualSequenceType(),
             clause.getMetadata()
         );
         clause.getSequenceType().resolve(result, clause.getMetadata());
@@ -309,7 +309,7 @@ public class StaticContextVisitor extends AbstractNodeVisitor<StaticContext> {
         StaticContext result = new StaticContext(argument);
         result.addVariable(
             clause.getVariableName(),
-            clause.getSequenceType(),
+            clause.getActualSequenceType(),
             clause.getMetadata()
         );
         clause.getSequenceType().resolve(result, clause.getMetadata());
