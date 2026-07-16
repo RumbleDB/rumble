7aca10d52e0c6aa7087a1351a7568a68a37e5e80
Jimmy Cai <jimmy@cai.im>
2026-07-17 00:06:05 +0200

    Refactor SchemaCatalog to encapsulate built-in schema creation and provide a static method for accessing it

--- src/main/java/org/rumbledb/context/SchemaCatalog.java
+++ src/main/java/org/rumbledb/context/SchemaCatalog.java
@@ -10,20 +10,39 @@ package org.rumbledb.context;
 import javax.xml.validation.Schema;
 
 import org.apache.xerces.impl.xs.SchemaGrammar;
+import org.apache.xerces.jaxp.validation.XMLSchemaFactory;
 import org.apache.xerces.xs.XSModel;
 import org.apache.xerces.xs.XSTypeDefinition;
+import org.xml.sax.SAXException;
 
 /** The XML Schema components compiled for an XQuery module. */
 public record SchemaCatalog(Schema validationSchema, XSModel schemaModel) {
+
+    private static final SchemaCatalog BUILT_IN = createBuiltIn();
+
+    public static SchemaCatalog builtIn() {
+        return BUILT_IN;
+    }
 
     public XSTypeDefinition getTypeDefinition(Name name) {
         XSTypeDefinition type = this.schemaModel.getTypeDefinition(name.getLocalName(), name.getNamespace());
         return type == null ? getBuiltInTypeDefinition(name) : type;
     }
 
-    public static XSTypeDefinition getBuiltInTypeDefinition(Name name) {
+    private static XSTypeDefinition getBuiltInTypeDefinition(Name name) {
         return Name.XS_NS.equals(name.getNamespace())
             ? SchemaGrammar.SG_SchemaNS.getGlobalTypeDecl(name.getLocalName())
             : null;
+    }
+
+    private static SchemaCatalog createBuiltIn() {
+        try {
+            return new SchemaCatalog(
+                    new XMLSchemaFactory().newSchema(),
+                    SchemaGrammar.SG_SchemaNS.toXSModel()
+            );
+        } catch (SAXException exception) {
+            throw new ExceptionInInitializerError(exception);
+        }
     }
 }
