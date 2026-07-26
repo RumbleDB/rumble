60a74beb9a1eba30d64b6f15fb83ff26f7523ff4
Jimmy Cai <jimmy@cai.im>
2026-07-26 12:22:04 +0200

    Add MappingLocalCursor for one-to-one transformation in local plans

---
+++ src/main/java/org/rumbledb/runtime/cursor/MappingLocalCursor.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.rumbledb.runtime.cursor;
+
+import java.util.function.Function;
+
+import lombok.NonNull;
+import org.rumbledb.context.DynamicContext;
+import org.rumbledb.exceptions.ExceptionMetadata;
+import org.rumbledb.runtime.plan.RuntimePlan;
+
+/**
+ * Cursor that lazily applies a one-to-one transformation to another local plan.
+ *
+ * @param <I> the input type
+ * @param <O> the output type
+ */
+public final class MappingLocalCursor<I, O> extends AbstractLocalCursor<O> {
+
+    private final RuntimePlan<I> inputPlan;
+    private final DynamicContext context;
+    private final Function<? super I, ? extends O> mapper;
+    private LocalCursor<I> inputCursor;
+
+    public MappingLocalCursor(
+            @NonNull RuntimePlan<I> inputPlan,
+            @NonNull DynamicContext context,
+            @NonNull Function<? super I, ? extends O> mapper,
+            @NonNull ExceptionMetadata metadata
+    ) {
+        super(metadata);
+        this.inputPlan = inputPlan;
+        this.context = context;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void openLocal() {
+        this.inputCursor = this.inputPlan.createLocalCursor(this.context);
+        this.inputCursor.open();
+    }
+
+    @Override
+    protected boolean hasNextLocal() {
+        return this.inputCursor.hasNext();
+    }
+
+    @Override
+    protected O nextLocal() {
+        return this.mapper.apply(this.inputCursor.next());
+    }
+
+    @Override
+    protected void closeLocal() {
+        if (this.inputCursor != null) {
+            this.inputCursor.close();
+            this.inputCursor = null;
+        }
+    }
+}
--- src/main/java/org/rumbledb/runtime/functions/FunctionUntypedAtomicCastIterator.java
+++ src/main/java/org/rumbledb/runtime/functions/FunctionUntypedAtomicCastIterator.java
@@ -6,15 +6,19 @@ import org.rumbledb.api.Item;
 import org.rumbledb.context.DynamicContext;
 import org.rumbledb.context.RuntimeStaticContext;
 import org.rumbledb.exceptions.CannotConvertToQNameException;
+import org.rumbledb.exceptions.ExceptionMetadata;
 import org.rumbledb.exceptions.UnexpectedTypeException;
 import org.rumbledb.items.structured.JSoundDataFrame;
 import org.rumbledb.runtime.HybridRuntimeIterator;
 import org.rumbledb.runtime.RuntimeIterator;
+import org.rumbledb.runtime.cursor.LocalCursor;
+import org.rumbledb.runtime.cursor.MappingLocalCursor;
 import org.rumbledb.runtime.flwor.NativeClauseContext;
 import org.rumbledb.runtime.typing.CastIterator;
 import org.rumbledb.types.BuiltinTypesCatalogue;
 import org.rumbledb.types.ItemType;
 
+import lombok.NonNull;
 import java.io.Serial;
 import java.util.Collections;
 
@@ -28,8 +32,7 @@ public class FunctionUntypedAtomicCastIterator extends HybridRuntimeIterator {
     private static final long serialVersionUID = 1L;
 
     private final RuntimeIterator iterator;
-    private final ItemType targetType;
-    private final String exceptionMessage;
+    private final UntypedAtomicCaster caster;
 
     public FunctionUntypedAtomicCastIterator(
             RuntimeIterator iterator,
@@ -39,8 +42,17 @@ public class FunctionUntypedAtomicCastIterator extends HybridRuntimeIterator {
     ) {
         super(Collections.singletonList(iterator), staticContext);
         this.iterator = iterator;
-        this.targetType = targetType;
-        this.exceptionMessage = exceptionMessage;
+        this.caster = new UntypedAtomicCaster(targetType, exceptionMessage, staticContext, getMetadata());
+    }
+
+    @Override
+    public LocalCursor<Item> createLocalCursor(DynamicContext context) {
+        return new MappingLocalCursor<>(
+                this.iterator,
+                context,
+                this.caster::call,
+                getMetadata()
+        );
     }
 
     @Override
@@ -55,18 +67,7 @@ public class FunctionUntypedAtomicCastIterator extends HybridRuntimeIterator {
 
     @Override
     protected JavaRDD<Item> getRDDAux(DynamicContext context) {
-        return this.iterator.getRDD(context)
-            .map(
-                new Function<Item, Item>() {
-                    @Serial
-                    private static final long serialVersionUID = 1L;
-
-                    @Override
-                    public Item call(Item item) {
-                        return castIfUntypedAtomic(item);
-                    }
-                }
-            );
+        return this.iterator.getRDD(context).map(this.caster);
     }
 
     @Override
@@ -77,7 +78,7 @@ public class FunctionUntypedAtomicCastIterator extends HybridRuntimeIterator {
 
     @Override
     public Item nextLocal() {
-        return castIfUntypedAtomic(this.iterator.next());
+        return this.caster.call(this.iterator.next());
     }
 
     @Override
@@ -95,66 +96,89 @@ public class FunctionUntypedAtomicCastIterator extends HybridRuntimeIterator {
         return NativeClauseContext.NoNativeQuery;
     }
 
-    private Item castIfUntypedAtomic(Item item) {
-        if (item == null || !item.isUntypedAtomic()) {
-            return item;
-        }
-        if (item.getDynamicType().isSubtypeOf(this.targetType)) {
-            return item;
-        }
-        if (
-            isQNameLikeTargetType(this.targetType)
-                || isNotationLikeTargetType(this.targetType)
+    private static final class UntypedAtomicCaster implements Function<Item, Item> {
+
+        @Serial
+        private static final long serialVersionUID = 1L;
+
+        private final ItemType targetType;
+        private final String exceptionMessage;
+        private final RuntimeStaticContext staticContext;
+        private final ExceptionMetadata metadata;
+
+        private UntypedAtomicCaster(
+                @NonNull ItemType targetType,
+                @NonNull String exceptionMessage,
+                @NonNull RuntimeStaticContext staticContext,
+                @NonNull ExceptionMetadata metadata
         ) {
-            if (usesQNameCoercionErrorSemantics()) {
-                throw new CannotConvertToQNameException(
-                        this.exceptionMessage
-                            + item.getDynamicType()
-                            + " cannot be implicitly converted to type "
-                            + this.targetType
-                            + ".",
-                        getMetadata()
-                );
+            this.targetType = targetType;
+            this.exceptionMessage = exceptionMessage;
+            this.staticContext = staticContext;
+            this.metadata = metadata;
+        }
+
+        @Override
+        public Item call(Item item) {
+            if (item == null || !item.isUntypedAtomic()) {
+                return item;
             }
-            throw new UnexpectedTypeException(
-                    this.exceptionMessage
-                        + item.getDynamicType()
-                        + " cannot be implicitly converted to type "
-                        + this.targetType
-                        + ".",
-                    getMetadata()
+            if (item.getDynamicType().isSubtypeOf(this.targetType)) {
+                return item;
+            }
+            if (isQNameLikeTargetType(this.targetType) || isNotationLikeTargetType(this.targetType)) {
+                throw invalidQNameOrNotationConversion(item);
+            }
+
+            Item converted = CastIterator.castItemToType(
+                item,
+                this.targetType,
+                this.metadata,
+                this.staticContext
             );
+            if (converted == null) {
+                throw unexpectedType(item);
+            }
+            return converted;
         }
-        Item converted = CastIterator.castItemToType(item, this.targetType, getMetadata(), this.staticContext);
-        if (converted == null) {
-            throw new UnexpectedTypeException(
-                    this.exceptionMessage
-                        + item.getDynamicType()
-                        + " cannot be implicitly converted to type "
-                        + this.targetType
-                        + ".",
-                    getMetadata()
-            );
+
+        private RuntimeException invalidQNameOrNotationConversion(Item item) {
+            if (usesQNameCoercionErrorSemantics()) {
+                return new CannotConvertToQNameException(errorMessage(item), this.metadata);
+            }
+            return unexpectedType(item);
         }
-        return converted;
+
+        private UnexpectedTypeException unexpectedType(Item item) {
+            return new UnexpectedTypeException(errorMessage(item), this.metadata);
+        }
+
+        private String errorMessage(Item item) {
+            return this.exceptionMessage
+                + item.getDynamicType()
+                + " cannot be implicitly converted to type "
+                + this.targetType
+                + ".";
+        }
+
+        private static boolean isQNameLikeTargetType(ItemType itemType) {
+            return itemType.equals(BuiltinTypesCatalogue.QNameItem)
+                || (itemType.isAtomicItemType()
+                    && !itemType.equals(BuiltinTypesCatalogue.errorItem)
+                    && itemType.getCastingPrimitiveType().equals(BuiltinTypesCatalogue.QNameItem));
+        }
+
+        private static boolean isNotationLikeTargetType(ItemType itemType) {
+            return itemType.equals(BuiltinTypesCatalogue.NOTATIONItem)
+                || (itemType.isAtomicItemType()
+                    && !itemType.equals(BuiltinTypesCatalogue.errorItem)
+                    && itemType.getCastingPrimitiveType().equals(BuiltinTypesCatalogue.NOTATIONItem));
+        }
+
+        private boolean usesQNameCoercionErrorSemantics() {
+            String queryLanguage = this.staticContext.getQueryLanguage();
+            return !queryLanguage.equals("xquery10") && !queryLanguage.equals("jsoniq10");
+        }
     }
 
-    private boolean isQNameLikeTargetType(ItemType itemType) {
-        return itemType.equals(BuiltinTypesCatalogue.QNameItem)
-            || (itemType.isAtomicItemType()
-                && !itemType.equals(BuiltinTypesCatalogue.errorItem)
-                && itemType.getCastingPrimitiveType().equals(BuiltinTypesCatalogue.QNameItem));
-    }
-
-    private boolean isNotationLikeTargetType(ItemType itemType) {
-        return itemType.equals(BuiltinTypesCatalogue.NOTATIONItem)
-            || (itemType.isAtomicItemType()
-                && !itemType.equals(BuiltinTypesCatalogue.errorItem)
-                && itemType.getCastingPrimitiveType().equals(BuiltinTypesCatalogue.NOTATIONItem));
-    }
-
-    private boolean usesQNameCoercionErrorSemantics() {
-        String queryLanguage = this.staticContext.getQueryLanguage();
-        return !queryLanguage.equals("xquery10") && !queryLanguage.equals("jsoniq10");
-    }
 }
