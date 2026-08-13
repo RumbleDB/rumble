5ce1fbf98156bf81d5ce78abfa95b652c73573ee
Jimmy Cai <jimmy@cai.im>
2026-08-13 13:14:19 +0200

    Enhance attribute node handling in TranslationVisitor and XQueryTranslationVisitor to support type names

--- src/main/java/org/rumbledb/compiler/XQueryTranslationVisitor.java
+++ src/main/java/org/rumbledb/compiler/XQueryTranslationVisitor.java
@@ -2301,16 +2301,16 @@ public class XQueryTranslationVisitor extends XQueryParserBaseVisitor<Node> {
         }
         if (kindTestContext.attributeTest() != null) {
             XQueryParser.AttributeTestContext attributeTestContext = kindTestContext.attributeTest();
-            if (attributeTestContext.typeName() != null) {
-                throw new UnsupportedFeatureException(
-                        "Typed attribute item tests are not supported yet",
-                        createMetadataFromContext(attributeTestContext));
-            }
+            Name typeName = attributeTestContext.typeName() == null
+                    ? null
+                    : parseEqName(attributeTestContext.typeName().eqName(), false, true, false, false);
             if (attributeTestContext.attributeNameOrWildcard() == null) {
                 return BuiltinTypesCatalogue.attributeNode;
             }
             if (attributeTestContext.attributeNameOrWildcard().attributeName() == null) {
-                return BuiltinTypesCatalogue.attributeNode;
+                return typeName == null
+                        ? BuiltinTypesCatalogue.attributeNode
+                        : ItemTypeFactory.attributeNodeItemType(null, typeName);
             }
             Name attributeName = parseEqName(
                     attributeTestContext
@@ -2321,7 +2321,9 @@ public class XQueryTranslationVisitor extends XQueryParserBaseVisitor<Node> {
                     false,
                     false,
                     false);
-            return ItemTypeFactory.attributeNodeItemType(attributeName);
+            return typeName == null
+                    ? ItemTypeFactory.attributeNodeItemType(attributeName)
+                    : ItemTypeFactory.attributeNodeItemType(attributeName, typeName);
         }
         if (kindTestContext.commentTest() != null) {
             return BuiltinTypesCatalogue.commentNode;
