d058270f2d36a1341cfe28945ecd269e46fc97ae
Jimmy Cai <jimmy@cai.im>
2026-09-08 17:32:54 +0200

    Refactor node handling in constructors to use NodeConstructionUtils for improved consistency and maintainability

--- src/main/java/org/rumbledb/runtime/xml/DirElemConstructorRuntimeIterator.java
+++ src/main/java/org/rumbledb/runtime/xml/DirElemConstructorRuntimeIterator.java
@@ -105,7 +105,7 @@ public class DirElemConstructorRuntimeIterator extends AbstractAtMostOneItemRunt
                                         "Attribute or namespace nodes must appear before all other nodes in element content");
                             }
                             if (item.isAttributeNode()) {
-                                attributes.add(item.copy(true));
+                                attributes.add(NodeConstructionUtils.copyNode(item, this.staticContext));
                             } else {
                                 namespaces.add(item.copy(true));
                             }
@@ -147,7 +147,7 @@ public class DirElemConstructorRuntimeIterator extends AbstractAtMostOneItemRunt
                                 textAccumulator = null;
                             }
                             // add the non-text node
-                            content.add(NamespaceFixupUtils.copyNodeForConstructor(item, this.staticContext));
+                            content.add(NodeConstructionUtils.copyNode(item, this.staticContext));
                             previousItemWasAtomic = false;
                         }
                     }
@@ -174,7 +174,7 @@ public class DirElemConstructorRuntimeIterator extends AbstractAtMostOneItemRunt
                 for (Item item : materialize.apply(iterator, contextToUse)) {
                     // attributes should be attribute nodes
                     if (item.isAttributeNode()) {
-                        attributes.add(item.copy(true));
+                        attributes.add(NodeConstructionUtils.copyNode(item, this.staticContext));
                     }
                 }
             }
@@ -196,6 +196,7 @@ public class DirElemConstructorRuntimeIterator extends AbstractAtMostOneItemRunt
             String documentPath = XMLDocumentPosition.generateConstructedTreePath();
             elementItem.setXmlDocumentPosition(documentPath, 0);
         }
+        NodeConstructionUtils.initializeElement(elementItem, this.staticContext);
         return elementItem;
     }
 
