e70bfcf03b16157dac985265dd0850908dd55280
Jimmy Cai <jimmy@cai.im>
2026-09-09 17:25:57 +0200

    Fix build

--- src/main/java/org/rumbledb/compiler/RuntimeIteratorVisitor.java
+++ src/main/java/org/rumbledb/compiler/RuntimeIteratorVisitor.java
@@ -1482,7 +1482,8 @@ public class RuntimeIteratorVisitor extends AbstractNodeVisitor<ItemRuntimePlan>
     @Override
     public ItemRuntimePlan visitValidateExpression(ValidateExpression expression, ItemRuntimePlan argument) {
         ItemRuntimePlan operand = this.visit(expression.getMainExpression(), argument);
-        XmlSchemaCatalog schemaCatalog = expression.getStaticContext().getXmlSchemaCatalog();
+        XmlSchemaCatalog schemaCatalog =
+                expression.getStaticContext().getInScopeSchemaTypes().getXmlSchemaCatalog();
         if (schemaCatalog == null && expression.getValidationMode() != ValidateExpression.ValidationMode.TYPE) {
             schemaCatalog = XmlSchemaCatalogLoader.loadBuiltInCatalog();
         }
